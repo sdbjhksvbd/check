b9635154c4c2760184cf024051ecebe3e2d857d0
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 23:42:34 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/models/services/data/Indexes.js
@@ -0,0 +1,34 @@
+define(
+    [
+        'jquery',
+        'underscore',
+        'models/EAIBase'
+    ],
+    function(
+        $,
+        _,
+        EAIBaseModel
+    ) {
+        return EAIBaseModel.extend({
+            url: 'data/indexes',
+            urlRoot: "data/indexes",
+            defaults: {
+                datatype: 'event',
+                'metric.timestampResolution': 's'
+            },
+
+            getDataType: function() {
+                return this.entry.content.get("datatype") || 'event';
+            },
+
+            getBucketPath: function() {
+                var selfStorageBucketPath = this.entry.content.get('archiver.selfStorageBucket');
+                var selfStorageBucketFolder = this.entry.content.get('archiver.selfStorageBucketFolder');
+                if (selfStorageBucketFolder) {
+                    selfStorageBucketPath += '/' + selfStorageBucketFolder;
+                }
+                return selfStorageBucketPath;
+            }
+        });
+    }
+);
