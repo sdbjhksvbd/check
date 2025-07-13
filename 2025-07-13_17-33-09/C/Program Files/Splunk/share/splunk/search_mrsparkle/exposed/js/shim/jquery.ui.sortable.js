cd3180cad5f608de49c03b357eb3ff8ca0c48254
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 23:52:50 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/shim/jquery.ui.sortable.js
@@ -0,0 +1,8 @@
+define(['jquery',
+       'jquery.ui.widget',
+       'jquery.ui.mouse',
+       'jquery.ui.draggable',
+       'jquery.ui.droppable',
+       'imports-loader?jQuery=jquery!jquery-ui/ui/widgets/sortable'], function($) {
+    return $;
+});
