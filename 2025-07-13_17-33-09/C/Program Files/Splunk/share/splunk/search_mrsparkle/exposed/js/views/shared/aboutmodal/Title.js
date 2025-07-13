64d434fed7c86522a18cc126f9f5d911f0d23c77
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 00:47:37 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/views/shared/aboutmodal/Title.js
@@ -0,0 +1,36 @@
+define([
+    'jquery',
+    'underscore',
+    'module',
+    'views/Base',
+    './Title.pcssm'
+],
+function(
+    $,
+    _,
+    module,
+    BaseView,
+    css
+){
+    return BaseView.extend({
+        moduleId: module.id,
+        css: css,
+        initialize: function() {
+            BaseView.prototype.initialize.apply(this, arguments);
+            this.model.serverInfo.on('change reset', function() {
+                this.render();
+            }, this);
+        },
+
+        render: function() {
+            var html = this.compiledTemplate({
+                css: css
+            });
+
+            this.$el.html(html);
+            return this;
+        },
+        template: '<span data-title-role="about" tabindex="0" aria-label="<%- _("About page").t() %>">' +
+            '<%- _("About").t() %></span>'
+    });
+});
