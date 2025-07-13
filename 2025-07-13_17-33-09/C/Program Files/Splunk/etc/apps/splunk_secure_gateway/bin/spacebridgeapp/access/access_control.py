e051c51615f95f91421b4e3867fabf543aba48be
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 19:40:32 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/etc/apps/splunk_secure_gateway/bin/spacebridgeapp/access/access_control.py
@@ -0,0 +1,36 @@
+"""
+Copyright (C) 2009-2023 Splunk Inc. All Rights Reserved.
+
+Module used to determine access control for modular_inputs
+"""
+from spacebridgeapp.rest.opt_in.opt_in_handler import is_opt_in, DEFAULT_OPT_IN
+from spacebridgeapp.rest.services.splunk_service import is_fips_mode
+from solnlib.server_info import ServerInfo
+from spacebridgeapp.util.config import load_config
+
+
+def allow_access(session_key):
+    """
+    Helper method to return logic on if access to Spacebridge should be granted
+
+    +============+===========+==============+
+    |     -      |  Non-FIPS |     FIPS     |
+    +============+===========+==============+
+    | Cloud      |   opt-in  | oia + opt-in |
+    +------------+-----------+--------------+
+    | Enterprise |        opt-in            |
+    +------------+-----------+--------------+
+
+    :return: True if access is granted
+    """
+    opt_in = is_opt_in(DEFAULT_OPT_IN, session_key)
+
+    # Check if user has acknowledged lack of fips compliance in Spacebridge
+    fips_mode = is_fips_mode(session_key)
+    is_cloud = ServerInfo(session_key).is_cloud_instance()
+    config = load_config(session_key)
+    
+    if is_cloud and fips_mode:
+        opt_in_approved = config.get_oia()
+        return opt_in and opt_in_approved
+    return opt_in
