04ce36d0fc1ea0f4e217dcc7c4cea2aab460bb5d
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 22:52:18 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/Python-3.7/Scripts/bottle.py
@@ -0,0 +1,4 @@
+#!C:\builds\splcore\main\build_home\splunk\bin\python3.exe
+# EASY-INSTALL-SCRIPT: 'bottle==0.12.25','bottle.py'
+__requires__ = 'bottle==0.12.25'
+__import__('pkg_resources').run_script('bottle==0.12.25', 'bottle.py')
