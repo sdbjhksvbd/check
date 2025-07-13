09ebf0040542307fa5a36f3830008332e0b81bc3
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 00:04:36 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/util/console.js
@@ -0,0 +1,16 @@
+define([
+            'underscore',
+            'util/console_dev',
+            'splunk.logger',
+            'splunk.util'
+        ],
+        function(
+            _,
+            devConsole,
+            SplunkLogger,
+            splunkUtils
+        ) {
+
+    return SplunkLogger.getLogger(splunkUtils.getConfigValue('USERNAME', '') + ':::' + window.location.href);
+
+});
