7ee59d8bad5b69f104ae96aaa8d04baaf9bcd898
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 19:34:51 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/etc/apps/splunk_monitoring_console/appserver/test/routers/test_managementconsoleforwardersetup.js
@@ -0,0 +1,50 @@
+define([
+    '@splunk/swc-mc/dist/test-dependencies',
+    'splunk_monitoring_console/routers/MonitoringConsoleForwarderSetup',
+    'splunk_monitoring_console/views/settings/forwarder_setup/enterprise/Master',
+    'splunk_monitoring_console/views/settings/forwarder_setup/lite/Master',
+], function (SwcMcTest, Router, MasterView, MasterLightView) {
+    var LOCALE = 'en-US';
+    var APP = 'splunk_monitoring_console';
+    var PAGE = 'managementconsole_forwarder_setup';
+
+    suite('DMC Forwarder Setup Router', function () {
+        util(SwcMcTest.QunitUtils.SplunkdPartials);
+
+        setup(function () {
+            this.router = new Router();
+            sinon.spy(MasterView.prototype, 'initialize');
+            sinon.spy(MasterLightView.prototype, 'initialize');
+
+            // Refer "Returning Promises from Stubs" section from below
+            // https://www.sitepoint.com/promises-in-javascript-unit-tests-the-definitive-guide/
+            this.router.deferreds.pageViewRendered = sinon.stub();
+            this.router.deferreds.pageViewRendered.returns(Promise.resolve('a success'));
+            this.router.deferreds.searchesCollectionDfd = sinon.stub();
+            this.router.deferreds.searchesCollectionDfd.returns(Promise.resolve('a success'));
+            this.router.pageView = {};
+            this.router.collection.searchesCollection = new SwcMcTest.MockSplunkDsCollection();
+            this.router.collection.searchesCollection.add(new SwcMcTest.MockSplunkDModel());
+            this.router.addPageViewEl = sinon.stub();
+
+            assert.ok(this.router, 'router created');
+        });
+
+        teardown(function () {
+            MasterView.prototype.initialize.restore();
+            MasterLightView.prototype.initialize.restore();
+        });
+
+        test('initialize enterprise view:', function (done) {
+            this.router.page(LOCALE, APP, PAGE);
+            this.router.pagePromise.then(function() {
+                assert.equal(
+                    MasterView.prototype.initialize.callCount,
+                    1,
+                    'Enterprise Master View should be instantiated once'
+                );
+                done();
+            });
+        });
+    });
+});
