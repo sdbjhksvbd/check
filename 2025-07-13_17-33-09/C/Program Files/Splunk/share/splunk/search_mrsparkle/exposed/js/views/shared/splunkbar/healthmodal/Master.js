09219f968c7ef24167aad8b6bdccc12c9bd40590
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 01:11:54 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/views/shared/splunkbar/healthmodal/Master.js
@@ -0,0 +1,35 @@
+define([
+    'jquery',
+    'underscore',
+    'module',
+    'views/shared/ModalLocalClassNames',
+    './HealthTitle',
+    './HealthContents',
+    './Master.pcssm',
+],
+function(
+    $,
+    _,
+    module,
+    ModalView,
+    HealthTitleView,
+    HealthContentsView,
+    css
+) {
+    return ModalView.extend({
+        moduleId: module.id,
+        css: _.extend({}, ModalView.prototype.css, css),
+        initialize: function() {
+            this.options.titleView = new HealthTitleView();
+            this.options.bodyView = new HealthContentsView({
+                model: {
+                    application: this.model.application,
+                    serverInfo: this.model.serverInfo,
+                    user: this.model.user,
+                }
+            });
+            this.options.scrollPropagation = true;
+            ModalView.prototype.initialize.apply(this, arguments);
+        },
+    });
+});
