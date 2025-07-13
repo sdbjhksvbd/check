5a96af2a49a0ff307c729d1ca35e12587895e0e1
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 23:30:41 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/contrib/jquery.ui.datepicker/jquery.ui.datepicker-no.js
@@ -0,0 +1,23 @@
+/* Norwegian initialisation for the jQuery UI date picker plugin. */
+/* Written by Naimdjon Takhirov (naimdjon@gmail.com). */
+jQuery(function($){
+    $.datepicker.regional['no'] = {
+		closeText: 'Lukk',
+        prevText: '&laquo;Forrige',
+		nextText: 'Neste&raquo;',
+		currentText: 'I dag',
+        monthNames: ['Januar','Februar','Mars','April','Mai','Juni',
+        'Juli','August','September','Oktober','November','Desember'],
+        monthNamesShort: ['Jan','Feb','Mar','Apr','Mai','Jun',
+        'Jul','Aug','Sep','Okt','Nov','Des'],
+		dayNamesShort: ['SÃ¸n','Man','Tir','Ons','Tor','Fre','LÃ¸r'],
+		dayNames: ['SÃ¸ndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','LÃ¸rdag'],
+		dayNamesMin: ['SÃ¸','Ma','Ti','On','To','Fr','LÃ¸'],
+		weekHeader: 'Uke',
+        dateFormat: 'yy-mm-dd',
+		firstDay: 0,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+    $.datepicker.setDefaults($.datepicker.regional['no']);
+});
