d2d135eabbd80392acdc5e316ba5e7dd0c1aef90
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 19:19:06 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/etc/apps/splunk-dashboard-studio/appserver/static/build/chunks/chunk-VHLICZNV.js
@@ -0,0 +1,32 @@
+import{f as e,g as p}from"./chunk-JNNA6YYS.js";import{k as r,m as u}from"./chunk-P7VIGQO4.js";import{i as t}from"./chunk-FOXWPXSG.js";var a=t(u(),1),x=t(u(),1),o=t(r(),1),y=t(r(),1),m={};e(m,{default:()=>s});p(m,x);var i,n,s=(n=(i=a.default)==null?void 0:i.default)!=null?n:a.default,f={};e(f,{default:()=>_});p(f,y);var l,d,_=(d=(l=o.default)==null?void 0:l.default)!=null?d:o.default,h=`
+    input[type='color'],
+    input[type='date'],
+    input[type='datetime-local'],
+    input[type='datetime'],
+    input[type='email'],
+    input[type='month'],
+    input[type='number'],
+    input[type='password'],
+    input[type='search'],
+    input[type='tel'],
+    input[type='text'],
+    input[type='time'],
+    input[type='url'],
+    input[type='week'] {
+        height: auto;
+        margin-bottom: 0px;
+        &:focus {
+            box-shadow: none;
+        }
+        padding-left: 0px;
+        padding-right: 0px;
+    }
+    
+    textarea {
+        height: auto;
+        margin-bottom: 0px;
+        &:focus {
+            box-shadow: none;
+        }
+    }
+`;export{s as a,_ as b,h as c};
