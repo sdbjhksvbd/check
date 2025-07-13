be141714cac95be4b69b0e93067e111ab8207113
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 23:13:52 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/collections/services/data/ui/Panels.js
@@ -0,0 +1,15 @@
+define([
+    'backbone',
+    'models/services/data/ui/Panel',
+    'collections/SplunkDsBase'
+], function (Backbone, PanelModel, SplunkDsBaseCollection) {
+
+    return SplunkDsBaseCollection.extend({
+        url: 'data/ui/panels',
+        model: PanelModel,
+        initialize: function () {
+            SplunkDsBaseCollection.prototype.initialize.apply(this, arguments);
+        }
+    });
+
+});
