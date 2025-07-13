d2094afc92450d37a5f75515dd718d22c0a166e6
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 19:20:25 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/etc/apps/splunk-dashboard-studio/appserver/static/build/examples/example-hub-image-visualization.js
@@ -0,0 +1,46 @@
+import{a as o}from"../chunks/chunk-N2ZXR5A7.js";import"../chunks/chunk-JH24LUXZ.js";import"../chunks/chunk-DLYUP5P4.js";import"../chunks/chunk-P7VIGQO4.js";import"../chunks/chunk-LT4H6J3K.js";import"../chunks/chunk-JNARL5PJ.js";import"../chunks/chunk-ELIEG7M2.js";import"../chunks/chunk-GECC65SD.js";import"../chunks/chunk-IZDCONH5.js";import"../chunks/chunk-UVCMF6AG.js";import"../chunks/chunk-VWCNWZTS.js";import"../chunks/chunk-HSXE43VV.js";import{a as i}from"../chunks/chunk-WZ7HRZ7Q.js";import"../chunks/chunk-S6E35LU3.js";import"../chunks/chunk-RQXY73SC.js";import"../chunks/chunk-6VJEWO5O.js";import"../chunks/chunk-EZGXPQUG.js";import{b as s}from"../chunks/chunk-NP732JJA.js";import{i as t}from"../chunks/chunk-FOXWPXSG.js";var n=t(s()),a=t(i());var e={visualizations:{viz_AohBmiPR:{type:"splunk.image",options:{preserveAspectRatio:!0,src:"/static/app/splunk-dashboard-studio/images/examples-hub/images/splunk_logo.png"}},viz_0kNU0yRv:{type:"splunk.image",options:{src:"/static/app/splunk-dashboard-studio/images/examples-hub/images/splunk_logo.png"}},viz_jtcenYVx:{type:"splunk.markdown",options:{content:"",markdown:`## Overview
+
+Images can be set in the dashboard or as part of the background. They are a great way to set some visual context, add company branding and also make your dashboard pop. `}},viz_uFN34V9Z:{type:"splunk.markdown",options:{content:"",markdown:`### Background Images
+
+In this page, we showcase a background used in one of your example dashboards. A background like this can help set the aesthetic of your dashboard, and then coloring options for each of the visualizations on your dashboard can compliment it.
+
+### Image as Background Source
+
+\`\`\`
+"layout": {
+		"type": "absolute",
+		"options": {
+			"height": 1400,
+			"width": 1440,
+			"backgroundColor": "transparent",
+			"display": "auto-scale",
+			"backgroundImage": {
+				"sizeType": "contain",
+				"x": 0,
+				"y": 0,
+				"src": "/static/app/splunk-dashboard-studio/images/examples-hub/ecommerce/background.png"
+			}
+		}
+}
+\`\`\``}},viz_h7074ePq:{type:"splunk.markdown",options:{content:"",markdown:`### Image Source - Preserved Aspect Ratio
+
+\`\`\`
+{
+    "type": "splunk.image",
+    "options": {
+        "preserveAspectRatio": true,
+        "src": "/static/app/splunk-dashboard-studio/images/examples-hub/images/splunk_logo.png"
+    },
+    "context": {}
+}
+\`\`\``}},viz_66Cmab3h:{type:"splunk.markdown",options:{content:"",markdown:`### Image Source - Aspect Ratio not Preserved
+
+\`\`\`
+{
+    "type": "splunk.image",
+    "options": {
+        "src": "/static/app/splunk-dashboard-studio/images/examples-hub/images/splunk_logo.png"
+    },
+    "context": {}
+}
+\`\`\``}}},dataSources:{},inputs:{},layout:{type:"absolute",options:{width:1440,backgroundColor:"transparent",display:"auto-scale",backgroundImage:{sizeType:"cover",x:0,y:0,src:"/static/app/splunk-dashboard-studio/images/examples-hub/ecommerce/background.png"},height:800},structure:[{item:"viz_AohBmiPR",type:"block",position:{x:740,y:40,w:330,h:320}},{item:"viz_0kNU0yRv",type:"block",position:{x:720,y:440,w:350,h:200}},{item:"viz_jtcenYVx",type:"block",position:{x:40,y:10,w:570,h:110}},{item:"viz_uFN34V9Z",type:"block",position:{x:40,y:150,w:590,h:480}},{item:"viz_h7074ePq",type:"block",position:{x:1080,y:20,w:340,h:350}},{item:"viz_66Cmab3h",type:"block",position:{x:1080,y:400,w:340,h:350}}]},description:"Use Images to add visual context and personalize your dashboard ",title:"Images"};(0,a.default)(n.default.createElement(o,{definition:e}),{pageTitle:"Images",hideFooter:!0,layout:"fixed"});
