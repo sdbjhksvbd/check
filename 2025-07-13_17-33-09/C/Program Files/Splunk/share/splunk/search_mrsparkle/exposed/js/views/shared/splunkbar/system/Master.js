e85fca4eb733d3efebfcfe973f21ec39c5f8d809
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 01:13:10 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/views/shared/splunkbar/system/Master.js
@@ -0,0 +1,40 @@
+define(
+[
+    'underscore',
+    'jquery',
+    'module',
+    'views/shared/Menu',
+    '../MenuButton',
+    './MenuContents',
+    'uri/route'
+],
+function(
+    _,
+    $,
+    module,
+    MenuView,
+    MenuButtonView,
+    MenuContentsView,
+    route
+){
+    return MenuView.extend({
+        moduleId: module.id,
+        initialize: function(){
+            this.options.dontAddModuleIdAsClass = true;
+            this.options.contentView = new MenuContentsView({
+                collection: {
+                    sections: this.collection.sections,
+                    managers: this.collection.managers,
+                    apps: this.collection.apps
+                },
+                model: {
+                    application: this.model.application,
+                    user: this.model.user,
+                    serverInfo: this.model.serverInfo,
+                }
+            });
+            this.options.toggleView = new MenuButtonView({label: _("Settings").t()});
+            MenuView.prototype.initialize.apply(this, arguments);
+        }
+    });
+});
