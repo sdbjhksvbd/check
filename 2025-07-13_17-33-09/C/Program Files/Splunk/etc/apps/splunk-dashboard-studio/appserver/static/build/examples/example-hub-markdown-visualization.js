da350e08162c7fb7a5cd9ddb6fb7ad7ef3d2edf6
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 19:20:31 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/etc/apps/splunk-dashboard-studio/appserver/static/build/examples/example-hub-markdown-visualization.js
@@ -0,0 +1,37 @@
+import{a as e}from"../chunks/chunk-N2ZXR5A7.js";import"../chunks/chunk-JH24LUXZ.js";import"../chunks/chunk-DLYUP5P4.js";import"../chunks/chunk-P7VIGQO4.js";import"../chunks/chunk-LT4H6J3K.js";import"../chunks/chunk-JNARL5PJ.js";import"../chunks/chunk-ELIEG7M2.js";import"../chunks/chunk-GECC65SD.js";import"../chunks/chunk-IZDCONH5.js";import"../chunks/chunk-UVCMF6AG.js";import"../chunks/chunk-VWCNWZTS.js";import"../chunks/chunk-HSXE43VV.js";import{a as l}from"../chunks/chunk-WZ7HRZ7Q.js";import"../chunks/chunk-S6E35LU3.js";import"../chunks/chunk-RQXY73SC.js";import"../chunks/chunk-6VJEWO5O.js";import"../chunks/chunk-EZGXPQUG.js";import{b as a}from"../chunks/chunk-NP732JJA.js";import{i as n}from"../chunks/chunk-FOXWPXSG.js";var t=n(a()),i=n(l());var o={visualizations:{viz_markdown:{type:"splunk.markdown",options:{markdown:`## Overview
+Markdown elements allow you to write markdown content on your dashboard. The text you are reading now is in fact part of a markdown element! You can write in the right editing panel and then preview on your dashboard. The following options are available:
+
+# Heading Level 1
+## Heading Level 2
+### Heading Level 3
+**Bold Text** 
+
+*Italic Text*
+
+[links](https://docs.splunk.com/Documentation)
+
+> quotes
+
+- first bullet item
+- second bullet item
+
+1. first item
+2. second item
+
+
+horizontal line:
+___
+
+\`Small Code Section\`
+\`\`\`
+//large code section
+"viz_markdown": {
+	"type": "splunk.markdown",
+	"options": {
+		"markdown": "## Overview\\nMarkdown elements allow you to write markdown content on your dashboard. The text you are reading now is in fact part of a markdown element! You can write in the right editing panel and then preview on your dashboard. The following options are available:\\n\\n# Heading Level 1\\n## Heading Level 2\\n### Heading Level 3\\n**Bold Text** \\n\\n*Italic Text*\\n\\n[links](www.splunk.com)\\n\\n> quotes\\n\\n- first bullet item\\n- second bullet item\\n\\n1. first item\\n2. second item\\n\\n\\nhorizontal line:\\n___\\n\\n\`Small Code Section\`\\n\`\`\`\\n//large code section\\n\\t\\t\\"viz_markdown\\": {\\n\\t\\t\\t\\"type\\": \\"splunk.markdown\\",\\n\\t\\t\\t\\"options\\": {\\n\\t\\t\\t\\t\\"markdown\\": \\"## Overview\\\\nMarkdown elements allow you to write markdown content on your dashboard. The text you are reading now is in fact part of a markdown element! You can write in the right editing panel and then preview on your dashboard. The following options are available:\\\\n\\\\n# Heading Level 1\\\\n## Heading Level 2\\\\n### Heading Level 3\\\\n**Bold Text** \\\\n\\\\n*Italic Text*\\\\n\\\\n[links](www.splunk.com)\\\\n\\\\n> quotes\\\\n\\\\n- first bullet item\\\\n- second bullet item\\\\n\\\\n1. first item\\\\n2. second item\\\\n\\\\n\\\\nhorizontal line:\\\\n___\\\\n\\\\n\`Small Code Section\`\\\\n\`\`\`\\\\n//large code section\\\\n\\\\\\"viz_markdown\\\\\\": {\\\\n\\\\t\\\\\\"type\\\\\\": \\\\\\"splunk.markdown\\\\\\",\\\\n\\\\t\\\\\\"options\\\\\\": {\\\\n\\\\t\\\\t\\\\\\"markdown\\\\\\": \\\\\\"## Overview\\\\\\\\nMarkdown elements allow you to write markdown content on your dashboard. The text you are reading now is in fact part of a markdown element! You can write in the right editing panel and then preview on your dashboard. The following options are available:\\\\\\\\n\\\\\\\\n# Heading Level 1\\\\\\\\n## Heading Level 2\\\\\\\\n### Heading Level 3\\\\\\\\n**Bold Text** \\\\\\\\n\\\\\\\\n*Italic Text*\\\\\\\\n\\\\\\\\n[links](www.splunk.com)\\\\\\\\n\\\\\\\\n> quotes\\\\\\\\n\\\\\\\\nhorizontal line:\\\\\\\\n___\\\\\\\\n\\\\\\\\n\`Small Code Section\`\\\\\\\\n\`\`\`\\\\\\\\n//large code section\\\\\\\\nviz_1:{\\\\\\\\n\\\\\\\\n\`\`\`\\\\\\\\n\\\\\\\\n\\\\\\"\\\\n\\\\t}\\\\n}\\\\n\`\`\`\\\\n\\\\n\\"\\n\\t\\t\\t}\\n\\t\\t}\\n\`\`\`\\n\\n![Image](https://www.splunk.com/content/dam/splunk2/images/logos/splunk/splunk-black-white-bg.png)\\n"
+	}
+}
+\`\`\`
+
+![Image](https://www.splunk.com/content/dam/splunk2/images/logos/splunk/splunk-black-white-bg.png)
+`,fontColor:"#ffffff"}},viz_ko8JSvNc:{type:"splunk.markdown",options:{markdown:"## Markdown Visualizations can also have different font colors and background colors, which apply to that entire panel and all the text within it. This can be configured from the UI editor of the markdown visualization. ",fontColor:"#dc4e41",backgroundColor:"#000000"}}},dataSources:{},defaults:{},inputs:{},layout:{type:"grid",options:{},structure:[{item:"viz_markdown",type:"block",position:{x:0,y:0,w:1440,h:1154}},{item:"viz_ko8JSvNc",type:"block",position:{x:0,y:1154,w:1440,h:235}}],globalInputs:[]},description:"Write Markdown in your dashboard to provide context and additional information",title:"Markdown"};(0,i.default)(t.default.createElement(e,{definition:o}),{pageTitle:"Markdown",hideFooter:!0,layout:"fixed"});
