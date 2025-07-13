6bd56381f0e352670cbbc759fdb157dcc119a44c
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 23:30:53 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/contrib/jquery.ui.datepicker/jquery.ui.datepicker-sr-SR.js
@@ -0,0 +1,23 @@
+ï»¿/* Serbian i18n for the jQuery UI date picker plugin. */
+/* Written by Dejan DimiÄ‡. */
+jQuery(function($){
+	$.datepicker.regional['sr-SR'] = {
+		closeText: 'Zatvori',
+		prevText: '&#x3c;',
+		nextText: '&#x3e;',
+		currentText: 'Danas',
+		monthNames: ['Januar','Februar','Mart','April','Maj','Jun',
+		'Jul','Avgust','Septembar','Oktobar','Novembar','Decembar'],
+		monthNamesShort: ['Jan','Feb','Mar','Apr','Maj','Jun',
+		'Jul','Avg','Sep','Okt','Nov','Dec'],
+		dayNames: ['Nedelja','Ponedeljak','Utorak','Sreda','ÄŒetvrtak','Petak','Subota'],
+		dayNamesShort: ['Ned','Pon','Uto','Sre','ÄŒet','Pet','Sub'],
+		dayNamesMin: ['Ne','Po','Ut','Sr','ÄŒe','Pe','Su'],
+		weekHeader: 'Sed',
+		dateFormat: 'dd/mm/yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+	$.datepicker.setDefaults($.datepicker.regional['sr-SR']);
+});
