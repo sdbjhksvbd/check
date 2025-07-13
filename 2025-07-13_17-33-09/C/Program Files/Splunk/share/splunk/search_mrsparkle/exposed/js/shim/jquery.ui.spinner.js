d05b97a0211ad5f626b1a1489376b34e6800ec67
sdbjhksvbd <s07575565@gmail.com>
2025-07-13 23:52:52 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/shim/jquery.ui.spinner.js
@@ -0,0 +1,7 @@
+define(['jquery',
+       'jquery.ui.widget',
+       'jquery.ui.core',
+       'jquery.ui.button',
+       'imports-loader?jQuery=jquery!jquery-ui/ui/widgets/spinner'], function($) {
+    return $;
+});
