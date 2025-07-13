5c192950031ec2c410ea0e9bba208a7d1068f63f
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 00:53:18 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files/Splunk/share/splunk/search_mrsparkle/exposed/js/views/shared/datasetcontrols/exploremenu/menu/ExploreList.js
@@ -0,0 +1,126 @@
+define(
+    [
+        'module',
+        'jquery',
+        'underscore',
+        'views/Base',
+        'splunk.util',
+        'uri/route'
+    ],
+    function(
+        module,
+        $,
+        _,
+        BaseView,
+        splunkUtil,
+        route
+    ) {
+        return BaseView.extend({
+            moduleId: module.id,
+            tagName: 'ul',
+            className: 'explore_actions',
+
+            initialize: function() {
+                BaseView.prototype.initialize.apply(this, arguments);
+            },
+
+            events: {
+                'click a.explore_link': function(e) {
+                    var linkname = e.currentTarget.innerHTML,
+                        type = e.currentTarget.getAttribute('data-type');
+                    splunkUtil.trackEvent({
+                        type: 'tableUi.interact',
+                        data: {
+                            action: linkname,
+                            type: type, 
+                            location: 'datasets listing'
+                        },
+                    });
+                }
+            },
+
+            render: function() {
+                var datasetName = this.model.dataset.getFromName(),
+                    fromType = this.model.dataset.getFromType(),
+                    fromQuery = this.model.dataset.getFromSearch(),
+                    selectedFieldsString = this.model.dataset.getSelectedFieldsString(),
+                    hasTimeField = this.model.dataset.hasField('_time'),
+
+                    appName = this.model.dataset.entry.acl.get('app'),
+                    datasetApp = _.find(this.collection.apps.models, function (app) {
+                        return app.entry.get('name') === appName;
+                    }),
+                    app = datasetApp && datasetApp.entry.content.get("visible") ? appName : this.options.alternateApp,
+                    openInApp = this.model.application.get("app"),
+                    searchLinkData = {
+                        q: fromQuery
+                    },
+                    pivotLinkData = {
+                        data: {
+                            dataset: datasetName,
+                            type: fromType
+                        }
+                    },
+                    extendSearchLink,
+                    extendPivotLink;
+
+                if (this.model.searchJob && !this.model.searchJob.isNew()) {
+                    if (hasTimeField) {
+                        pivotLinkData.data.windowedEarliest = this.model.searchJob.getWindowedEarliestTimeOrAllTime();
+                        pivotLinkData.data.windowedLatest = this.model.searchJob.getWindowedLatestTimeOrAllTime();
+                    }
+                    pivotLinkData.data.earliest = this.model.searchJob.getDispatchEarliestTime();
+                    pivotLinkData.data.latest = this.model.searchJob.getDispatchLatestTime();
+
+                    searchLinkData.sid = this.model.searchJob.id;
+                    searchLinkData.earliest = this.model.searchJob.getDispatchEarliestTime();
+                    searchLinkData.latest = this.model.searchJob.getDispatchLatestTime();
+                    searchLinkData.q = this.model.searchJob.getSearch();
+                }
+
+                extendPivotLink = route.pivot(
+                    this.model.application.get('root'),
+                    this.model.application.get('locale'),
+                    this.model.application.get('app'),
+                    pivotLinkData);
+
+                if (openInApp === "system") {
+                    openInApp = app;
+                }
+
+                if (selectedFieldsString) {
+                    searchLinkData['display.events.fields'] = selectedFieldsString;
+                }
+
+                extendSearchLink = route.search(
+                    this.model.application.get('root'),
+                    this.model.application.get('locale'),
+                    openInApp,
+                    {
+                        data: searchLinkData
+                    });
+
+                this.$el.html(this.compiledTemplate({
+                    type: this.model.dataset.getDatasetDisplayType(),
+                    canSearch: this.model.dataset.canSearch(),
+                    searchLink: extendSearchLink,
+                    canPivot: this.model.dataset.canPivot(),
+                    pivotLink: extendPivotLink
+                }));
+
+                return this;
+            },
+
+            template: '\
+                <% if (canPivot) { %>\
+                    <li><a class="explore_link" data-type="<%= type %>" href="<%= pivotLink %>">\
+                        <%- _("Visualize with Pivot").t() %></a></li>\
+                <% } %>\
+                <% if (canSearch) { %>\
+                    <li><a class="explore_link" data-type="<%= type %>" href="<%= searchLink %>">\
+                        <%- _("Investigate in Search").t() %></a></li>\
+                <% } %>\
+            '
+        });
+    }
+);
