7944d77970f0c5cafcc9a571ba37a599652eca4d
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:41 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/es-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"Mi equipo",SELECT_FILES:"Seleccionar archivos de mi equipo",NO_RECENT_FOLDERS_SELECT:"Haz clic en Examinar para abrir un archivo del equipo.",HEADING_RECENT_LOCATIONS:"Ubicaciones recientes",HEADING_SELECT_FROM_RECENT_FOLDER:"Seleccionar de la carpeta Recientesâ€¦",HEADING_SELECT_FOLDER:"Guardar en la carpeta Recientesâ€¦",BROWSE_BUTTON:"Examinar",BROWSE_DIALOG_BUTTON:"Examinar...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"Elegir otra carpeta...",BROWSE_FOLDER_TEXT:"Seleccionar una carpeta distinta",NO_RECENT_FOLDERS_SAVE_AS:"No hay carpetas recientes",NO_FOLDERS_MYCOMPUTER_PROMPT:"TodavÃ­a no hay archivos aquÃ­",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"Las carpetas que contengan los archivos que abras desde tu equipo aparecerÃ¡n aquÃ­."});
