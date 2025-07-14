0e329d03ccb62b749d406c07490116e26589a18b
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:10:06 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/tracked-send/js/viewer/nls/root/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({APP_NAME:"VIEWER",UNSUPPORTED_BROWSER_TITLE_2:"Browser Unsupported",UNSUPPORTED_BROWSER_MSG_2:"You are running an unsupported version of Internet Explorer. To view shared file(s), you must upgrade to the <a class='dialog-link' href='<%= ie %>'>latest version of Internet Explorer</a>, or use the latest version of <a class='dialog-link' href='<%= chrome %>'>Google Chrome</a>, <a class='dialog-link' href='<%= safari %>'>Safari</a> or <a class='dialog-link' href='<%= firefox %>'>Firefox</a>."});
