d30b2a188090d657cf86439d8f3b3b8872d53049
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:44 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/fi-fi/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"Oma tietokone",SELECT_FILES:"Valitse tiedostot Omasta tietokoneesta",NO_RECENT_FOLDERS_SELECT:"Avaa tiedosto tietokoneestasi napsauttamalla 'Selaa'.",HEADING_RECENT_LOCATIONS:"Ã„skeiset sijainnit",HEADING_SELECT_FROM_RECENT_FOLDER:"Valitse Ã¤skeisestÃ¤ kansiosta...",HEADING_SELECT_FOLDER:"Tallenna Ã¤skeiseen kansioon...",BROWSE_BUTTON:"Selaa",BROWSE_DIALOG_BUTTON:"Selaa...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"Valitse toinen kansio...",BROWSE_FOLDER_TEXT:"Valitse toinen kansio",NO_RECENT_FOLDERS_SAVE_AS:"Ei Ã¤skeisiÃ¤ kansioita",NO_FOLDERS_MYCOMPUTER_PROMPT:"TÃ¤Ã¤llÃ¤ ei vielÃ¤ ole tiedostoja.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"Kun avaat tiedostoja tietokoneelta, nÃ¤et niiden kansiot tÃ¤Ã¤llÃ¤."});
