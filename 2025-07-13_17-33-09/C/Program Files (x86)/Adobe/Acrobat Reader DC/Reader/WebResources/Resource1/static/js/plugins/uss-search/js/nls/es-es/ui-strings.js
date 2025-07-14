2fed53df1bb824df86135c80d2455290f892c40a
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:04 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/es-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"Resultados de bÃºsqueda para <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Cerrar",SEARCH_CLOSE_LABEL:"Cerrar bÃºsqueda",SEARCH_SIGNOUT_PROMPT:"Para buscar otras categorÃ­as, <%= prefix %>inicie sesiÃ³n.<%= suffix %>",SEARCH_SIGNIN:"Iniciar sesiÃ³n",SEARCH_OFFLINE_PROMPT:"No se ha podido completar la solicitud. Se ha producido un error al conectar con el servicio. <%= prefix %>IntÃ©ntalo de nuevo mÃ¡s tarde.<%= suffix %>",SEARCH_ONLINE:"Reintentar",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"No se han encontrado resultados",SEARCH_NO_RESULTS_MESSAGE:"No hemos encontrado ningÃºn resultado para <b><%= searchQuery %></b>.<br> Comprueba la ortografÃ­a o intenta ampliar el Ã¡mbito de la bÃºsqueda.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"No se han encontrado resultados en <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"No hemos encontrado ningÃºn resultado para <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"Se ha producido un error",SEARCH_CATEGORY_ERROR:"No hemos podido buscar esta categorÃ­a.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"Resultados de la bÃºsqueda (<%= category %>) para <%= query %>: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"Resultados de la bÃºsqueda (<%= category %>): <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"No hemos podido buscar esta categorÃ­a. IntÃ©ntalo de nuevo mÃ¡s tarde.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
