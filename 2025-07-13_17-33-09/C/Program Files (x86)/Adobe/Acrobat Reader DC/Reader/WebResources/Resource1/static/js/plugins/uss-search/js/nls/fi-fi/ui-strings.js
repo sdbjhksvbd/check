71f86bd4ea3439f8acc7c9a354f86b6f1db70b2c
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:07 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/fi-fi/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"Kyselyn <b><%= searchQuery %></b> hakutulokset",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Sulje",SEARCH_CLOSE_LABEL:"Sulje haku",SEARCH_SIGNOUT_PROMPT:"Jos haluat tehdÃ¤ haun muista luokista, <%= prefix %>kirjaudu sisÃ¤Ã¤n.<%= suffix %>",SEARCH_SIGNIN:"Kirjaudu sisÃ¤Ã¤n",SEARCH_OFFLINE_PROMPT:"PyyntÃ¶Ã¤si ei voitu suorittaa. Palveluun yhteyttÃ¤ muodostettaessa tapahtui virhe. <%= prefix %>YritÃ¤ uudelleen.<%= suffix %>",SEARCH_ONLINE:"YritÃ¤ uudelleen",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Tuloksia ei lÃ¶ytynyt.",SEARCH_NO_RESULTS_MESSAGE:"KyselyllÃ¤ <b><%= searchQuery %></b> ei lÃ¶ytynyt tuloksia.<br> Tarkista oikeinkirjoitus tai yritÃ¤ laajentaa hakua.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Luokasta <%= category %> ei lÃ¶ytynyt tuloksia.",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"KyselyllÃ¤ <b><%= searchQuery %></b> ei lÃ¶ytynyt tuloksia.",SEARCH_NO_RESULTS_ERROR:"Jokin meni vikaan.",SEARCH_CATEGORY_ERROR:"Emme pystyneet tekemÃ¤Ã¤n hakua tÃ¤stÃ¤ luokasta.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> Kyselyn <%= query %> hakutulokset: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> Hakutulokset: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Emme pystyneet tekemÃ¤Ã¤n hakua tÃ¤stÃ¤ luokasta. YritÃ¤ myÃ¶hemmin uudelleen.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
