2543ac469c2572d92122a9b194708ab10a8c3b69
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:37 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/en-gb/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"My Computer",SELECT_FILES:"Choose Files from My Computer",NO_RECENT_FOLDERS_SELECT:"Click 'Browse' to open a file from your computer.",HEADING_RECENT_LOCATIONS:"Recent Locations",HEADING_SELECT_FOLDER:"Select Folder",BROWSE_BUTTON:"Browse",BROWSE_FOLDER_TEXT:"Select a different folder",NO_RECENT_FOLDERS_SAVE_AS:"No Recent Folders"});
