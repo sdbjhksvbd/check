772dbb057fac4f0438ce0afa87b9476dc6900cc8
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:43 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/tr-tr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"Taramalar",SCAN_FILES_TITLE:"Ãœcretsiz Adobe Scan uygulamasÄ±nÄ± edinin",SCAN_FILES_READER_CONTENT:"DilediÄŸiniz her ÅŸeyi PDF'ye taramak iÃ§in telefonunuzu veya tabletinizi taÅŸÄ±nabilir bir tarayÄ±cÄ±ya dÃ¶nÃ¼ÅŸtÃ¼rÃ¼n. Hemen indirin.",SCAN_FILES_SIGN_IN_CONTENT:"Zaten Adobe Scan kullanÄ±cÄ±sÄ± mÄ±sÄ±nÄ±z? TaramalarÄ±nÄ±za eriÅŸmek iÃ§in oturum aÃ§Ä±n.",SCAN_FILES_CONTENT:"DilediÄŸiniz her ÅŸeyi PDF'ye taramak iÃ§in telefonunuzu veya tabletinizi taÅŸÄ±nabilir bir tarayÄ±cÄ±ya dÃ¶nÃ¼ÅŸtÃ¼rÃ¼n. Hemen indirin.",NO_SCAN_TEXT:"KullanÄ±labilir taramanÄ±z yok.",SCAN_FILES_QR_CODE_TITLE:"Ãœcretsiz Adobe Scan uygulamasÄ±nÄ± edinin",SCAN_FILES_QR_CODE_CONTENT:"Mobil aygÄ±tÄ±nÄ±zda dilediÄŸinizi PDF'ye dÃ¶nÃ¼ÅŸtÃ¼rmek iÃ§in QR kodunu tarayÄ±n ve ardÄ±ndan taranmÄ±ÅŸ PDF'lerinize buradan eriÅŸin."});
