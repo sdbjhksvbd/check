57626341d522c23e99de9ec1b147847e39fffd3f
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:51:35 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-files/js/nls/hr-hr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({FOOTER_ANCHOR:"Idi na web-mjesto Adobe Document Cloud",NOTIFICATION_HEADING:"<%= cloudName %> Datoteke",CLOUD_NAME:"SpremiÅ¡te u oblaku tvrtke Adobe",UNSUPPORTED_CONNECTOR_MSG:"<%= actionName %> trenutno nije podrÅ¾ano za datoteke spremljene u <%= connectorLabel %>."});
