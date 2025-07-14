8e7d5cdd4b31ac602376b07ad43552fe96c1033c
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:15 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/hu-hu/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"VÃ¡rakozik...",OAUTHDIALOG_2:"Megnyitottunk egy Ãºj bÃ¶ngÃ©szÅ‘lapot, ahol bejelentkezhet.",OAUTHDIALOG_3:"A hitelesÃ­tÃ©s bÃ¶ngÃ©szÅ‘ben valÃ³ elvÃ©gzÃ©se utÃ¡n a munkafolyamat automatikusan az alkalmazÃ¡sban folytatÃ³dik.",OAUTHDIALOG_TROUBLE:"ProblÃ©mÃ¡t tapasztal bejelentkezÃ©skor?",OAUTHDIALOG_TIMEOUT_1:"Sikertelen bejelentkezÃ©s. PrÃ³bÃ¡lja Ãºjra.",CANCEL_BUTTON_TEXT:"MÃ©gse",CANCEL_BUTTON_TEXT_2:"BezÃ¡rÃ¡s",OAUTHDIALOG_HEADING_2:"Sikertelen bejelentkezÃ©s"});
