7e2ea2a43c6884fb3dcb9da39e6589b36babf9f7
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:03 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/nl-nl/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Selecteer een gevoeligheidslabel",CANCEL_BUTTON_TEXT:"Annuleren",UPDATE_BUTTON_TEXT:"Bijwerken",APPLY_BUTTON_TEXT:"Toepassen",BACK_BUTTON_TEXT:"Vorige",SUBMIT_BUTTON_TEXT:"Verzenden",SAVE_BUTTON_TEXT:"Opslaan",ADD_BUTTON_TEXT:"Toevoegen",JUSTIFICATION_REQUIRED:"Verantwoording vereist",JUSTIFICATION_HEAD_TEXT:"Uw organisatie vereist verantwoording voor wijziging van het gevoeligheidslabel.",JUSTIFICATION_OPTION_1:"Het vorige label is niet meer geldig",JUSTIFICATION_OPTION_2:"Het vorige label was onjuist",JUSTIFICATION_OPTION_OTHER:"Anders (uitleg geven) - Geen gevoelige gegevens invoeren",ENTER_JUSTIFICATION:"Verantwoording invoeren",DELETE_LABEL:"Label verwijderen",LABEL_DETAILS_TOOLTIP:"Details gevoeligheidslabel",SPECIFY_USER_OR_GROUP:"Gebruiker, groep of domein specificeren",DEFINE_LABEL_PERMISSION:"Labeltoestemming definiÃ«ren",MORE_OPTIONS:"Meer opties",CAN_READ:"Kunnen lezen",CAN_CHANGE:"Kunnen wijzigen",CAN_FULL_CONTROL:"Kunnen volledig beheren",READ:"Lezen",READ_COPY:"Lezen (kan kopiÃ«ren)",READ_PRINT:"Lezen (kan afdrukken)",READ_COPY_PRINT:"Lezen (kan kopiÃ«ren & printen)",CHANGE:"Wijzigen",CHANGE_PRINT:"Wijzigen (kunnen afdrukken)",FULL_CONTROL:"Volledig beheren",DELETE:"Verwijderen",CUSTOM:"Aangepast",OWNER:"Eigenaar",OWNER_SIGNAL:"(u)",DOCUMENT_EXPIRY:"Dit document verloopt op ",READ_PERMISSION_TEXT:"Mensen met toestemming voor lezen kunnen ook",COPY_CHECKBOX:"Dit document kopiÃ«ren",CHANGE_PERMISSION_TEXT:"Mensen met toestemming voor lezen of wijzigen kunnen ook",PRINT_CHECKBOX:"Dit document afdrukken",MMDDYYYY_PLACEHOLDER:"dd/mm/yyyy",EXPIRY_DATE_ERROR:"Een geldige verloopdatum invoeren",UDP_EDIT_LABEL:"Labelmachtigingen bewerken",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"ONTVANGERS TOEGEVOEGD",GET_MS_GRAPH_CONTACTS_MSG:"Meld u aan met uw Office-account voor toegang tot Outlook-contactpersonen.",GET_MS_GRAPH_CONTACTS_MSG_1:"Aanmelden met uw ",GET_MS_GRAPH_CONTACTS_MSG_2:" account voor toegang ",GET_MS_GRAPH_CONTACTS_MSG_3:" contactpersonen.",EMAIL_REMOVE:"E-mail verwijderen"});
