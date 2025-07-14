97a6b440cb27fb5da964bb0a92f639b9c2b23b95
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:23 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/pt-br/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Aguardando...",OAUTHDIALOG_2:"Abrimos uma nova guia do navegador para vocÃª fazer logon.",OAUTHDIALOG_3:"Ao terminar a autenticaÃ§Ã£o no navegador, vocÃª continuarÃ¡ automaticamente no aplicativo.",OAUTHDIALOG_TROUBLE:"Problemas ao se conectar?",OAUTHDIALOG_TIMEOUT_1:"NÃ£o foi possÃ­vel conectar vocÃª. Tente novamente.",CANCEL_BUTTON_TEXT:"Cancelar",CANCEL_BUTTON_TEXT_2:"Fechar",OAUTHDIALOG_HEADING_2:"Falha ao fazer logon"});
