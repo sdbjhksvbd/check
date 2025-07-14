aab10922d8419decfcd00cf6ec83eeb845cd387b
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:19 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/nb-no/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"SÃ¸keresultater for <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Lukk",SEARCH_CLOSE_LABEL:"Lukk  SÃ¸k",SEARCH_SIGNOUT_PROMPT:"<%= prefix %>Logg pÃ¥<%= suffix %> for Ã¥ sÃ¸ke i andre kategorier.",SEARCH_SIGNIN:"Logg pÃ¥",SEARCH_OFFLINE_PROMPT:"Kunne ikke fullfÃ¸re forespÃ¸rselen. Det oppstod en feil under tilkobling til tjenesten. <%= prefix %>PrÃ¸v pÃ¥ nytt.<%= suffix %>",SEARCH_ONLINE:"PrÃ¸v pÃ¥ nytt",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Fant ingen resultater",SEARCH_NO_RESULTS_MESSAGE:"Vi fant ingen resultater for <b><%= searchQuery %></b>.<br> Kontroller stavemÃ¥ten eller forsÃ¸k Ã¥ utvide sÃ¸ket.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Fant ingen resultater i <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Vi fant ingen resultater for <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"Noe gikk galt",SEARCH_CATEGORY_ERROR:"Vi fikk ikke sÃ¸kt i denne kategorien.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> SÃ¸keresultater for <%= query %>: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> SÃ¸keresultater: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Vi fikk ikke sÃ¸kt i denne kategorien. PrÃ¸v pÃ¥ nytt senere.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
