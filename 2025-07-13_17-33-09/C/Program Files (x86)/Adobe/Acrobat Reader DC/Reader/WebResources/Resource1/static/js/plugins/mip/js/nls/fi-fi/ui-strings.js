47db85c48fd1adf7eae3c18d403fc89422ffadb1
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:48:54 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/fi-fi/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Valitse herkkyysmerkintÃ¤",CANCEL_BUTTON_TEXT:"Peruuta",UPDATE_BUTTON_TEXT:"PÃ¤ivitÃ¤",APPLY_BUTTON_TEXT:"KÃ¤ytÃ¤",BACK_BUTTON_TEXT:"Takaisin",SUBMIT_BUTTON_TEXT:"LÃ¤hetÃ¤",SAVE_BUTTON_TEXT:"Tallenna",ADD_BUTTON_TEXT:"LisÃ¤Ã¤",JUSTIFICATION_REQUIRED:"Syy annettava",JUSTIFICATION_HEAD_TEXT:"Organisaatiosi vaatii herkkyyden selitteen muutoksen syyn.",JUSTIFICATION_OPTION_1:"Edellinen selite ei enÃ¤Ã¤ pÃ¤de",JUSTIFICATION_OPTION_2:"Edellinen selite oli virheellinen",JUSTIFICATION_OPTION_OTHER:"Muu syy (selitÃ¤) â€“ Ã„lÃ¤ anna arkaluonteisia tietoja",ENTER_JUSTIFICATION:"Anna syy",DELETE_LABEL:"Poista selite",LABEL_DETAILS_TOOLTIP:"Herkkyyden selitteen tiedot",SPECIFY_USER_OR_GROUP:"MÃ¤Ã¤ritÃ¤ kÃ¤yttÃ¤jÃ¤, ryhmÃ¤ tai toimialue",DEFINE_LABEL_PERMISSION:"MÃ¤Ã¤ritÃ¤ selitteen kÃ¤yttÃ¶oikeus",MORE_OPTIONS:"LisÃ¤Ã¤ vaihtoehtoja",CAN_READ:"Voi lukea",CAN_CHANGE:"Voi muuttaa",CAN_FULL_CONTROL:"Voi hallita tÃ¤ysin",READ:"Luku",READ_COPY:"Luku (voi kopioida)",READ_PRINT:"Luku (voi tulostaa)",READ_COPY_PRINT:"Luku (voi kopioida ja tulostaa)",CHANGE:"Muutos",CHANGE_PRINT:"Muutos (voi tulostaa)",FULL_CONTROL:"TÃ¤ydellinen hallinta",DELETE:"Poista",CUSTOM:"Mukautettu",OWNER:"Omistaja",OWNER_SIGNAL:"(sinÃ¤)",DOCUMENT_EXPIRY:"TÃ¤mÃ¤ dokumentti vanhentuu ",READ_PERMISSION_TEXT:"Ihmiset, joilla on lukuoikeudet, voivat suorittaa myÃ¶s seuraavan toiminnon:",COPY_CHECKBOX:"Kopioi tÃ¤mÃ¤ dokumentti",CHANGE_PERMISSION_TEXT:"Ihmiset, joilla on luku- tai muutosoikeudet, voivat suorittaa myÃ¶s seuraavan toiminnon:",PRINT_CHECKBOX:"Tulosta tÃ¤mÃ¤ dokumentti",MMDDYYYY_PLACEHOLDER:"kk/pp/vvvv",EXPIRY_DATE_ERROR:"Anna kelvollinen vanhentumispÃ¤ivÃ¤",UDP_EDIT_LABEL:"Muokkaa tunnuksen kÃ¤yttÃ¶oikeuksia",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"LISÃ„TYT VASTAANOTTAJAT",GET_MS_GRAPH_CONTACTS_MSG:"Kirjaudu sisÃ¤Ã¤n Office-tilisi tunnistetiedoilla, jotta voit kÃ¤yttÃ¤Ã¤ Outlookin yhteystietoja.",GET_MS_GRAPH_CONTACTS_MSG_1:"Kirjaudu sisÃ¤Ã¤n ",GET_MS_GRAPH_CONTACTS_MSG_2:" -tilisi tunnistetiedoilla, jotta voit kÃ¤yttÃ¤Ã¤ sovelluksen ",GET_MS_GRAPH_CONTACTS_MSG_3:" yhteystietoja.",EMAIL_REMOVE:"Poista sÃ¤hkÃ¶posti"});
