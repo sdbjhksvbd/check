20f090e303e130abc50ed1c6507b8b226692db1d
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:11 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/fr-fr/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"En attente...",OAUTHDIALOG_2:"Nous avons ouvert un nouvel onglet de navigateur pour que vous puissiez vous connecter.",OAUTHDIALOG_3:"Une fois lâ€™authentification dans le navigateur terminÃ©e, vous reviendrez automatiquement dans lâ€™application.",OAUTHDIALOG_TROUBLE:"Un problÃ¨me pour vous connecterÂ ?",OAUTHDIALOG_TIMEOUT_1:"Nous n'avons pas rÃ©ussi Ã  vous connecter. Veuillez rÃ©essayer.",CANCEL_BUTTON_TEXT:"Annuler",CANCEL_BUTTON_TEXT_2:"Fermer",OAUTHDIALOG_HEADING_2:"Ã‰chec de la connexion"});
