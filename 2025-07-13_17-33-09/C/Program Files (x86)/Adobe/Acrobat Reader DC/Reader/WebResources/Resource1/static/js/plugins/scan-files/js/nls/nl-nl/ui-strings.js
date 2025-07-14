10c43e7e8ae19a5b70363259d78546dbe77ddf2d
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:33 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/nl-nl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"Scans",SCAN_FILES_TITLE:"Download de gratis Adobe Scan-app",SCAN_FILES_READER_CONTENT:"Maak een draagbare scanner van uw telefoon of mobiel en scan alles wat u wilt naar een PDF. Download de app nu.",SCAN_FILES_SIGN_IN_CONTENT:"Bent u al een Adobe Scan-gebruiker? Meld u aan om toegang tot uw scans te krijgen.",SCAN_FILES_CONTENT:"Maak een draagbare scanner van uw telefoon of mobiel en scan alles wat u wilt naar een PDF. Download de app nu.",NO_SCAN_TEXT:"U hebt geen beschikbare scans.",SCAN_FILES_QR_CODE_TITLE:"Download de gratis Adobe Scan-app",SCAN_FILES_QR_CODE_CONTENT:"Scan de QR-code om alle documenten op uw mobiele apparaat om te zetten in een PDF en open de gescande PDF's hier."});
