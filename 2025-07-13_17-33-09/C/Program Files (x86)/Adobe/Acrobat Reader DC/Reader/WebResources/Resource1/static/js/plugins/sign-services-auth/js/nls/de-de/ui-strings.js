272e35c64c8ca886f8664c5df684443e487e1cca
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:07:50 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/sign-services-auth/js/nls/de-de/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SIGNIN_REQUIRED:"Anmeldung erforderlich",PRELOGIN_DIALOG_BODY_DEFAULT:"Bitte melde dich erneut an. Dies dient zum Schutz deiner unterschriebenen Vereinbarungen.",PRELOGIN_DIALOG_BODY_DOCTAB:"Wenn du auf â€žAbbrechenâ€œ klickst, werden alle offenen Vereinbarungen zum Unterschreiben geschlossen.",CHECKBOX_TEXT:"FÃ¼r die nÃ¤chsten 14 Tage angemeldet bleiben",CANCEL:"Abbrechen",NEXT:"Weiter",SIGOUT_DIALOG_TITLE:"Melde dich von Adobe Acrobat Reader ab",SIGOUT_DIALOG_DESCRIPTION_1:'Du bist derzeit mit deiner Adobe ID <strong>"<%=emailId%>"</strong> angemeldet. Durch Klicken auf â€žAbmeldenâ€œ meldest du dich von der Anwendung auf diesem GerÃ¤t ab. Diese App fordert dich mÃ¶glicherweise zum SchlieÃŸen auf. Die App wird nicht deinstalliert.',SIGOUT_DIALOG_DESCRIPTION_2:"Diese Anwendung und Cloud-Ressourcen, die mit deiner Adobe ID verknÃ¼pft sind, sind verfÃ¼gbar, wenn du dich erneut anmeldest.",SIGOUT_DIALOG_CANCEL_BTN:"Abbrechen",SIGOUT_DIALOG_SIGNOUT_BTN:"Abmelden"});
