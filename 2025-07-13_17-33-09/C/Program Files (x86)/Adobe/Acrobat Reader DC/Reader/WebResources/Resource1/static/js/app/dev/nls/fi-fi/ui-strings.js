fa3d9ac5d20a7025c6b04509ec462c26fef3018a
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:36:37 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/app/dev/nls/fi-fi/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({COPYRIGHT:"Â©2012-2015 Adobe Systems Incorporated, All Rights Reserved.",HELP:"Ohje",PRIVACY:"Tietosuoja",TERMS:"KÃ¤yttÃ¶ehdot",COOKIES:"EvÃ¤steet",PRIVACY_POLICY:"TietosuojakÃ¤ytÃ¤ntÃ¶ ja evÃ¤steet",TERMS_OF_USE:"KÃ¤yttÃ¶ehdot",WELCOME_MSG:"Adobe Document Services -palvelujen avulla voit kÃ¤sitellÃ¤ tiedostojasi millÃ¤ tahansa laitteella",LOAD_FAILURES:"Lataa epÃ¤onnistuneet",MODULES_NOT_LOADED:"Seuraavien moduulien lataus epÃ¤onnistui:<br/><br/><%= modules %>",SIGN_IN:"Kirjaudu",CREATE_ACCOUNT:"Luo tili",FORGOT_PASSWORD:"Ongelmia kirjauduttaessa?",EMAIL_VERIFICATION:"SÃ¤hkÃ¶postin vahvistus",PDF_SERVICES:"Â Â /Â Â PDF-palvelut",ACCOUNT:"Tili",LINK_CREATIVE_CLOUD_ACCOUNT:"LinkitÃ¤ Creative Cloud -tili...",LINK_DROPBOX_ACCOUNT:"LinkitÃ¤ Dropbox-tili...",MANAGE_SUB:"Oma sopimus...",USER_ACCOUNT:"KÃ¤yttÃ¤jÃ¤tili...",ASK_US_A_QUESTION:"Tee meille kysymys...",SUPPORT:"Tuki...",CHAT_WITH_US:"Keskustele kanssamme...",ABOUT_APP:"Tietoja Document Cloudista...",SIGN_OUT:"Kirjaudu ulos",SHOW_PROGRESS:"NÃ¤ytÃ¤ tilanne",PROGRESS_COMPLETE:"Tilanne: valmis",REMOVE_HISTORY:"Poista historia",OPERATION_IN_PROGRESS_DLG_TITLE:"Toiminto meneillÃ¤Ã¤n",OPERATION_IN_PROGRESS_DLG_MSG:"Toiminto on meneillÃ¤Ã¤n. Jatkaminen peruuttaa toiminnon. Haluatko jatkaa?",QUOTA_LEGEND:"<%= using %>/<%= allotment %>",STORAGE_EXCEEDED_LEARN_MORE:"Tallennustila ylitettiin (<a class='quota-display-learn-more'>LisÃ¤tietoja</a>)",ABOUT_DLG_TITLE:"Tietoja Document Cloudista",ABOUT_DLG_COPYRIGHT_PARA:"Copyright Â© 2012-2015 Adobe Systems Incorporated and its licensors. All Rights Reserved.",ABOUT_DLG_TRADEMARK_PARA:"Adobe, the Adobe logo, and Acrobat are either registered trademarks or trademarks of Adobe Systems Incorporated in the United States and/or other countries. All other trademarks are the property of their respective owners.",ABOUT_DLG_PORTIONS_PARA:"Joissakin osissa kÃ¤ytÃ¶ssÃ¤:",ABOUT_DLG_MICROSOFT_PARA:"Microsoft Windows Media Technologies. Copyright Â© 1999-2002, 2006 Microsoft Corporation. All Rights Reserved.",ABOUT_DLG_IRIS_PARA:"OCR technology by Image Recognition Integrated Systems S.A. Copyright Â© 1987 - 2012, I.R.I.S. S.A. All rights reserved.",ABOUT_DLG_SOLID_PARA:"Copyright Â© 2003-2011 Solid Documents, LLC",ABOUT_DLG_THIRD_PARTY_PARA:"Kolmansien osapuolien ilmoitukset ja niiden ohjelmistojen ehdot (<%= thirdPartyLink %>) on liitetty tÃ¤hÃ¤n tiedoksi.",ABOUT_DLG_BUILD_NUMBER:"Koontiversio: <%= buildNum %>",NO_CONNECTION_TITLE:"Ei Internet-yhteyttÃ¤",NO_CONNECTION_MSG:"TÃ¤mÃ¤n alueen ominaisuudet edellyttÃ¤vÃ¤t Internet-yhteyttÃ¤. YhdistÃ¤ tietokone Internetiin ja yritÃ¤ uudelleen.",NETWORK_FAILURE:"Verkkoyhteysvirhe",NETWORK_DISCONNECTED:"VerkkoyhteyttÃ¤ ei ilmeisesti ole. Reader ei ehkÃ¤ toimi tÃ¤ydellisesti ilman verkkoyhteyttÃ¤."});
