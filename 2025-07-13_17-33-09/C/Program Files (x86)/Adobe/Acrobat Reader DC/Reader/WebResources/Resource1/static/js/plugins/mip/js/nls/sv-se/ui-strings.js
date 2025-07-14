fdf72eeb7080836bc99ffe6e5f5d1a70048a20c0
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:10 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/sv-se/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"VÃ¤lj en kÃ¤nslighetsetikett",CANCEL_BUTTON_TEXT:"Avbryt",UPDATE_BUTTON_TEXT:"Uppdatera",APPLY_BUTTON_TEXT:"AnvÃ¤nd",BACK_BUTTON_TEXT:"BakÃ¥t",SUBMIT_BUTTON_TEXT:"Skicka",SAVE_BUTTON_TEXT:"Spara",ADD_BUTTON_TEXT:"LÃ¤gg till",JUSTIFICATION_REQUIRED:"Motivering krÃ¤vs",JUSTIFICATION_HEAD_TEXT:"Din organisation krÃ¤ver en motivering fÃ¶r att Ã¤ndra kÃ¤nslighetsmÃ¤rkning.",JUSTIFICATION_OPTION_1:"Den tidigare mÃ¤rkningen gÃ¤ller inte lÃ¤ngre",JUSTIFICATION_OPTION_2:"FÃ¶regÃ¥ende mÃ¤rkning var felaktig",JUSTIFICATION_OPTION_OTHER:"Annat (fÃ¶rklara) â€“ ange inte kÃ¤nsliga uppgifter",ENTER_JUSTIFICATION:"Ange motivering",DELETE_LABEL:"Ta bort etikett",LABEL_DETAILS_TOOLTIP:"Information om kÃ¤nslighetsmÃ¤rkning",SPECIFY_USER_OR_GROUP:"Ange anvÃ¤ndare, grupp eller domÃ¤n",DEFINE_LABEL_PERMISSION:"Definiera behÃ¶righet fÃ¶r mÃ¤rkning",MORE_OPTIONS:"Fler alternativ",CAN_READ:"Kan lÃ¤sa",CAN_CHANGE:"Kan Ã¤ndras",CAN_FULL_CONTROL:"Kan ha full kontroll",READ:"LÃ¤st",READ_COPY:"LÃ¤s (kan kopieras)",READ_PRINT:"LÃ¤s (kan skrivas ut)",READ_COPY_PRINT:"LÃ¤s (kan kopieras och skrivas ut)",CHANGE:"Ã„ndra",CHANGE_PRINT:"Ã„ndra (kan skriva ut)",FULL_CONTROL:"Full kontroll",DELETE:"Ta bort",CUSTOM:"Eget",OWNER:"Ã„gare",OWNER_SIGNAL:"(du)",DOCUMENT_EXPIRY:"Detta dokument upphÃ¶r att gÃ¤lla den ",READ_PERMISSION_TEXT:"Personer med lÃ¤sbehÃ¶righet fÃ¥r ocksÃ¥ gÃ¶ra det",COPY_CHECKBOX:"Kopiera det hÃ¤r dokumentet",CHANGE_PERMISSION_TEXT:"Personer med lÃ¤s- eller Ã¤ndringsbehÃ¶righet fÃ¥r ocksÃ¥ gÃ¶ra det",PRINT_CHECKBOX:"Skriv ut det hÃ¤r dokumentet",MMDDYYYY_PLACEHOLDER:"mm/dd/Ã¥Ã¥Ã¥Ã¥",EXPIRY_DATE_ERROR:"Ange ett giltigt utgÃ¥ngsdatum",UDP_EDIT_LABEL:"Redigera etikettbehÃ¶righeter",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"TILLAGDA MOTTAGARE",GET_MS_GRAPH_CONTACTS_MSG:"Logga in med ditt Office-konto fÃ¶r att komma Ã¥t Outlook-kontakter.",GET_MS_GRAPH_CONTACTS_MSG_1:"Logga in med ditt ",GET_MS_GRAPH_CONTACTS_MSG_2:" konto fÃ¶r att komma Ã¥t ",GET_MS_GRAPH_CONTACTS_MSG_3:" kontakter.",EMAIL_REMOVE:"Ta bort e-post"});
