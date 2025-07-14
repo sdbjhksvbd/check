959e45c7a68c72c498b698ac32b83aded4fd4904
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:51 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/ro-ro/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"FiÈ™iere <%= connectorLabel %>",GENERIC_ERROR_TITLE:"Eroare",GENERIC_ERROR_MSG:"A apÄƒrut o eroare la preluarea fiÅŸierelor.",CONNECTOR_FOOTER_ANCHOR_TEXT:"AccesaÈ›i site-ul web <%= connectorDisplayName %>",ACCOUNT_REMOVED_MESSAGE:"Acrobat nu vÄƒ poate accesa contul <%= connectorDisplayName %>",ACCESS_REMOVED_ACTION:"ConectaÈ›i-vÄƒ la contul dvs. <%= connectorDisplayName %> pentru a vÄƒ accesa fiÈ™ierele.",ACCESS_REMOVED_CTA:"Conectare",CONNECTOR_NOTIFICATION_HEADING:"FiÅŸiere <%= connectorDisplayName %>",FETCHING_TRANSCRIPTS:"Vi se obÈ›in transcrierile..."});
