89f2cb7099cf6ade16f03ed074203a3fd4f069b3
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:33 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/tr-tr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"<b><%= searchQuery %></b> iÃ§in arama sonuÃ§larÄ±",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Kapat",SEARCH_CLOSE_LABEL:"AramayÄ±  KapatÄ±n",SEARCH_SIGNOUT_PROMPT:"DiÄŸer kategorilerde arama yapmak iÃ§in lÃ¼tfen <%= prefix %>oturum aÃ§Ä±n.<%= suffix %>",SEARCH_SIGNIN:"Oturum AÃ§",SEARCH_OFFLINE_PROMPT:"Talebiniz tamamlanamadÄ±. Hizmete baÄŸlanÄ±rken bir hata oluÅŸtu. LÃ¼tfen <%= prefix %>tekrar deneyin.<%= suffix %>",SEARCH_ONLINE:"Yeniden Dene",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"SonuÃ§ bulunamadÄ±",SEARCH_NO_RESULTS_MESSAGE:"<b><%= searchQuery %></b> iÃ§in hiÃ§bir sonuÃ§ bulamadÄ±k.<br>YazÄ±mÄ± kontrol edin veya aramanÄ±zÄ± geniÅŸletmeyi deneyin.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"<%= category %> kategorisinde sonuÃ§ bulunamadÄ±",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"<b><%= searchQuery %></b> iÃ§in hiÃ§bir sonuÃ§ bulamadÄ±k.",SEARCH_NO_RESULTS_ERROR:"Bir sorun oluÅŸtu",SEARCH_CATEGORY_ERROR:"Bu kategoride arama yapÄ±lamadÄ±.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> <%= query %> sorgusu iÃ§in sonuÃ§larda ara: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> Arama sonuÃ§larÄ±: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Bu kategoride arama yapÄ±lamadÄ±. LÃ¼tfen daha sonra tekrar deneyin.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
