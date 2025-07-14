4164afd2807948c8d93b87d248a137bcf1a4a299
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:11 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/tr-tr/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Hassasiyet etiketi seÃ§in",CANCEL_BUTTON_TEXT:"Ä°ptal",UPDATE_BUTTON_TEXT:"GÃ¼ncelle",APPLY_BUTTON_TEXT:"Uygula",BACK_BUTTON_TEXT:"Geri",SUBMIT_BUTTON_TEXT:"GÃ¶nder",SAVE_BUTTON_TEXT:"Kaydet",ADD_BUTTON_TEXT:"Ekle",JUSTIFICATION_REQUIRED:"Neden gerekli",JUSTIFICATION_HEAD_TEXT:"KuruluÅŸunuz, hassasiyet etiketini deÄŸiÅŸtirmenizin nedenini talep ediyor.",JUSTIFICATION_OPTION_1:"Ã–nceki etiket artÄ±k geÃ§erli deÄŸil",JUSTIFICATION_OPTION_2:"Ã–nceki etiket yanlÄ±ÅŸ",JUSTIFICATION_OPTION_OTHER:"DiÄŸer (aÃ§Ä±klayÄ±n) - Hassas bilgi yazmayÄ±n",ENTER_JUSTIFICATION:"Neden girin",DELETE_LABEL:"Etiketi sil",LABEL_DETAILS_TOOLTIP:"Hassasiyet etiketi ayrÄ±ntÄ±larÄ±",SPECIFY_USER_OR_GROUP:"KullanÄ±cÄ±, grup veya alan belirtin",DEFINE_LABEL_PERMISSION:"Etiket izni belirleyin",MORE_OPTIONS:"Daha fazla seÃ§enek",CAN_READ:"Okuyabilir",CAN_CHANGE:"DeÄŸiÅŸtirebilir",CAN_FULL_CONTROL:"Tamamen kontrol edebilir",READ:"Okuma",READ_COPY:"Okuma (kopyalayabilir)",READ_PRINT:"Okuma (yazdÄ±rabilir)",READ_COPY_PRINT:"Okuma (kopyalayabilir ve yazdÄ±rabilir)",CHANGE:"DeÄŸiÅŸtirme",CHANGE_PRINT:"DeÄŸiÅŸtirme (yazdÄ±rabilir)",FULL_CONTROL:"Tam kontrol",DELETE:"Silme",CUSTOM:"Ã–zel",OWNER:"Sahip",OWNER_SIGNAL:"(siz)",DOCUMENT_EXPIRY:"Belgenin sÃ¼resi ÅŸu tarihte doluyor: ",READ_PERMISSION_TEXT:"Okuma izni olan kullanÄ±cÄ±lar ÅŸunlarÄ± da yapabilir:",COPY_CHECKBOX:"Bu belgeyi kopyalama",CHANGE_PERMISSION_TEXT:"Okuma veya deÄŸiÅŸtirme izni olan kullanÄ±cÄ±lar ÅŸunlarÄ± da yapabilir:",PRINT_CHECKBOX:"Bu belgeyi yazdÄ±r",MMDDYYYY_PLACEHOLDER:"gg/aa/yyyy",EXPIRY_DATE_ERROR:"GeÃ§erli bir sona erme tarihi girin",UDP_EDIT_LABEL:"Etiket izinlerini dÃ¼zenle",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"EKLENEN ALICILAR",GET_MS_GRAPH_CONTACTS_MSG:"Outlook kiÅŸilerine eriÅŸmek iÃ§in Office hesabÄ±nÄ±za giriÅŸ yapÄ±n.",GET_MS_GRAPH_CONTACTS_MSG_1:"KiÅŸilerinize eriÅŸmek iÃ§in lÃ¼tfenÂ  ",GET_MS_GRAPH_CONTACTS_MSG_2:" hesabÄ±nÄ±zlaÂ  ",GET_MS_GRAPH_CONTACTS_MSG_3:" giriÅŸ yapÄ±n.",EMAIL_REMOVE:"E-posta adresini kaldÄ±r"});
