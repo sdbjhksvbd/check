c06e4dc2032bc03bafdb0316639cec1b21960e8b
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:07:49 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/sign-services-auth/js/nls/da-dk/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SIGNIN_REQUIRED:"Login krÃ¦ves",PRELOGIN_DIALOG_BODY_DEFAULT:"Log pÃ¥ igen. Dette er med til at holde dine aftaler sikre.",PRELOGIN_DIALOG_BODY_DOCTAB:"Klik pÃ¥ Annuller for at lukke alle Ã¥bne Sign-aftaler.",CHECKBOX_TEXT:"Lad mig vÃ¦re logget pÃ¥ i de nÃ¦ste 14 dage",CANCEL:"Annuller",NEXT:"FortsÃ¦t",SIGOUT_DIALOG_TITLE:"Log af Adobe Acrobat Reader",SIGOUT_DIALOG_DESCRIPTION_1:'Du er i Ã¸jeblikket logget pÃ¥ med dit Adobe ID <strong>"<%=emailId%>"</strong>. Ved at klikke pÃ¥ "Log af" logges du af programmet pÃ¥ denne enhed. Appen beder muligvis om at blive afsluttet. Appen afinstalleres ikke.',SIGOUT_DIALOG_DESCRIPTION_2:"Dette program og cloudaktiver, der er relateret til dit Adobe ID, vil vÃ¦re tilgÃ¦ngelige, nÃ¥r du logger pÃ¥ igen.",SIGOUT_DIALOG_CANCEL_BTN:"Annuller",SIGOUT_DIALOG_SIGNOUT_BTN:"Log af"});
