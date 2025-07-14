b416826217750bcab35b0f5b831df10dcf104f18
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:31 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/pl-pl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"Dodaj",ADD_BUTTON_ARIA_LABEL:"Dodaj <%= connectorName %>",ADD_ACCOUNT_TITLE:"Dodaj konto",ADD_MEETING_SOURCE_TITLE:"Konto Microsoft Teams jest juÅ¼ poÅ‚Ä…czone",ADD_ACCOUNT_MESSAGE:"Masz juÅ¼ konto dla tej konfiguracji przechowywania. UsuÅ„ konto i sprÃ³buj ponownie.",ADD_MEETING_SOURCE_MESSAGE:"Konto Microsoft Teams zostaÅ‚o juÅ¼ poÅ‚Ä…czone. UsuÅ„ to konto i sprÃ³buj ponownie.",ADD_BUTTON_ONE_CONNECTOR:"Dodaj konto",CONNECTOR_DESC:"PoÅ‚Ä…cz siÄ™ z serwerem Microsoft SharePoint lub SharePoint Online, aby uzyskaÄ‡ dostÄ™p do plikÃ³w z poziomu aplikacji.",COMING_SOON_MSG:"JuÅ¼ wkrÃ³tce wiÄ™cej typÃ³w kont...",ADD_ACCOUNT_HEADER_TITLE:"Dodaj konto pamiÄ™ci masowej",ADD_ACCOUNT_HEADER_DESCRIPTION:"PoÅ‚Ä…cz siÄ™ ze swoim kontem, aby uzyskaÄ‡ dostÄ™p do plikÃ³w z programu Acrobat.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"MoÅ¼e nastÄ…piÄ‡ przekierowanie do przeglÄ…darki w celu zalogowania.",SIGN_IN_TO_TITLE:"Zaloguj siÄ™ do konta <%= connectorName %>",ENTER_EMAIL_ID:"E-mail",CHOOSE_ACCOUNT:"Wybierz konto <%= connectorName %>",PERSONAL_ACCOUNT_MSG:"Zaloguj siÄ™, uÅ¼ywajÄ…c konta <%= MSBrandName %>",BUSINESS_ACCOUNT_MSG:"Zaloguj siÄ™, uÅ¼ywajÄ…c konta firmowego lub szkolnego",ENTER_EMAIL_ID_TOOLTIP:"WprowadÅº adres e-mail konta <%= connectorName %>, ktÃ³re chcesz dodaÄ‡",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"Dodaj transkrypcje spotkaÅ„",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Korzystaj zÂ funkcji generatywnej SI, aby tworzyÄ‡ podsumowania spotkaÅ„, tematy dyskusji, obszary wymagajÄ…ce podjÄ™cia dziaÅ‚aÅ„ iÂ nie tylko na podstawie dowolnej transkrypcji spotkania.",ADD_BUTTON_TEAMS_CONNECTOR:"Zaloguj siÄ™ do aplikacji Teams",ADD_BUTTON_ZOOM_CONNECTOR:"Dowiedz siÄ™ wiÄ™cej",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"Wybierz ÅºrÃ³dÅ‚o spotkania tutaj lub otwÃ³rz plik VTT bezpoÅ›rednio wÂ programie Acrobat."});
