77631a407bcd73acd78ee2167f9faf4fae6b6a38
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:28 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/root/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"Search results for <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Close",SEARCH_CLOSE_LABEL:"Close Search",SEARCH_SIGNOUT_PROMPT:"To search other categories, please <%= prefix %>sign in.<%= suffix %>",SEARCH_SIGNIN:"Sign In",SEARCH_OFFLINE_PROMPT:"Could not complete your request. An error occurred while connecting to the service. Please <%= prefix %>try again.<%= suffix %>",SEARCH_ONLINE:"Try Again",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"No results found",SEARCH_NO_RESULTS_MESSAGE:"We couldn't find any results for <b><%= searchQuery %></b>.<br> Check your spelling or try broadening your search.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"No results found in <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"We couldn't find any results for <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"Something went wrong",SEARCH_CATEGORY_ERROR:"We weren't able to search this category.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> Search results for <%= query %> : <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> Search results : <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"We weren't able to search this category. Please try again later.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
