efe491e4a0b94aeeb332fbd66ab776c7e32f0aeb
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:51:32 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-files/js/nls/fr-fr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({FOOTER_ANCHOR:"Atteindre le site web Adobe DocumentÂ Cloud",NOTIFICATION_HEADING:"Fichiers <%= cloudName %>",CLOUD_NAME:"Espace de stockage Adobe",UNSUPPORTED_CONNECTOR_MSG:"<%= actionName %> nâ€™est pas pris en charge actuellement pour les fichiers stockÃ©s dans <%= connectorLabel %>."});
