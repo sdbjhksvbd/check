4fa6e292cd9a62e4837ea8eead9e2a9b2e48db00
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:51:40 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-files/js/nls/nb-no/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({FOOTER_ANCHOR:"GÃ¥ til nettstedet for Adobe Document Cloud",NOTIFICATION_HEADING:"<%= cloudName %>-filer",CLOUD_NAME:"Adobes skybaserte lagring",UNSUPPORTED_CONNECTOR_MSG:"<%= actionName %> stÃ¸ttes ikke for filer som er lagret i <%= connectorLabel %>."});
