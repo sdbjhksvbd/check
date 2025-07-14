461d1274e0197f3309733c34f5dba5430225f760
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:06 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/ro-ro/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Selectare etichetÄƒ de confidenÅ£ialitate",CANCEL_BUTTON_TEXT:"Anulare",UPDATE_BUTTON_TEXT:"Actualizare",APPLY_BUTTON_TEXT:"Aplicare",BACK_BUTTON_TEXT:"ÃŽnapoi",SUBMIT_BUTTON_TEXT:"Trimitere",SAVE_BUTTON_TEXT:"Salva",ADD_BUTTON_TEXT:"AdÄƒugare",JUSTIFICATION_REQUIRED:"Este necesarÄƒ o justificare",JUSTIFICATION_HEAD_TEXT:"OrganizaÈ›ia dvs. solicitÄƒ o justificare pentru modificarea etichetei de sensibilitate.",JUSTIFICATION_OPTION_1:"Eticheta anterioarÄƒ nu mai este valabilÄƒ",JUSTIFICATION_OPTION_2:"Eticheta anterioarÄƒ a fost incorectÄƒ",JUSTIFICATION_OPTION_OTHER:"Altele (explicaÈ›i) - Nu introduceÈ›i informaÈ›ii sensibile",ENTER_JUSTIFICATION:"Introducere justificare",DELETE_LABEL:"È˜tergere  etichetÄƒ",LABEL_DETAILS_TOOLTIP:"Detalii privind eticheta de sensibilitate",SPECIFY_USER_OR_GROUP:"SpecificaÈ›i utilizatorul, grupul sau domeniul",DEFINE_LABEL_PERMISSION:"DefiniÈ›i permisiunea de etichetare",MORE_OPTIONS:"OpÈ›iuni suplimentare",CAN_READ:"Poate citi",CAN_CHANGE:"Poate modifica",CAN_FULL_CONTROL:"Poate controla Ã®n totalitate",READ:"Citi",READ_COPY:"Citi (poate copia)",READ_PRINT:"Citi (poate imprima)",READ_COPY_PRINT:"Citi (poate copia È™i imprima)",CHANGE:"Modificare",CHANGE_PRINT:"Modifica (pot imprima)",FULL_CONTROL:"Control complet",DELETE:"È˜tergere",CUSTOM:"Personalizat",OWNER:"Proprietar",OWNER_SIGNAL:"(dvs.)",DOCUMENT_EXPIRY:"Acest document expirÄƒ la ",READ_PERMISSION_TEXT:"Persoanele cu permisiune de citire au È™i permisiunea de a",COPY_CHECKBOX:"Copia acest document",CHANGE_PERMISSION_TEXT:"Persoanele cu permisiune de citire sau modificare au È™i permisiunea de a",PRINT_CHECKBOX:"Imprima acest document",MMDDYYYY_PLACEHOLDER:"zz/ll/aaaa",EXPIRY_DATE_ERROR:"IntroduceÈ›i o datÄƒ de expirare validÄƒ",UDP_EDIT_LABEL:"Editare permisiuni de etichetare",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"DESTINATARI ADÄ‚UGAÈšI",GET_MS_GRAPH_CONTACTS_MSG:"AutentificaÈ›i-vÄƒ cu contul Office pentru a accesa contactele Outlook.",GET_MS_GRAPH_CONTACTS_MSG_1:"ConectaÈ›i-vÄƒ la ",GET_MS_GRAPH_CONTACTS_MSG_2:" contul dvs. pentru a accesa ",GET_MS_GRAPH_CONTACTS_MSG_3:" contactele.",EMAIL_REMOVE:"Eliminare e-mail"});
