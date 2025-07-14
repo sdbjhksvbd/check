a935b2a46293a675bde68dbcf1339def91e507c6
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:18 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/es-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"Digitalizaciones",SCAN_FILES_TITLE:"Instala la aplicaciÃ³n gratuita Adobe Scan",SCAN_FILES_READER_CONTENT:"Transforma tu telÃ©fono o tableta en un escÃ¡ner portÃ¡til y escanea cualquier documento para convertirlo en PDF. DescÃ¡rgala ahora.",SCAN_FILES_SIGN_IN_CONTENT:"Â¿Ya eres usuario de Adobe Scan? Inicia sesiÃ³n para acceder a los archivos escaneados.",SCAN_FILES_CONTENT:"Transforma tu telÃ©fono o tableta en un escÃ¡ner portÃ¡til y digitaliza cualquier documento para convertirlo en PDF. DescÃ¡rgala ahora.",NO_SCAN_TEXT:"No tienes ninguna digitalizaciÃ³n disponible.",SCAN_FILES_QR_CODE_TITLE:"Obtenga la aplicaciÃ³n gratuita Adobe Scan",SCAN_FILES_QR_CODE_CONTENT:"Escanea el cÃ³digo QR para convertir cualquier cosa en un archivo PDF desde tu dispositivo mÃ³vil y accede a los PDF escaneados desde aquÃ­."});
