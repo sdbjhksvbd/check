72372c69f13626b7b98de5773b667ca5834a9850
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:17 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/it-it/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"Risultati della ricerca per <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Chiudi",SEARCH_CLOSE_LABEL:"Chiudi ricerca",SEARCH_SIGNOUT_PROMPT:"Per cercare in altre categorie, Ã¨ necessario eseguire l'<%= prefix %>accesso.<%= suffix %>",SEARCH_SIGNIN:"Accedi",SEARCH_OFFLINE_PROMPT:"Impossibile completare la richiesta. Si Ã¨ verificato un errore durante la connessione al servizio. <%= prefix %>Riprova.<%= suffix %>",SEARCH_ONLINE:"Riprova",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Nessun risultato trovato",SEARCH_NO_RESULTS_MESSAGE:"Impossibile trovare risultati per <b><%= searchQuery %></b>.<br> Controlla se il termine immesso Ã¨ corretto o prova ad ampliare la ricerca.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Nessun risultato trovato in <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Impossibile trovare risultati per <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"Si Ã¨ verificato un errore",SEARCH_CATEGORY_ERROR:"Impossibile cercare in questa categoria.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"Risultati di ricerca <%= category %> per <%= query %>: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"Risultati di ricerca <%= category %>: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Impossibile cercare in questa categoria. Riprova piÃ¹ tardi.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
