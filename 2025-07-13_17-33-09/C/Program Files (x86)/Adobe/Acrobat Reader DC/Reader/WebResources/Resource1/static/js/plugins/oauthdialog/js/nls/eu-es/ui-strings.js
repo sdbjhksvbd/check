7172d7fb39b04aefde9cded42eec00ef8a6306d9
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:08 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/eu-es/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Zain...",OAUTHDIALOG_2:"Beste fitxa bat ireki dizugu arakatzailean saioa has dezazun.",OAUTHDIALOG_3:"Arakatzailean autentifikazioa amaitu ondoren, aurrera egingo duzu automatikoki aplikazioan.",OAUTHDIALOG_TROUBLE:"Arazoak dituzu saioa hasteko?",OAUTHDIALOG_TIMEOUT_1:"Ezin izan dugu hasi zure saioa. Saiatu berriro geroago.",CANCEL_BUTTON_TEXT:"Utzi",CANCEL_BUTTON_TEXT_2:"Itxi",OAUTHDIALOG_HEADING_2:"Ezin izan da hasi saioa"});
