e4b1c9d0d96663d46d44aa725a72be336d178358
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:29 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/nl-nl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"Toevoegen",ADD_BUTTON_ARIA_LABEL:"<%= connectorName %> toevoegen",ADD_ACCOUNT_TITLE:"Account toevoegen",ADD_MEETING_SOURCE_TITLE:"Microsoft Teams-account is al gekoppeld",ADD_ACCOUNT_MESSAGE:"U hebt al een account ingesteld voor deze opslag. Verwijder dit account en probeer het vervolgens opnieuw.",ADD_MEETING_SOURCE_MESSAGE:"U hebt al een Microsoft Teams-account gekoppeld. Verwijder dat account en probeer het opnieuw.",ADD_BUTTON_ONE_CONNECTOR:"Account toevoegen",CONNECTOR_DESC:"Maak verbinding met Microsoft SharePoint Server of SharePoint Online om uw bestanden vanuit een toepassing te openen.",COMING_SOON_MSG:"Binnenkort meer accounttypen beschikbaarâ€¦",ADD_ACCOUNT_HEADER_TITLE:"Opslagaccount toevoegen",ADD_ACCOUNT_HEADER_DESCRIPTION:"Meld u aan bij uw account om toegang te krijgen tot uw bestanden vanuit Acrobat.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Wellicht wordt u doorgestuurd naar een webbrowser om u aan te melden.",SIGN_IN_TO_TITLE:"Aanmelden bij <%= connectorName %>",ENTER_EMAIL_ID:"E-mail",CHOOSE_ACCOUNT:"<%= connectorName %>-account kiezen",PERSONAL_ACCOUNT_MSG:"Aanmelden met uw <%= MSBrandName %>-account",BUSINESS_ACCOUNT_MSG:"Aanmelden met uw werk- of schoolaccount",ENTER_EMAIL_ID_TOOLTIP:"Voer het e-mailadres van het <%= connectorName %>-account in dat u wilt toevoegen",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"Transcripten van vergaderingen toevoegen",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Gebruik de generatieve AI-functies om samenvattingen van vergaderingen, gespreksonderwerpen<br/> discussiepunten, actie-items en meer te genereren uit elk transcript van een vergadering.",ADD_BUTTON_TEAMS_CONNECTOR:"Aanmelden bij Teams",ADD_BUTTON_ZOOM_CONNECTOR:"Meer informatie",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"Kies hier een bron voor de vergadering of open rechtstreeks een VTT-bestand in Acrobat."});
