8813cf12cd5a0415b44411ee1466b44af113c566
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:30 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/it-it/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"Scansioni",SCAN_FILES_TITLE:"Scarica l'app gratuita Adobe Scan",SCAN_FILES_READER_CONTENT:"Converti il tuo smartphone o tablet in uno scanner portatile, per acquisire tutto in PDF. Scarica l'app.",SCAN_FILES_SIGN_IN_CONTENT:"Sei giÃ  un utente di Adobe Scan? Esegui il login per accedere alle tue scansioni.",SCAN_FILES_CONTENT:"Converti il tuo smartphone o tablet in uno scanner portatile, per acquisire tutto in PDF. Scarica l'app.",NO_SCAN_TEXT:"Non vi sono scansioni disponibili.",SCAN_FILES_QR_CODE_TITLE:"Scarica lâ€™app gratuita Adobe Scan",SCAN_FILES_QR_CODE_CONTENT:"Scansiona il codice QR per convertire in PDF dal tuo dispositivo mobile, quindi accedi ai PDF scansionati da qui."});
