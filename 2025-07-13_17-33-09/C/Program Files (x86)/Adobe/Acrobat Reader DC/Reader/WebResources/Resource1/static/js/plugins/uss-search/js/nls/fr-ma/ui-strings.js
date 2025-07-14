88f926a9a9fab36749bd2895a37d730a0103d2d9
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:10 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/fr-ma/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"RÃ©sultats de la recherche pourÂ : <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Fermer",SEARCH_CLOSE_LABEL:"Fermer la recherche",SEARCH_SIGNOUT_PROMPT:"Pour effectuer une recherche dans dâ€™autres catÃ©gories, veuillez <%= prefix %>vous connecter.<%= suffix %>",SEARCH_SIGNIN:"Se connecter",SEARCH_OFFLINE_PROMPT:"Impossible de traiter votre requÃªte. Une erreur s'est produite lors de la connexion au service. Veuillez <%= prefix %>rÃ©essayer.<%= suffix %>",SEARCH_ONLINE:"RÃ©essayer",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Aucun rÃ©sultat trouvÃ©",SEARCH_NO_RESULTS_MESSAGE:"Nous nâ€™avons trouvÃ© aucun rÃ©sultat pour <b><%= searchQuery %></b>.<br> VÃ©rifiez quâ€™il nâ€™y a pas de faute ou essayez dâ€™Ã©largir votre recherche.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Aucun rÃ©sultat trouvÃ© dans <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Nous nâ€™avons trouvÃ© aucun rÃ©sultat pour <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"Un problÃ¨me sâ€™est produit.",SEARCH_CATEGORY_ERROR:"Nous nâ€™avons pas pu effectuer de recherche dans cette catÃ©gorie.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> RÃ©sultats de la recherche pour <%= query %>Â : <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> RÃ©sultats de la rechercheÂ : <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Nous nâ€™avons pas pu effectuer de recherche dans cette catÃ©gorie. Veuillez rÃ©essayer plus tard.",AGREEMENT_ENTERPRISE_HEADER:"AdobeÂ AcrobatÂ Sign"});
