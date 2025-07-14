e0c54e4b74683f195f59f08cfb7e66b838d85f7f
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:52:34 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-recent-files/js/nls/pt-br/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"Recentes",MY_RECENT_FILES:"Meus arquivos recentes",NO_RECENT_FILES:"Nenhum arquivo recente",NO_SEARCH_RESULTS:"Sem resultados de pesquisa.",TRY_DIFFERENT_SEARCH:"Tente uma pesquisa diferente."});
