fd2f745f7c4ba9b2034e02625ca26f1093233a01
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:06 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/createpdfupsell-app/js/plugins/rhp/createpdfupsell-app-selector.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2016 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define("acplugins/createpdfupsell-app/js/ac-createpdfupsell-app",["plugins-common/ac-plugins"],function(e){var p=e.CreatepdfupsellApp;return p||(p=e.CreatepdfupsellApp={}),p}),define("acplugins/createpdfupsell-app/js/nls/ui-strings",{root:!0,"xx-xx":!0,"de-de":!0,"en-gb":!0,"es-es":!0,"fr-fr":!0,"it-it":!0,"ja-jp":!0,"da-dk":!0,"fi-fi":!0,"nb-no":!0,"nl-nl":!0,"pt-br":!0,"sv-se":!0,"zh-cn":!0,"zh-tw":!0,"ko-kr":!0,"cs-cz":!0,"hu-hu":!0,"pl-pl":!0,"ru-ru":!0,"uk-ua":!0,"tr-tr":!0,"sk-sk":!0,"sl-si":!0,"bg-bg":!0}),define("acplugins/createpdfupsell-app/js/nls/root/ui-strings",{CREATEPDFUPSELL_APP:"Ceate PDF",CREATEPDFUPSELL_APP_TOOL_VIEW_TITLE_RHP:"Convert files to PDF and easily combine them with other file types with a paid subscription",CREATEPDFUPSELL_APP_ICON_TOOLTIP:"Learn more",CREATEPDFUPSELL_APP_START:"Learn more",CREATEPDFUPSELL_APP_HEADING:"Adobe Acrobat Pro",CREATEPDFUPSELL_APP_RHP_GO_URL:"http://www.adobe.com/go/cpdfrhprdr_12_0_11"}),define("acplugins/createpdfupsell-app/js/views/selector-view",["core/ac-core","acplugins/createpdfupsell-app/js/ac-createpdfupsell-app","i18n!acplugins/createpdfupsell-app/js/nls/ui-strings","core/views/plugin-selector-view"],function(e,p,l){return p.SelectorView=e.PluginSelectorView.extend({getSelectorLabel:function(){return l.CREATEPDFUPSELL_APP},getSelectorIconClass:function(){return"createpdfupsellapp-toolbar-icon"},getSiteCatalystName:function(){return"createpdfupsellapp"}}),p.SelectorView}),define("acplugins/createpdfupsell-app/js/ops/plugin/starting-plugin-op",["core/ac-core","acplugins/createpdfupsell-app/js/ac-createpdfupsell-app","core/ops/op"],function(e,p){return p.StartingPluginOp=e.Op.extend({run:function(){return this._getResolvedPromise()}}),p.StartingPluginOp}),define("acplugins/createpdfupsell-app/js/ops/full-preview-overlay/selecting-full-preview-overlay-op",["core/ac-core","acplugins/createpdfupsell-app/js/ac-createpdfupsell-app","core/ops/op"],function(e,p){return p.SelectingFullPreviewOverlayOp=e.Op.extend({run:function(){return e.opFactory.create("SelectingPluginOp",this.options).run("full_preview_overlay_example")}}),p.SelectingFullPreviewOverlayOp}),define("acplugins/createpdfupsell-app/js/ops/op-factory",["core/ac-core","acplugins/createpdfupsell-app/js/ac-createpdfupsell-app","acplugins/createpdfupsell-app/js/ops/plugin/starting-plugin-op","acplugins/createpdfupsell-app/js/ops/full-preview-overlay/selecting-full-preview-overlay-op"],function(e,p){p.OpFactory=e.Op.extend({create:function(e,l){var a=p[e];return a?new a(l):void LOG(0,"AcCreatePdfUpsellApp.OpFactory.create: Unrecognized class: "+e)}})}),define("acplugins/createpdfupsell-app/js/createpdfupsell-app",["acplugins/createpdfupsell-app/js/ac-createpdfupsell-app","acplugins/createpdfupsell-app/js/ops/op-factory"],function(e){_.extend(e,{init:function(){e.opFactory=new e.OpFactory}})}),define("acplugins/createpdfupsell-app/js/plugins/rhp/createpdfupsell-app-selector",["acplugins/createpdfupsell-app/js/ac-createpdfupsell-app","acplugins/createpdfupsell-app/js/views/selector-view","acplugins/createpdfupsell-app/js/createpdfupsell-app"],function(e){return e.SelectorView});
