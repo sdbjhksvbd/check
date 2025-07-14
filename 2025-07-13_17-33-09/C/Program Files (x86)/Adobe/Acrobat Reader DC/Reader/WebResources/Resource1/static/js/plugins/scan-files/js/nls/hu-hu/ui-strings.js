749db5290c87979dd5904aeac1adf9cfd2c595ef
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:28 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/hu-hu/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"SzkennelÃ©s",SCAN_FILES_TITLE:"Az ingyenes Adobe Scan alkalmazÃ¡s beszerzÃ©se",SCAN_FILES_READER_CONTENT:"AlakÃ­tsa telefonjÃ¡t vagy tÃ¡blagÃ©pÃ©t hordozhatÃ³ szkennerrÃ©, hogy aztÃ¡n bÃ¡rmibÅ‘l PDF-et kÃ©szÃ­thessen. TÃ¶ltse le most!",SCAN_FILES_SIGN_IN_CONTENT:"MÃ¡r hasznÃ¡lja az Adobe Scan alkalmazÃ¡st? Jelentkezzen be a beolvasott dokumentumok elÃ©rÃ©sÃ©hez.",SCAN_FILES_CONTENT:"AlakÃ­tsa telefonjÃ¡t vagy tÃ¡blagÃ©pÃ©t hordozhatÃ³ szkennerrÃ©, hogy aztÃ¡n bÃ¡rmibÅ‘l PDF-et kÃ©szÃ­thessen. TÃ¶ltse le most!",NO_SCAN_TEXT:"Nem Ã¡ll rendelkezÃ©sre egyetlen beolvasott fÃ¡jl sem.",SCAN_FILES_QR_CODE_TITLE:"Az ingyenes Adobe Scan alkalmazÃ¡s beszerzÃ©se",SCAN_FILES_QR_CODE_CONTENT:"Olvassa be a QR-kÃ³dot, Ã©s kÃ©szÃ­tsen PDF-et bÃ¡rmilyen dokumentumbÃ³l mobileszkÃ¶zÃ©n, majd fÃ©rjen hozzÃ¡ a szkennelt PDF-hez innen."});
