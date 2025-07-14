4ca84cd76c3d99ffbf186471c6c02629cca80b25
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:20 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/eu-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"Eskaneatzeak",SCAN_FILES_TITLE:"Lortu doan Adobe Scan aplikazioa",SCAN_FILES_READER_CONTENT:"Bihurtu telefonoa edo tableta eskaner eramangarri eta eskaneatu nahi duzuna PDF formatuan. Deskargatu orain.",SCAN_FILES_SIGN_IN_CONTENT:"Adobe Scan erabiltzen duzu jada? Hasi saioa eskaneatutako dokumentuak atzitzeko.",SCAN_FILES_CONTENT:"Bihurtu telefonoa edo tableta eskaner eramangarri eta eskaneatu nahi duzuna PDF formatuan. Deskargatu orain.",NO_SCAN_TEXT:"Ez duzu eskaneatutako fitxategirik.",SCAN_FILES_QR_CODE_TITLE:"Lortu doan Adobe Scan aplikazioa",SCAN_FILES_QR_CODE_CONTENT:"Eskaneatu QR kodea gailu mugikorrean edozein fitxategi PDF bihurtzeko, eta, ondoren, atzitu eskaneatutako PDFak hemen."});
