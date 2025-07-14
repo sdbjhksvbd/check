72e7ba0d65b4a670048aa0bd2d0e76e5fcebb1f5
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:40 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/hu-hu/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"<%= connectorLabel %>  fÃ¡jlok",GENERIC_ERROR_TITLE:"Hiba",GENERIC_ERROR_MSG:"Hiba tÃ¶rtÃ©nt a fÃ¡jlok lehÃ­vÃ¡sa kÃ¶zben.",CONNECTOR_FOOTER_ANCHOR_TEXT:"LÃ©pjen a(z) <%= connectorDisplayName %> weboldalra",ACCOUNT_REMOVED_MESSAGE:"Az Acrobat nem tud hozzÃ¡fÃ©rni <%= connectorDisplayName %> fiÃ³kjÃ¡hoz",ACCESS_REMOVED_ACTION:"A fÃ¡jlok elÃ©rÃ©sÃ©hez jelentkezzen be <%= connectorDisplayName %> fiÃ³kjÃ¡ba.",ACCESS_REMOVED_CTA:"BejelentkezÃ©s",CONNECTOR_NOTIFICATION_HEADING:"<%= connectorDisplayName %> fÃ¡jl",FETCHING_TRANSCRIPTS:"Jegyzetek Ã¶sszeÃ¡llÃ­tÃ¡sa..."});
