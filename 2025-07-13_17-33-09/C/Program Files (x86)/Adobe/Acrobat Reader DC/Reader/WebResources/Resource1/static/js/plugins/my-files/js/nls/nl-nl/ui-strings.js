b6350d2cbbe20489e84912e30fd0dd1bdcf2dada
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:51:42 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-files/js/nls/nl-nl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({FOOTER_ANCHOR:"Ga naar de website van Adobe Document Cloud",NOTIFICATION_HEADING:"Bestanden <%= cloudName %>",CLOUD_NAME:"Adobe-cloudopslag",UNSUPPORTED_CONNECTOR_MSG:"<%= actionName %> wordt momenteel niet ondersteund voor bestanden die zijn opgeslagen in <%= connectorLabel %>."});
