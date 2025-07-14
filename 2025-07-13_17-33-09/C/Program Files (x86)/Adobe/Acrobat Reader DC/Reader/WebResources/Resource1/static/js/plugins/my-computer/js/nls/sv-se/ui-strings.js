27bb19082464bb0f4a665ffe9338d867020e4edf
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:50:05 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/sv-se/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"Din dator",SELECT_FILES:"VÃ¤lj filer frÃ¥n Min dator",NO_RECENT_FOLDERS_SELECT:"Klicka pÃ¥ 'BlÃ¤ddra' fÃ¶r att Ã¶ppna en fil frÃ¥n datorn.",HEADING_RECENT_LOCATIONS:"Senast anvÃ¤nda platser",HEADING_SELECT_FROM_RECENT_FOLDER:"VÃ¤lj i mappen med senaste filerâ€¦",HEADING_SELECT_FOLDER:"Spara i mappen med senaste filerâ€¦",BROWSE_BUTTON:"BlÃ¤ddra",BROWSE_DIALOG_BUTTON:"BlÃ¤ddra...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"VÃ¤lj en annan mapp...",BROWSE_FOLDER_TEXT:"VÃ¤lj en annan mapp",NO_RECENT_FOLDERS_SAVE_AS:"Inga mappar har anvÃ¤nts nyligen",NO_FOLDERS_MYCOMPUTER_PROMPT:"Det finns inga filer hÃ¤r Ã¤nnu.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"NÃ¤r du Ã¶ppnar filer frÃ¥n datorn ser du mapparna hÃ¤r."});
