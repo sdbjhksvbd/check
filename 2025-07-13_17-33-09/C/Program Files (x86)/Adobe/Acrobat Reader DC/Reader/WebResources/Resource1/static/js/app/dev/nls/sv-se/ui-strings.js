751777fa6842182a3cf54109caa188c0c2f21115
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:36:56 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/app/dev/nls/sv-se/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({COPYRIGHT:"Â©2012-2015 Adobe Systems Incorporated, All Rights Reserved.",HELP:"HjÃ¤lp",PRIVACY:"Sekretess",TERMS:"Villkor",COOKIES:"Cookies",PRIVACY_POLICY:"Integritetspolicy & cookies",TERMS_OF_USE:"AnvÃ¤ndningsvillkor",WELCOME_MSG:"AnvÃ¤nd Adobe Document Services fÃ¶r att komma Ã¥t dina filer frÃ¥n alla enheter",LOAD_FAILURES:"LÃ¤s in fel",MODULES_NOT_LOADED:"FÃ¶ljande moduler kunde inte lÃ¤sas in:<br/><br/><%= modules %>",SIGN_IN:"Logga in",CREATE_ACCOUNT:"Skapa konto",FORGOT_PASSWORD:"Har du problem med inloggningen?",EMAIL_VERIFICATION:"E-postverifikation",PDF_SERVICES:"Â Â /Â Â PDF-tjÃ¤nster",ACCOUNT:"Konto",LINK_CREATIVE_CLOUD_ACCOUNT:"LÃ¤nka Creative Cloud-konto...",LINK_DROPBOX_ACCOUNT:"LÃ¤nka Dropbox-konto...",MANAGE_SUB:"Min plan...",USER_ACCOUNT:"AnvÃ¤ndarkonto...",ASK_US_A_QUESTION:"StÃ¤ll en frÃ¥ga till oss...",SUPPORT:"Support...",CHAT_WITH_US:"Chatta med oss...",ABOUT_APP:"Om Document Cloud...",SIGN_OUT:"Logga ut",SHOW_PROGRESS:"Visa fÃ¶rlopp",PROGRESS_COMPLETE:"FÃ¶rloppet Ã¤r klart",REMOVE_HISTORY:"Ta bort historik",OPERATION_IN_PROGRESS_DLG_TITLE:"Ã…tgÃ¤rd pÃ¥gÃ¥r",OPERATION_IN_PROGRESS_DLG_MSG:"En Ã¥tgÃ¤rd pÃ¥gÃ¥r just nu. Om du fortsÃ¤tter avbryts Ã¥tgÃ¤rden. Vill du fortsÃ¤tta?",QUOTA_LEGEND:"<%= using %> av <%= allotment %>",STORAGE_EXCEEDED_LEARN_MORE:"Lagringsutrymmet har Ã¶verskridits (<a class='quota-display-learn-more'>LÃ¤s mer</a>)",ABOUT_DLG_TITLE:"Om Document Cloud",ABOUT_DLG_COPYRIGHT_PARA:"Copyright Â© 2012-2015 Adobe Systems Incorporated and its licensors. All Rights Reserved.",ABOUT_DLG_TRADEMARK_PARA:"Adobe, the Adobe logo, and Acrobat are either registered trademarks or trademarks of Adobe Systems Incorporated in the United States and/or other countries. All other trademarks are the property of their respective owners.",ABOUT_DLG_PORTIONS_PARA:"Delar anvÃ¤nder:",ABOUT_DLG_MICROSOFT_PARA:"Microsoft Windows Media Technologies. Copyright Â© 1999-2002, 2006 Microsoft Corporation. All Rights Reserved.",ABOUT_DLG_IRIS_PARA:"OCR technology by Image Recognition Integrated Systems S.A. Copyright Â© 1987 - 2012, I.R.I.S. S.A. All rights reserved.",ABOUT_DLG_SOLID_PARA:"Copyright Â© 2003-2011 Solid Documents, LLC",ABOUT_DLG_THIRD_PARTY_PARA:"Meddelanden och villkor frÃ¥n tredje part om tredje parts programvara finns pÃ¥ <%= thirdPartyLink %> och hÃ¤r fÃ¶r din referens.",ABOUT_DLG_BUILD_NUMBER:"Build: <%= buildNum %>",NO_CONNECTION_TITLE:"Ingen internetuppkoppling",NO_CONNECTION_MSG:"Funktionerna i den hÃ¤r delen krÃ¤ver en internetuppkoppling. Anslut datorn till internet och fÃ¶rsÃ¶k igen.",NETWORK_FAILURE:"NÃ¤tverksanslutningsfel",NETWORK_DISCONNECTED:"Det tycks inte finnas en nÃ¤tverksanslutning. Reader kanske inte kan fungera helt utan en nÃ¤tverksanslutning"});
