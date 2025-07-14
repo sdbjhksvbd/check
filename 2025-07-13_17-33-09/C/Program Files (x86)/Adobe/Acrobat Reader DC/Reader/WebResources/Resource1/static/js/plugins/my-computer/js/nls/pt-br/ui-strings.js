0eb74cef9179d82da17980edbbc9f4a861c376d0
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:59 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/pt-br/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"Seu computador",SELECT_FILES:"Escolher arquivos do Meu Computador",NO_RECENT_FOLDERS_SELECT:"Clique em 'Navegar' para abrir um arquivo do seu computador.",HEADING_RECENT_LOCATIONS:"Locais recentes",HEADING_SELECT_FROM_RECENT_FOLDER:"Selecionar da pasta recenteâ€¦",HEADING_SELECT_FOLDER:"Salvar na pasta recente...",BROWSE_BUTTON:"Procurar",BROWSE_DIALOG_BUTTON:"Procurar...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"Escolher outra pasta...",BROWSE_FOLDER_TEXT:"Selecione uma pasta diferente",NO_RECENT_FOLDERS_SAVE_AS:"Nenhuma pasta recente",NO_FOLDERS_MYCOMPUTER_PROMPT:"Ainda nÃ£o hÃ¡ arquivos aqui.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"Quando vocÃª abrir arquivos pelo seu computador, as respectivas pastas serÃ£o exibidas aqui."});
