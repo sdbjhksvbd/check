dc80c17e96e9b9b12b63728394b56ca031410bef
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:04 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/en-il/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Waiting...",OAUTHDIALOG_2:"We have opened a new browser tab for you to sign in.",OAUTHDIALOG_3:"After you finish authentication in the browser, you will automatically continue within the app.",OAUTHDIALOG_TROUBLE:"Trouble getting in?",OAUTHDIALOG_TIMEOUT_1:"We did not manage to sign you in. Please try again.",CANCEL_BUTTON_TEXT:"Cancel",CANCEL_BUTTON_TEXT_2:"Close",OAUTHDIALOG_HEADING_2:"Sign in failed"});
