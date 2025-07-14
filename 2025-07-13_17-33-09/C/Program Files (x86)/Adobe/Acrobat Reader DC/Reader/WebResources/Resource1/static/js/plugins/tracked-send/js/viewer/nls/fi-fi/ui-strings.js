9835b00fd4f63b294e94412d66bd746cc3bcda68
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:09:54 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/tracked-send/js/viewer/nls/fi-fi/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({APP_NAME:"KATSELUOHJELMA",UNSUPPORTED_BROWSER_TITLE_2:"Selainta ei tueta",UNSUPPORTED_BROWSER_MSG_2:"KÃ¤ytÃ¤t Internet Explorerin versiota, jota ei tueta. Jaettujen tiedostojen tarkastelua varten sinun on pÃ¤ivitettÃ¤vÃ¤ <a class='dialog-link' href='<%= ie %>'>Internet Explorerin uusimpaan versioon</a> tai kÃ¤ytettÃ¤vÃ¤ <a class='dialog-link' href='<%= chrome %>'>Google Chromen</a>, <a class='dialog-link' href='<%= safari %>'>Safarin</a> tai <a class='dialog-link' href='<%= firefox %>'>Firefoxin</a> uusinta versiota."});
