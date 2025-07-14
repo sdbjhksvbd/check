6823d5e29eaf1e6ef32f5d06e85929792aeb3b6a
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:16 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/it-it/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"In attesa...",OAUTHDIALOG_2:"Ãˆ stata aperta una nuova scheda del browser per eseguire lâ€™accesso.",OAUTHDIALOG_3:"Una volta completata lâ€™autenticazione nel browser, tornerai automaticamente allâ€™app per procedere.",OAUTHDIALOG_TROUBLE:"DifficoltÃ  di accesso?",OAUTHDIALOG_TIMEOUT_1:"Non Ã¨ stato possibile eseguire lâ€™accesso. Riprova.",CANCEL_BUTTON_TEXT:"Annulla",CANCEL_BUTTON_TEXT_2:"Chiudi",OAUTHDIALOG_HEADING_2:"Accesso non riuscito"});
