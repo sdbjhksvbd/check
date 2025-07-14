40ae34f8486e31a64192099c46f25ea6c761debd
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:45 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/nl-nl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"<%= connectorLabel %>-bestanden",GENERIC_ERROR_TITLE:"Fout",GENERIC_ERROR_MSG:"Er is een fout opgetreden tijdens het ophalen van uw bestanden.",CONNECTOR_FOOTER_ANCHOR_TEXT:"Ga naar de <%= connectorDisplayName %>-website",ACCOUNT_REMOVED_MESSAGE:"Acrobat heeft geen toegang tot uw <%= connectorDisplayName %>-account",ACCESS_REMOVED_ACTION:"Meld u aan bij uw <%= connectorDisplayName %>-account om toegang te krijgen tot uw bestanden.",ACCESS_REMOVED_CTA:"Aanmelden",CONNECTOR_NOTIFICATION_HEADING:"<%= connectorDisplayName %> Bestanden",FETCHING_TRANSCRIPTS:"Uw transcripten ophalen..."});
