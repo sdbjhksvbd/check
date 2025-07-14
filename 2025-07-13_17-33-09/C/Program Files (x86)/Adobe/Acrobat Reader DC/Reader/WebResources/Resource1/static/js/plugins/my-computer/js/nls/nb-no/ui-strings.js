3debd9b5bb46f486f849f436f2274db84e900fe4
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:54 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/nb-no/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"Din datamaskin",SELECT_FILES:"Velg filer fra Min datamaskin",NO_RECENT_FOLDERS_SELECT:"Klikk Bla gjennom for Ã¥ Ã¥pne en fil fra datamaskinen.",HEADING_RECENT_LOCATIONS:"Sist brukte plasseringer",HEADING_SELECT_FROM_RECENT_FOLDER:"Velg fra sist brukte mappe...",HEADING_SELECT_FOLDER:"Lagre i sist brukte mappe...",BROWSE_BUTTON:"Bla gjennom",BROWSE_DIALOG_BUTTON:"Bla gjennom...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"Velg en annen mappe...",BROWSE_FOLDER_TEXT:"Velg en annen mappe",NO_RECENT_FOLDERS_SAVE_AS:"Ingen nylige mapper",NO_FOLDERS_MYCOMPUTER_PROMPT:"Det er ingen filer her ennÃ¥.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"NÃ¥r du Ã¥pner filer fra datamaskinen, vil du se filmappene her."});
