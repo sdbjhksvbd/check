10cb02fccd1e7d414dce38a451c20d9806094019
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:58 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/pl-pl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"TwÃ³j komputer",SELECT_FILES:"W oknie MÃ³j komputer wybierz pozycjÄ™ Pliki",NO_RECENT_FOLDERS_SELECT:"Kliknij przycisk PrzeglÄ…daj, aby otworzyÄ‡ plik na komputerze.",HEADING_RECENT_LOCATIONS:"Ostatnie poÅ‚oÅ¼enia",HEADING_SELECT_FROM_RECENT_FOLDER:"Wybierz z ostatnio uÅ¼ywanych...",HEADING_SELECT_FOLDER:"Zapisz w ostatnio uÅ¼ywanych...",BROWSE_BUTTON:"PrzeglÄ…daj",BROWSE_DIALOG_BUTTON:"PrzeglÄ…daj...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"Wybierz inny folder...",BROWSE_FOLDER_TEXT:"Wybierz inny folder",NO_RECENT_FOLDERS_SAVE_AS:"Brak ostatnich folderÃ³w",NO_FOLDERS_MYCOMPUTER_PROMPT:"Nie ma tu jeszcze Å¼adnych plikÃ³w.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"Po otwarciu plikÃ³w z komputera wyÅ›wietlane sÄ… ich foldery."});
