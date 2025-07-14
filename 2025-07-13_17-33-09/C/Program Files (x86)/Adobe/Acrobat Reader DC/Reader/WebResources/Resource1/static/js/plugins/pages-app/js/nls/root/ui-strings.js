e4fc9cd797546987178b531e6eef232376ad0302
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:00:04 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/pages-app/js/nls/root/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({PAGES_APP:"Organize Pages",PAGES_APP_TOOL_VIEW_TITLE_RHP:"Delete, insert, extract and rotate pages.",PAGES_APP_ICON_TOOLTIP:"Learn more",PAGES_APP_START:"Try now",PAGES_APP_HEADING:"Adobe Acrobat Pro",PAGES_APP_RHP_GO_URL:"http://www.adobe.com/go/organizerhprdr_12_0_0"});
