c418c86d4da5f042cf5f9cf153238ba00b26d921
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:51:30 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-files/js/nls/fi-fi/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({FOOTER_ANCHOR:"Siirry Adobe Document Cloud -sivustoon",NOTIFICATION_HEADING:"<%= cloudName %> tiedostoa",CLOUD_NAME:"Adoben pilvitallennustila",UNSUPPORTED_CONNECTOR_MSG:"<%= actionName %> ei tÃ¤llÃ¤ hetkellÃ¤ tue tiedostoja, jotka on tallennettu sovellukseen <%= connectorLabel %>."});
