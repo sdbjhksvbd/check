97ddde099e09f473db27f1ea7b45bed24985c34e
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:38 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/ro-ro/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"ScanÄƒri",SCAN_FILES_TITLE:"ObÈ›ineÈ›i aplicaÈ›ia gratuitÄƒ Adobe Scan",SCAN_FILES_READER_CONTENT:"TransformaÈ›i telefonul sau tableta Ã®ntr-un scaner portabil pentru a scana orice pe PDF. DescÄƒrcaÈ›i acum.",SCAN_FILES_SIGN_IN_CONTENT:"SunteÈ›i deja utilizator Adobe Scan? ConectaÈ›i-vÄƒ pentru a vÄƒ accesa scanÄƒrile.",SCAN_FILES_CONTENT:"TransformaÈ›i telefonul sau tableta Ã®ntr-un scaner portabil pentru a scana orice pe PDF. DescÄƒrcaÈ›i acum.",NO_SCAN_TEXT:"Nu aveÈ›i niciun fiÈ™ier scanat disponibil.",SCAN_FILES_QR_CODE_TITLE:"ObÈ›ineÈ›i aplicaÈ›ia gratuitÄƒ Adobe Scan",SCAN_FILES_QR_CODE_CONTENT:"ScanaÅ£i codul QR pentru a transforma orice lucru Ã®n PDF pe dispozitivul dvs. mobil, apoi accesaÅ£i fiÅŸierele PDF scanate aici."});
