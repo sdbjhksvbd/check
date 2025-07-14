6fb8296874c6633f2119d3d8fa7dfe7d0e8c5a2f
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:34 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/fi-fi/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"<%= connectorLabel %> -tiedostot",GENERIC_ERROR_TITLE:"Virhe",GENERIC_ERROR_MSG:"Tiedostojasi noudettaessa tapahtui virhe.",CONNECTOR_FOOTER_ANCHOR_TEXT:"Siirry <%= connectorDisplayName %> -sivustoon",ACCOUNT_REMOVED_MESSAGE:"Acrobat ei pysty kÃ¤yttÃ¤mÃ¤Ã¤n <%= connectorDisplayName %>-tiliÃ¤si.",ACCESS_REMOVED_ACTION:"Kirjaudu sisÃ¤Ã¤n palvelussa <%= connectorDisplayName %> olevaan tiliisi tiedostojen kÃ¤yttÃ¶Ã¤ varten.",ACCESS_REMOVED_CTA:"Kirjaudu sisÃ¤Ã¤n",CONNECTOR_NOTIFICATION_HEADING:"<%= connectorDisplayName %> â€“ Tiedostot",FETCHING_TRANSCRIPTS:"Haetaan transkriptioitasi..."});
