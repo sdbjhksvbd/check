b919534e3ae4845bfc197b0957c014d1a0eb0546
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:19 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/fi-fi/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"LisÃ¤Ã¤",ADD_BUTTON_ARIA_LABEL:"<%= connectorName %>: lisÃ¤Ã¤",ADD_ACCOUNT_TITLE:"LisÃ¤Ã¤ tili",ADD_MEETING_SOURCE_TITLE:"Microsoft Teams -tili on jo linkitetty.",ADD_ACCOUNT_MESSAGE:"Olet jo luonut tilin tÃ¤tÃ¤ tallennuspalvelua varten. Poista tili ja yritÃ¤ uudelleen.",ADD_MEETING_SOURCE_MESSAGE:"Olet jo linkittÃ¤nyt Microsoft Teams -tilin. Poista kyseinen tili ja yritÃ¤ uudelleen.",ADD_BUTTON_ONE_CONNECTOR:"LisÃ¤Ã¤ tili",CONNECTOR_DESC:"Muodosta yhteys Microsoft SharePoint Serveriin tai SharePoint Onlineen, jos haluat kÃ¤yttÃ¤Ã¤ tiedostojasi sovelluksesta.",COMING_SOON_MSG:"LisÃ¤Ã¤ tilityyppejÃ¤ on tulossa...",ADD_ACCOUNT_HEADER_TITLE:"LisÃ¤Ã¤ tallennustili",ADD_ACCOUNT_HEADER_DESCRIPTION:"Muodosta yhteys tiliisi, jotta voit kÃ¤yttÃ¤Ã¤ tiedostojasi Acrobatista.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Sinut voidaan ohjata verkkoselaimeen sisÃ¤Ã¤nkirjautumista varten.",SIGN_IN_TO_TITLE:"Kirjaudu sisÃ¤Ã¤n kohteeseen <%= connectorName %>",ENTER_EMAIL_ID:"SÃ¤hkÃ¶posti",CHOOSE_ACCOUNT:"Valitse <%= connectorName %>-tili",PERSONAL_ACCOUNT_MSG:"Kirjaudu sisÃ¤Ã¤n <%= MSBrandName %>-tilisi tunnistetiedoilla",BUSINESS_ACCOUNT_MSG:"Kirjaudu sisÃ¤Ã¤n tyÃ¶- tai koulutilisi tunnistetiedoilla",ENTER_EMAIL_ID_TOOLTIP:"Anna lisÃ¤ttÃ¤vÃ¤n <%= connectorName %>-tilin sÃ¤hkÃ¶postiosoite",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"LisÃ¤Ã¤ kokousten transkriptiot",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"KÃ¤ytÃ¤ generatiivisia tekoÃ¤lytoimintoja muun muassa kokousyhteenvetojen,<br/> keskustelunaiheiden ja toimintakohteiden luontiin mistÃ¤ tahansa kokoustranskriptiosta.",ADD_BUTTON_TEAMS_CONNECTOR:"Kirjaudu Teamsiin",ADD_BUTTON_ZOOM_CONNECTOR:"Lue lisÃ¤Ã¤",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"Valitse kokouksen lÃ¤hde tÃ¤Ã¤llÃ¤ tai avaa VTT-tiedosto suoraan Acrobatissa."});
