c196861d8b276a55f07a1ec95590340ed47d9bf9
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:36:54 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/app/dev/nls/root/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({COPYRIGHT:"Â©2012-2015 Adobe Systems Incorporated, All Rights Reserved.",HELP:"Help",PRIVACY:"Privacy",TERMS:"Terms",COOKIES:"Cookies",PRIVACY_POLICY:"Privacy Policy & Cookies",TERMS_OF_USE:"Terms of Use",WELCOME_MSG:"Use Adobe Document Services and access your files from any device",LOAD_FAILURES:"Load Failures",MODULES_NOT_LOADED:"The following modules failed to load:<br/><br/><%= modules %>",SIGN_IN:"Sign In",CREATE_ACCOUNT:"Create Account",FORGOT_PASSWORD:"Trouble Signing In?",EMAIL_VERIFICATION:"Email Verification",PDF_SERVICES:"Â Â /Â Â PDF services",ACCOUNT:"Account",LINK_CREATIVE_CLOUD_ACCOUNT:"Link Creative Cloud Account...",LINK_DROPBOX_ACCOUNT:"Link Dropbox Account...",MANAGE_SUB:"My Plan...",USER_ACCOUNT:"User Account...",ASK_US_A_QUESTION:"Ask Us a Question...",SUPPORT:"Support...",CHAT_WITH_US:"Chat With Us...",ABOUT_APP:"About Document Cloud...",SIGN_OUT:"Sign Out",SHOW_PROGRESS:"Show Progress",PROGRESS_COMPLETE:"Progress Complete",REMOVE_HISTORY:"Remove History",OPERATION_IN_PROGRESS_DLG_TITLE:"Operation in Progress",OPERATION_IN_PROGRESS_DLG_MSG:"There is currently an operation in process. Continuing will cancel that operation. Would you like to continue?",QUOTA_LEGEND:"<%= using %> of <%= allotment %>",STORAGE_EXCEEDED_LEARN_MORE:"Storage Exceeded (<a class='quota-display-learn-more'>Learn more</a>)",ABOUT_DLG_TITLE:"About Document Cloud",ABOUT_DLG_COPYRIGHT_PARA:"Copyright Â© 2012-2015 Adobe Systems Incorporated and its licensors. All Rights Reserved.",ABOUT_DLG_TRADEMARK_PARA:"Adobe, the Adobe logo, and Acrobat are either registered trademarks or trademarks of Adobe Systems Incorporated in the United States and/or other countries. All other trademarks are the property of their respective owners.",ABOUT_DLG_PORTIONS_PARA:"Portions utilize:",ABOUT_DLG_MICROSOFT_PARA:"Microsoft Windows Media Technologies. Copyright Â© 1999-2002, 2006 Microsoft Corporation. All Rights Reserved.",ABOUT_DLG_IRIS_PARA:"OCR technology by Image Recognition Integrated Systems S.A. Copyright Â© 1987 - 2012, I.R.I.S. S.A. All rights reserved.",ABOUT_DLG_SOLID_PARA:"Copyright Â© 2003-2011 Solid Documents, LLC",ABOUT_DLG_THIRD_PARTY_PARA:"Third Party notices, terms and conditions pertaining to third party software can be found at <%= thirdPartyLink %> and are incorporated by reference herein.",ABOUT_DLG_BUILD_NUMBER:"Build: <%= buildNum %>",NO_CONNECTION_TITLE:"No Internet Connection",NO_CONNECTION_MSG:"The features in this area require Internet connectivity. Please connect your computer to the Internet and try again.",NETWORK_FAILURE:"Network Connection Failure",NETWORK_DISCONNECTED:"It appears that there is no network connection. Reader might not be fully functional in the absence of a network connection"});
