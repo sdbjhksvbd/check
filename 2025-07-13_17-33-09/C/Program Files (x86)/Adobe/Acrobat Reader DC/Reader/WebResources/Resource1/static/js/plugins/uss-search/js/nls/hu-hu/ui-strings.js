1701ab53662ff68d1ac9e1c5ff68e30f4af82a35
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:16 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/hu-hu/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"TalÃ¡latok a kÃ¶vetkezÅ‘ keresÃ©si kifejezÃ©sre: <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"BezÃ¡rÃ¡s",SEARCH_CLOSE_LABEL:"BezÃ¡rÃ¡s  KeresÃ©s",SEARCH_SIGNOUT_PROMPT:"A mÃ¡s kategÃ³riÃ¡kban valÃ³ keresÃ©shez <%= prefix %>jelentkezzen be.<%= suffix %>",SEARCH_SIGNIN:"BejelentkezÃ©s",SEARCH_OFFLINE_PROMPT:"KÃ©relme nem hajthatÃ³ vÃ©gre. Hiba tÃ¶rtÃ©nt a szolgÃ¡ltatÃ¡shoz valÃ³ csatlakozÃ¡s kÃ¶zben. KÃ©rjÃ¼k, <%= prefix %>prÃ³bÃ¡lja Ãºjra.<%= suffix %>",SEARCH_ONLINE:"PrÃ³bÃ¡lja Ãºjra",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Nincs talÃ¡lat",SEARCH_NO_RESULTS_MESSAGE:"Nem talÃ¡ltunk eredmÃ©nyeket a kÃ¶vetkezÅ‘ keresÃ©si kifejezÃ©sre: <b><%= searchQuery %></b>.<br>EllenÅ‘rizze, hogy helyesen adta-e meg a kifejezÃ©st, vagy prÃ³bÃ¡lja meg kibÅ‘vÃ­teni a keresÃ©st.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Nem talÃ¡lhatÃ³k eredmÃ©nyek a(z) <%= category %> kategÃ³riÃ¡ban",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Nem talÃ¡ltunk eredmÃ©nyeket a kÃ¶vetkezÅ‘ keresÃ©si kifejezÃ©sre: <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"Hiba tÃ¶rtÃ©nt",SEARCH_CATEGORY_ERROR:"Nem tudtunk keresni ebben a kategÃ³riÃ¡ban.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> TalÃ¡latok szÃ¡ma a(z) <%= query %> lekÃ©rdezÃ©sre: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> TalÃ¡latok szÃ¡ma: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Nem tudtunk keresni ebben a kategÃ³riÃ¡ban. PrÃ³bÃ¡lja Ãºjra kÃ©sÅ‘bb.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
