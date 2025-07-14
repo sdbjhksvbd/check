33fe640bead56f707826104d1db56262600a22af
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:16 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/es-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"Agregar",ADD_BUTTON_ARIA_LABEL:"Agregar <%= connectorName %>",ADD_ACCOUNT_TITLE:"Agregar cuenta",ADD_MEETING_SOURCE_TITLE:"La cuenta de Microsoft Teams ya estÃ¡ vinculada",ADD_ACCOUNT_MESSAGE:"Ya tienes una cuenta configurada para este almacÃ©n. Elimina dicha cuenta y vuelve a intentarlo.",ADD_MEETING_SOURCE_MESSAGE:"Ya has vinculado una cuenta de Microsoft Teams. Elimina esa cuenta e intÃ©ntalo de nuevo.",ADD_BUTTON_ONE_CONNECTOR:"Agregar cuenta",CONNECTOR_DESC:"ConÃ©ctate a Microsoft SharePoint Server o SharePoint Online para acceder a tus archivos desde la aplicaciÃ³n.",COMING_SOON_MSG:"PrÃ³ximamente, mÃ¡s tipos de cuentasâ€¦",ADD_ACCOUNT_HEADER_TITLE:"Agregar cuenta de almacenamiento",ADD_ACCOUNT_HEADER_DESCRIPTION:"Vincula una cuenta para acceder a tus archivos desde Acrobat.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Es posible que debas iniciar sesiÃ³n en un navegador web.",SIGN_IN_TO_TITLE:"Iniciar sesiÃ³n en <%= connectorName %>",ENTER_EMAIL_ID:"Correo electrÃ³nico",CHOOSE_ACCOUNT:"Elegir cuenta de <%= connectorName %>",PERSONAL_ACCOUNT_MSG:"Inicia sesiÃ³n con tu cuenta de <%= MSBrandName %>",BUSINESS_ACCOUNT_MSG:"Inicia sesiÃ³n con tu cuenta de trabajo o centro educativo",ENTER_EMAIL_ID_TOOLTIP:"Escribe el correo electrÃ³nico de la cuenta de <%= connectorName %> que deseas agregar.",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"AÃ±adir transcripciones de reuniones",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Utiliza las funciones de la IA generativa para crear resÃºmenes de reuniones, temas de<br/> debate, medidas y mÃ¡s desde la transcripciÃ³n de cualquier reuniÃ³n.",ADD_BUTTON_TEAMS_CONNECTOR:"Iniciar sesiÃ³n en Teams",ADD_BUTTON_ZOOM_CONNECTOR:"MÃ¡s informaciÃ³n",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"Elige el origen de una reuniÃ³n aquÃ­ o abre un archivo VTT directamente en Acrobat."});
