f04af73cb3bd85ef1e44036c3a5500f274b97c1b
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:09 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/ca-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"Escanejos",SCAN_FILES_TITLE:"Obteniu l'aplicaciÃ³ gratuÃ¯ta Adobe Scan",SCAN_FILES_READER_CONTENT:"Convertiu el telÃ¨fon o la tauleta en un escÃ ner portÃ til per escanejar qualsevol cosa en PDF. Descarregueu l'aplicaciÃ³ ara.",SCAN_FILES_SIGN_IN_CONTENT:"Ja sou usuari de l'Adobe Scan? Inicieu la sessiÃ³ per accedir a les vostres digitalitzacions.",SCAN_FILES_CONTENT:"Convertiu el telÃ¨fon o la tauleta en un escÃ ner portÃ til per escanejar qualsevol cosa en PDF. Descarregueu l'aplicaciÃ³ ara.",NO_SCAN_TEXT:"No tens cap escaneig disponible.",SCAN_FILES_QR_CODE_TITLE:"ObtÃ©n l'aplicaciÃ³ gratuÃ¯ta Adobe Scan",SCAN_FILES_QR_CODE_CONTENT:"Escanegeu el codiÂ QR per convertir el que vulgueu en un PDF amb el dispositiu mÃ²bil i accediu als vostres PDF digitalitzats aquÃ­."});
