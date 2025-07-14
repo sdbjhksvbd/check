f2bc3969f0f98554885943bc5aae911e42030d3f
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:11:55 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/da-dk/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"SÃ¸geresultater for <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Luk",SEARCH_CLOSE_LABEL:"Luk sÃ¸gning",SEARCH_SIGNOUT_PROMPT:"For at sÃ¸ge i andre kategorier skal du <%= prefix %>logge pÃ¥.<%= suffix %>",SEARCH_SIGNIN:"Log pÃ¥ ",SEARCH_OFFLINE_PROMPT:"Din anmodning kunne ikke fuldfÃ¸res. Der opstod en fejl under forsÃ¸get pÃ¥ at oprette forbindelse til tjenesten. <%= prefix %>PrÃ¸v igen.<%= suffix %>",SEARCH_ONLINE:"PrÃ¸v igen",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Ingen resultater fundet",SEARCH_NO_RESULTS_MESSAGE:"Vi kunne ikke finde nogen resultater for <b><%= searchQuery %></b>.<br> Tjek for stavefejl, eller prÃ¸v at udvide din sÃ¸gning.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Ingen resultater fundet i <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Vi kunne ikke finde nogen resultater for <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"Der opstod en fejl",SEARCH_CATEGORY_ERROR:"Vi kunne ikke sÃ¸ge i denne kategori.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> SÃ¸geresultater for <%= query %> : <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> SÃ¸geresultater : <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Vi kunne ikke sÃ¸ge i denne kategori. PrÃ¸v igen senere.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
