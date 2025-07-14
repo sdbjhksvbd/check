b07ddaa2dd51b6f88fa45ff7ec5c3411b83b1b75
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:05 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/eu-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"<b><%= searchQuery %></b> bilaketaren emaitzak",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Itxi",SEARCH_CLOSE_LABEL:"Itxi Bilatu",SEARCH_SIGNOUT_PROMPT:"Beste kategoria batzuk bilatzeko, <%= prefix %>hasi saioa.<%= suffix %>",SEARCH_SIGNIN:"Hasi saioa",SEARCH_OFFLINE_PROMPT:"Ezin izan da osatu eskaera. Arazo bat izan da zerbitzura konektatzean. <%= prefix %>Saiatu berriro.<%= suffix %>",SEARCH_ONLINE:"Saiatu berriro",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Ez da aurkitu emaitzarik",SEARCH_NO_RESULTS_MESSAGE:"Ezin izan dugu aurkitu <b><%= searchQuery %></b> bilaketaren emaitzarik.<br> Egiaztatu ortografia eta zabaldu bilaketa.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Ez da aurkitu emaitzarik <%= category %> kategorian",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Ezin izan dugu aurkitu <b><%= searchQuery %></b> bilaketaren emaitzarik.",SEARCH_NO_RESULTS_ERROR:"Arazoren bat izan da",SEARCH_CATEGORY_ERROR:"Ezin izan dugu aurkitu kategoria hau.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> Bilaketa-emaitzak <%= query %> kontsultarako: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> Bilaketa-emaitzak: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Ezin izan dugu bilatu kategoria hau. Saiatu berriro geroago.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
