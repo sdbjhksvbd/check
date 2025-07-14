dbe8d21cb88deb9bcfc1bd31128bbb72a328a2fd
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:45:25 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/editpdf/js/nls/fr-fr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({EDIT_PDF:"Modifier les fichiers PDF",EDIT_PDF_TOOL_VIEW_TITLE:"Modifier facilement du texte et des images dans des documents PDF avec AcrobatÂ Pro",EDIT_PDF_TOOL_VIEW_TITLE_RHP:"Modifier facilement du texte et des images des documents PDF",EDIT_PDF_ICON_TOOLTIP:"En savoir plus",EDIT_PDF_START:"En savoir plus",EDIT_PDF_HEADING:"Adobe AcrobatÂ Pro",EDIT_PDF_FULL_GO_URL:"http://www.adobe.com/go/editrdr_12_0_0_fr",EDIT_PDF_RHP_GO_URL:"http://www.adobe.com/go/editrhprdr_12_0_0_fr"});
