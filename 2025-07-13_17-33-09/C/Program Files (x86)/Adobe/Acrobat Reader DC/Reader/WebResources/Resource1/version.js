dab5df922e5fedc34fe283336650bb8b7db3228c
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:07:12 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/version.js
@@ -0,0 +1,122 @@
+{
+    "aicuc": {
+        "plugin_ids": [
+            "createpdf",
+            "createpdf_rhp",
+            "exportpdf",
+            "exportpdf_rhp",
+            "combineconvertpdf_rhp"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/aicuc",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+    "framework": {
+        "plugin_ids": ["root"],
+        "rna-version": 1,
+        "git_path": ".",
+        "git_commit": "",
+        "git_repo": "framework"
+    },
+    "my_files": {
+        "plugin_ids": [
+            "my_files"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/my-files",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+    "fss": {
+        "plugin_ids": [
+            "fillsign"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/fss",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+    "tracked-send": {
+        "plugin_ids": [
+            "sentfiles",
+            "send"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/tracked-send",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+    "ccx": {
+        "plugin_ids": [
+            "ccx"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/ccx",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+	"send_for_sign": {
+        "plugin_ids": [
+            "send_for_sign"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/send-for-sign",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+    "unified_share": {
+        "plugin_ids": [
+            "unified_share"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/unified-share",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+    "sign_services_auth": {
+        "plugin_ids" : [
+            "sign_services_auth"
+        ],
+        "rna-version": 1,
+        "git_path" : "12X/static/js/plugins/sign-services-auth",
+        "git_commit" : "",
+        "git_repo" : "plugins"
+    },
+    "reviews": {
+        "plugin_ids": [
+            "reviews"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/reviews",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+    "signatures": {
+        "plugin_ids": [
+            "signatures"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/signatures",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+    "activity_badge": {
+        "plugin_ids": [
+            "activity_badge"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/activity-badge",
+        "git_commit": "",
+        "git_repo": "plugins"
+    },
+    "app_center": {
+        "plugin_ids": [
+            "app_center"
+        ],
+        "rna-version": 1,
+        "git_path": "12X/static/js/plugins/app-center",
+        "git_commit": "",
+        "git_repo": "plugins"
+    }
+}
