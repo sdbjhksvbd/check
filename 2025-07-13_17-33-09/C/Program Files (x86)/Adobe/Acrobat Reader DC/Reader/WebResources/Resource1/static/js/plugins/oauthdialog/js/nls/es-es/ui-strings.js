a78dcfbd5eefa2dc58f45f81bfaffaaab6eb2344
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:06 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/es-es/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Esperandoâ€¦",OAUTHDIALOG_2:"Hemos abierto una nueva pestaÃ±a del navegador para que inicies sesiÃ³n.",OAUTHDIALOG_3:"Tras completar la autenticaciÃ³n en el navegador, continuarÃ¡s automÃ¡ticamente dentro de la aplicaciÃ³n.",OAUTHDIALOG_TROUBLE:"Â¿No puedes entrar?",OAUTHDIALOG_TIMEOUT_1:"No hemos podido completar el inicio de sesiÃ³n. IntÃ©ntalo de nuevo.",CANCEL_BUTTON_TEXT:"Cancelar",CANCEL_BUTTON_TEXT_2:"Cerrar",OAUTHDIALOG_HEADING_2:"Error de inicio de sesiÃ³n"});
