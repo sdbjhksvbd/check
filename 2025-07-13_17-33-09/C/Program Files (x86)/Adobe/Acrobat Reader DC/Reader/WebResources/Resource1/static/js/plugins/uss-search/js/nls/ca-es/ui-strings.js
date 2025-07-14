442ecca592d3e8c2ac01eb6ad4e045a8938a8ce2
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:11:53 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/ca-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"Resultats de la cerca per a <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Tanca",SEARCH_CLOSE_LABEL:"Tanca la cerca",SEARCH_SIGNOUT_PROMPT:"Per cercar altres categories, <%= prefix %>inicieu la sessiÃ³.<%= suffix %>",SEARCH_SIGNIN:"Inicia la sessiÃ³",SEARCH_OFFLINE_PROMPT:"No sâ€™ha pogut completar la solÂ·licitud. Sâ€™ha produÃ¯t un error en connectar amb el servei. <%= prefix %>Torneu-ho a provar.<%= suffix %>",SEARCH_ONLINE:"Torna-ho a provar",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"No s'ha trobat cap resultat",SEARCH_NO_RESULTS_MESSAGE:"No hem trobat cap resultat per a <b><%= searchQuery %></b>.<br> Comproveu-ne l'ortografia o mireu de fer una cerca mÃ©s genÃ¨rica.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"No s'ha trobat cap resultat a <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"No hem trobat cap resultat per a <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"S'ha produÃ¯t un error",SEARCH_CATEGORY_ERROR:"No hem pogut cercar aquesta categoria.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"Resultats de la cerca (<%= category %>) per a <%= query %>: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"Resultats de la cerca (<%= category %>): <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"No hem pogut cercar aquesta categoria. Torneu-ho a provar mÃ©s tard.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
