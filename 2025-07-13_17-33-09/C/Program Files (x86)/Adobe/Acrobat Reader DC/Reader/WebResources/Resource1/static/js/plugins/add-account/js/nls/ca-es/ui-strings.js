a190fdd550cd6bf4024bf3fc83874552c44d8b28
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:05 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/ca-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"Afegeix",ADD_BUTTON_ARIA_LABEL:"Afegeix el connector <%= connectorName %>",ADD_ACCOUNT_TITLE:"Afegeix un compte",ADD_MEETING_SOURCE_TITLE:"El compte de Microsoft Teams ja estÃ  enllaÃ§at",ADD_ACCOUNT_MESSAGE:"Ja teniu configurat un compte per a aquest magatzem. Suprimiu aquest compte i torneu-ho a provar.",ADD_MEETING_SOURCE_MESSAGE:"Ja heu enllaÃ§at un compte de Microsoft Teams. Elimineu el compte i torneu-ho a provar.",ADD_BUTTON_ONE_CONNECTOR:"Afegeix un compte",CONNECTOR_DESC:"Connecteu-vos al Microsoft SharePoint Server o al SharePoint Online per accedir als vostres fitxers des de l'aplicaciÃ³.",COMING_SOON_MSG:"Properament s'afegiran mÃ©s tipus de comptes...",ADD_ACCOUNT_HEADER_TITLE:"Afegiu un compte d'emmagatzematge",ADD_ACCOUNT_HEADER_DESCRIPTION:"Connecteu-vos al vostre compte per accedir als fitxers des de l'Acrobat.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Ã‰s possible que se us redirigeixi a un navegador web per iniciar la sessiÃ³.",SIGN_IN_TO_TITLE:"Inicieu la sessiÃ³ a <%= connectorName %>",ENTER_EMAIL_ID:"Correu electrÃ²nic ",CHOOSE_ACCOUNT:"Trieu el compte de <%= connectorName %>",PERSONAL_ACCOUNT_MSG:"Inicieu la sessiÃ³ amb el vostre compte de <%= MSBrandName %>",BUSINESS_ACCOUNT_MSG:"Inicieu la sessiÃ³ amb el vostre compte professional o del centre educatiu",ENTER_EMAIL_ID_TOOLTIP:"IntroduÃ¯u el correu electrÃ²nic del compte <%= connectorName %> que vulgueu afegir.",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"Afegiu transcripcions de les reunions",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Utilitzeu les funcions dâ€™IA generativa per crear resums de les reunions, temes de <br/>debat, mesures i molt mÃ©s a partir dâ€™una transcripciÃ³ dâ€™una reuniÃ³.",ADD_BUTTON_TEAMS_CONNECTOR:"Inicia sessiÃ³ a Teams",ADD_BUTTON_ZOOM_CONNECTOR:"MÃ©s informaciÃ³",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"Trieu una font per a la reuniÃ³ aquÃ­ o obriu un fitxer VTT directament a Acrobat."});
