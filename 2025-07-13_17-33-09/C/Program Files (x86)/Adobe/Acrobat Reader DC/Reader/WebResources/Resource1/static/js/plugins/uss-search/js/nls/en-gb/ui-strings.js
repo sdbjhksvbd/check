8a9e1a1a036bd2878ce0306b61c4a2da8f5cbb25
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:00 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/en-gb/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"[__ _Search Results ___]",SEARCH_COUNT:"[__ (<%=count%>) __]",SEARCH_CLOSE:"[__Close__]",SEARCH_SIGNOUT_PROMPT:"[__ _ _ _ _ _ Please sign in to see results from Document Cloud. _ _ _ _ _ __]",SEARCH_SIGNIN:"[__SignIn__]",SEARCH_OFFLINE_PROMPT:"[__ _ _ _ _ _ _ _ _ _You are offline. Please connect to Internet to see results from Document Cloud. _ _ _ _ _ _ _ _ ___]",SEARCH_ONLINE:"[__Try Again__]",GENERIC_ARIA_LABEL_STRING:"[__ _ _ <%= prefix %>. <%= suffix %> _ _ __]"});
