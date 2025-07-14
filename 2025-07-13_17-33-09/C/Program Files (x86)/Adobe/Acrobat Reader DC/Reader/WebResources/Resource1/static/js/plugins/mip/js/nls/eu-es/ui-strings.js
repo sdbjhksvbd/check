56e51a9643a9155511303d2fb3e5ab5b1727174c
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:48:52 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/eu-es/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Hautatu konfidentzialtasun-etiketa bat",CANCEL_BUTTON_TEXT:"Utzi",UPDATE_BUTTON_TEXT:"Eguneratu",APPLY_BUTTON_TEXT:"Aplikatu",BACK_BUTTON_TEXT:"Atzera",SUBMIT_BUTTON_TEXT:"Bidali",SAVE_BUTTON_TEXT:"Gorde",ADD_BUTTON_TEXT:"Gehitu",JUSTIFICATION_REQUIRED:"Azalpen bat behar da",JUSTIFICATION_HEAD_TEXT:"Erakundeak sentikortasun-etiketa aldatzeko arrazoi bat ematea eskatu dizu.",JUSTIFICATION_OPTION_1:"Aurreko etiketa ez da aplikatzen jada",JUSTIFICATION_OPTION_2:"Aurreko etiketa ez zen zuzena",JUSTIFICATION_OPTION_OTHER:"Beste bat (azaldu): ez idatzi kontuzko daturik",ENTER_JUSTIFICATION:"Idatzi arrazoi bat",DELETE_LABEL:"Ezabatu  etiketa",LABEL_DETAILS_TOOLTIP:"Sentikortasun-etiketaren xehetasunak",SPECIFY_USER_OR_GROUP:"Zehaztu erabiltzaile, talde edo domeinu bat",DEFINE_LABEL_PERMISSION:"Definitu etiketaren baimena",MORE_OPTIONS:"Aukera gehiago",CAN_READ:"Irakur dezake",CAN_CHANGE:"Aldaketak egin ditzake",CAN_FULL_CONTROL:"Kontrol osoa izan dezake",READ:"Irakurri",READ_COPY:"Irakurri (kopia dezake)",READ_PRINT:"Irakurri (inprima dezake)",READ_COPY_PRINT:"Irakurri (kopiatu eta inprima dezake)",CHANGE:"Aldatu",CHANGE_PRINT:"Aldatu (inprima dezake)",FULL_CONTROL:"Kontrol osoa",DELETE:"Ezabatu",CUSTOM:"Pertsonalizatua",OWNER:"Jabea",OWNER_SIGNAL:"(zu)",DOCUMENT_EXPIRY:"Dokumentuaren iraungitze-data: ",READ_PERMISSION_TEXT:"Irakurtzeko baimena dutenek hauek egiteko baimena ere dute:",COPY_CHECKBOX:"Kopiatu dokumentu hau",CHANGE_PERMISSION_TEXT:"Irakurtzeko edo aldatzeko baimenak dituztenek hauek egiteko baimena ere dute:",PRINT_CHECKBOX:"Inprimatu dokumentu hau",MMDDYYYY_PLACEHOLDER:"hh/ee/uuuu",EXPIRY_DATE_ERROR:"Idatzi balio duen iraungitze-data bat",UDP_EDIT_LABEL:"Editatu etiketa-baimenak",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"GEHITUTAKO HARTZAILEAK",GET_MS_GRAPH_CONTACTS_MSG:"Outlook kontaktuak atzitzeko, hasi saioa Office kontuarekin.",GET_MS_GRAPH_CONTACTS_MSG_1:"Hasi saioa honekin: ",GET_MS_GRAPH_CONTACTS_MSG_2:" Sartzeko kontua ",GET_MS_GRAPH_CONTACTS_MSG_3:" kontaktuak.",EMAIL_REMOVE:"Kendu helbide elektronikoa"});
