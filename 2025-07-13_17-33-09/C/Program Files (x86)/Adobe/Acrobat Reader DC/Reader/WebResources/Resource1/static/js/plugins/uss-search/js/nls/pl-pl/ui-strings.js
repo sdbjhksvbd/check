430273e62c0ba69d6f9401555d0dc0827b89b853
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:22 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/pl-pl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"Wyniki wyszukiwania dla <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Zamknij",SEARCH_CLOSE_LABEL:"Zamknij  Szukaj",SEARCH_SIGNOUT_PROMPT:"Aby przeszukaÄ‡ inne kategorie, <%= prefix %>zaloguj siÄ™.<%= suffix %>",SEARCH_SIGNIN:"Zaloguj siÄ™",SEARCH_OFFLINE_PROMPT:"Nie moÅ¼na ukoÅ„czyÄ‡ Å¼Ä…dania. Podczas Å‚Ä…czenia z usÅ‚ugÄ… wystÄ…piÅ‚ bÅ‚Ä…d. <%= prefix %>SprÃ³buj ponownie.<%= suffix %>",SEARCH_ONLINE:"SprÃ³buj ponownie",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Brak wynikÃ³w",SEARCH_NO_RESULTS_MESSAGE:"Nie udaÅ‚o siÄ™ znaleÅºÄ‡ Å¼adnych wynikÃ³w dla <b><%= searchQuery %></b>.<br> SprawdÅº pisowniÄ™ lub sprÃ³buj rozszerzyÄ‡ wyszukiwanie.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Nie znaleziono wynikÃ³w w kategorii <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Nie udaÅ‚o siÄ™ znaleÅºÄ‡ Å¼adnych wynikÃ³w dla <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"CoÅ› poszÅ‚o nie tak",SEARCH_CATEGORY_ERROR:"Nie byliÅ›my w stanie przeszukaÄ‡ tej kategorii.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"Wyniki wyszukiwania w kategorii <%= category %> dla zapytania <%= query %>: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"Wyniki wyszukiwania w kategorii <%= category %>: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Nie byliÅ›my w stanie przeszukaÄ‡ tej kategorii. SprÃ³buj ponownie pÃ³Åºniej.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
