808a4c99eef1391b3f631cb560879a1eb2948e65
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:10 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/fi-fi/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Odotetaan...",OAUTHDIALOG_2:"Olemme avanneet uuden selainvÃ¤lilehden sisÃ¤Ã¤nkirjautumista varten.",OAUTHDIALOG_3:"Kun olet suorittanut todennuksen selaimessa, jatkat automaattisesti sovelluksessa.",OAUTHDIALOG_TROUBLE:"Kirjautumisongelmia?",OAUTHDIALOG_TIMEOUT_1:"SisÃ¤Ã¤nkirjautuminen epÃ¤onnistui. YritÃ¤ uudelleen.",CANCEL_BUTTON_TEXT:"Peruuta",CANCEL_BUTTON_TEXT_2:"Sulje",OAUTHDIALOG_HEADING_2:"SisÃ¤Ã¤nkirjautuminen epÃ¤onnistui"});
