9b002a1bfa1139447725a44093e1939a9b3cfc31
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:48:58 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/fr-ma/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"SÃ©lectionner une Ã©tiquette de sensibilitÃ©",CANCEL_BUTTON_TEXT:"Annuler",UPDATE_BUTTON_TEXT:"Mettre Ã  jour",APPLY_BUTTON_TEXT:"Appliquer",BACK_BUTTON_TEXT:"Retour",SUBMIT_BUTTON_TEXT:"Envoyer",SAVE_BUTTON_TEXT:"Enregistrer",ADD_BUTTON_TEXT:"Ajouter",JUSTIFICATION_REQUIRED:"Justification requise",JUSTIFICATION_HEAD_TEXT:"Votre entreprise a besoin d'une justification pour modifier le libellÃ© de sensibilitÃ©.",JUSTIFICATION_OPTION_1:"Le libellÃ© prÃ©cÃ©dent ne s'applique plus",JUSTIFICATION_OPTION_2:"Le libellÃ© prÃ©cÃ©dent Ã©tait incorrect",JUSTIFICATION_OPTION_OTHER:"Autre (expliquer) - Ne saisissez pas de donnÃ©es sensibles",ENTER_JUSTIFICATION:"Saisir une justification",DELETE_LABEL:"Supprimer le libellÃ©",LABEL_DETAILS_TOOLTIP:"DÃ©tails du libellÃ© de sensibilitÃ©",SPECIFY_USER_OR_GROUP:"PrÃ©ciser l'utilisateur, le groupe ou le domaine",DEFINE_LABEL_PERMISSION:"DÃ©finir l'autorisation de libellÃ©",MORE_OPTIONS:"Autres options",CAN_READ:"Peut lire",CAN_CHANGE:"Peut modifier",CAN_FULL_CONTROL:"Peut tout contrÃ´ler",READ:"Lire",READ_COPY:"Lire (peut copier)",READ_PRINT:"Lire (peut imprimer)",READ_COPY_PRINT:"Lire (peut copier et imprimer)",CHANGE:"Modifier",CHANGE_PRINT:"Modifier (peut imprimer)",FULL_CONTROL:"ContrÃ´le total",DELETE:"Supprimer",CUSTOM:"Personnaliser",OWNER:"PropriÃ©taire",OWNER_SIGNAL:"(vous)",DOCUMENT_EXPIRY:"Ce document expire le ",READ_PERMISSION_TEXT:"Les personnes autorisÃ©es Ã  lire sont Ã©galement autorisÃ©es Ã ",COPY_CHECKBOX:"Copier ce document",CHANGE_PERMISSION_TEXT:"Les personnes autorisÃ©es Ã  lire ou Ã  modifier sont Ã©galement autorisÃ©es Ã ",PRINT_CHECKBOX:"Imprimer ce document",MMDDYYYY_PLACEHOLDER:"mm/jj/aaaa",EXPIRY_DATE_ERROR:"Saisir une date d'expiration valide",UDP_EDIT_LABEL:"Modifier les autorisations dâ€™Ã©tiquette",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"DESTINATAIRES AJOUTÃ‰S",GET_MS_GRAPH_CONTACTS_MSG:"Connectez-vous avec votre compte Office pour accÃ©der aux contacts Outlook.",GET_MS_GRAPH_CONTACTS_MSG_1:"Connectez-vous avec votre compte ",GET_MS_GRAPH_CONTACTS_MSG_2:" pour accÃ©der aux ",GET_MS_GRAPH_CONTACTS_MSG_3:" contacts.",EMAIL_REMOVE:"Supprimer l'e-mail"});
