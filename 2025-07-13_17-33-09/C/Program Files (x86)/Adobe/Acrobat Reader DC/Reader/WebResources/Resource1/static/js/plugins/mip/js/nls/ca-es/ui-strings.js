93af34a5b64ca672e233d12fc8e72629ffd82c8c
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:48:42 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/ca-es/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Seleccioneu una etiqueta de confidencialitat",CANCEL_BUTTON_TEXT:"CancelÂ·la",UPDATE_BUTTON_TEXT:"Actualitza",APPLY_BUTTON_TEXT:"Aplica",BACK_BUTTON_TEXT:"Enrere",SUBMIT_BUTTON_TEXT:"Envia",SAVE_BUTTON_TEXT:"Desa",ADD_BUTTON_TEXT:"Afegeix",JUSTIFICATION_REQUIRED:"JustificaciÃ³ necessÃ ria",JUSTIFICATION_HEAD_TEXT:"Lâ€™organitzaciÃ³ requereix una justificaciÃ³ per canviar lâ€™etiqueta de confidencialitat.",JUSTIFICATION_OPTION_1:"Lâ€™etiqueta anterior ja no sâ€™aplica",JUSTIFICATION_OPTION_2:"Lâ€™etiqueta anterior era incorrecta",JUSTIFICATION_OPTION_OTHER:"Altres (expliqueu sense introduir dades confidencials)",ENTER_JUSTIFICATION:"IntroduÃ¯u la justificaciÃ³",DELETE_LABEL:"Suprimeix lâ€™etiqueta",LABEL_DETAILS_TOOLTIP:"Detalls de lâ€™etiqueta de confidencialitat",SPECIFY_USER_OR_GROUP:"Especifiqueu usuari, grup o domini",DEFINE_LABEL_PERMISSION:"Definiu el permÃ­s dâ€™etiquetes",MORE_OPTIONS:"MÃ©s opcions",CAN_READ:"Pot llegir",CAN_CHANGE:"Pot canviar",CAN_FULL_CONTROL:"Pot controlar totalment",READ:"Llegir",READ_COPY:"Llegir (pot copiar)",READ_PRINT:"Llegir (pot imprimir)",READ_COPY_PRINT:"Llegir (pot copiar i imprimir)",CHANGE:"Canvia",CHANGE_PRINT:"Canviar (pot imprimir)",FULL_CONTROL:"Control total",DELETE:"Suprimeix",CUSTOM:"Personalitzat",OWNER:"Propietari",OWNER_SIGNAL:"(vÃ³s)",DOCUMENT_EXPIRY:"Aquest document caduca el dia ",READ_PERMISSION_TEXT:"Les persones amb permÃ­s de lectura tambÃ© poden fer-ho",COPY_CHECKBOX:"Copiar aquest document",CHANGE_PERMISSION_TEXT:"Les persones amb permÃ­s de lectura o canvi tambÃ© poden fer-ho",PRINT_CHECKBOX:"Imprimir aquest document",MMDDYYYY_PLACEHOLDER:"mm/dd/aaaa",EXPIRY_DATE_ERROR:"IntroduÃ¯u una data de caducitat vÃ lida",UDP_EDIT_LABEL:"Edita els permisos de les etiquetes",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"DESTINATARIS AFEGITS",GET_MS_GRAPH_CONTACTS_MSG:"Inicieu la sessiÃ³ al compte de lâ€™Office per accedir als contactes de lâ€™Outlook.",GET_MS_GRAPH_CONTACTS_MSG_1:"Inicieu la sessiÃ³ amb ",GET_MS_GRAPH_CONTACTS_MSG_2:" compte per entrar ",GET_MS_GRAPH_CONTACTS_MSG_3:" Contactes.",EMAIL_REMOVE:"Suprimeix lâ€™adreÃ§a electrÃ²nica"});
