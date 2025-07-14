85cbbcfa1ff465a4bacf865cddc20138a2daa031
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:52 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/it-it/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"Computer",SELECT_FILES:"Scegliete i file dal computer",NO_RECENT_FOLDERS_SELECT:"Fate clic su Sfoglia per aprire un file dal computer.",HEADING_RECENT_LOCATIONS:"Percorsi recenti",HEADING_SELECT_FROM_RECENT_FOLDER:"Seleziona da cartella Recente...",HEADING_SELECT_FOLDER:"Salva in cartella Recente...",BROWSE_BUTTON:"Sfoglia",BROWSE_DIALOG_BUTTON:"Sfoglia...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"Scegli una cartella diversa...",BROWSE_FOLDER_TEXT:"Seleziona una cartella diversa",NO_RECENT_FOLDERS_SAVE_AS:"Nessuna cartella recente",NO_FOLDERS_MYCOMPUTER_PROMPT:"Non vi Ã¨ ancora alcun file.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"Quando apri i file dal computer, potrai vedere qui le relative cartelle."});
