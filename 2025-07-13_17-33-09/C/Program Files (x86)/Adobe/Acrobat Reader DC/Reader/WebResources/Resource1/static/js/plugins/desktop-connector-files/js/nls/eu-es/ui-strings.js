11237211b93c251f4759bb918c34d4ce6d28af41
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:33 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/eu-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"<%= connectorLabel %> fitxategiak",GENERIC_ERROR_TITLE:"Errorea",GENERIC_ERROR_MSG:"Errore bat gertatu da fitxategiak eskuratzean.",CONNECTOR_FOOTER_ANCHOR_TEXT:"Joan <%= connectorDisplayName %> webgunera",ACCOUNT_REMOVED_MESSAGE:"Acrobat-ek ezin du atzitu <%= connectorDisplayName %> kontua",ACCESS_REMOVED_ACTION:"Hasi saioa <%= connectorDisplayName %> kontuan fitxategiak atzitzeko.",ACCESS_REMOVED_CTA:"Hasi saioa",CONNECTOR_NOTIFICATION_HEADING:"<%= connectorDisplayName %> Fitxategiak",FETCHING_TRANSCRIPTS:"Transkripzioak eskuratzenâ€¦"});
