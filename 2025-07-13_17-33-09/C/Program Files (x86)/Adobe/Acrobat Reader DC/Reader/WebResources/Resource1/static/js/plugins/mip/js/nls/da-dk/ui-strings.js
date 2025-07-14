c217835e2700ac683b51f4f4c13fcc25031ba6c4
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:48:44 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/da-dk/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"VÃ¦lg en fÃ¸lsomhedsetiket",CANCEL_BUTTON_TEXT:"Annuller",UPDATE_BUTTON_TEXT:"Opdater",APPLY_BUTTON_TEXT:"Anvend",BACK_BUTTON_TEXT:"Tilbage",SUBMIT_BUTTON_TEXT:"Send",SAVE_BUTTON_TEXT:"Gem",ADD_BUTTON_TEXT:"TilfÃ¸j",JUSTIFICATION_REQUIRED:"Begrundelse krÃ¦ves",JUSTIFICATION_HEAD_TEXT:"Din organisation krÃ¦ver begrundelse for at skifte fÃ¸lsomhedsetiket.",JUSTIFICATION_OPTION_1:"Den forrige etiket gÃ¦lder ikke lÃ¦ngere",JUSTIFICATION_OPTION_2:"Den forrige etiket var forkert",JUSTIFICATION_OPTION_OTHER:"Andet (forklar) â€“ angiv ikke fÃ¸lsomme data",ENTER_JUSTIFICATION:"Angiv begrundelse",DELETE_LABEL:"Slet etiket",LABEL_DETAILS_TOOLTIP:"Detaljer for fÃ¸lsomhedsetiket",SPECIFY_USER_OR_GROUP:"Angiv bruger, gruppe eller domÃ¦ne",DEFINE_LABEL_PERMISSION:"Definer etikettilladelse",MORE_OPTIONS:"Flere indstillinger",CAN_READ:"Kan lÃ¦se",CAN_CHANGE:"Kan skifte",CAN_FULL_CONTROL:"Kan fuldt styre",READ:"LÃ¦s",READ_COPY:"LÃ¦s (kan kopiere)",READ_PRINT:"LÃ¦s (kan udskrive)",READ_COPY_PRINT:"LÃ¦s (kan kopiere og udskrive)",CHANGE:"Skift",CHANGE_PRINT:"Skift (kan udskrive)",FULL_CONTROL:"Fuld kontrol",DELETE:"Slet",CUSTOM:"Brugerdefineret",OWNER:"Ejer",OWNER_SIGNAL:"(dig)",DOCUMENT_EXPIRY:"Dette dokument udlÃ¸ber ",READ_PERMISSION_TEXT:"Personer med lÃ¦setilladelse kan ogsÃ¥",COPY_CHECKBOX:"Kopiere dette dokument",CHANGE_PERMISSION_TEXT:"Personer med lÃ¦se- eller skiftetilladelse kan ogsÃ¥",PRINT_CHECKBOX:"Udskrive dette dokument",MMDDYYYY_PLACEHOLDER:"dd/mm/Ã¥Ã¥Ã¥Ã¥",EXPIRY_DATE_ERROR:"Angiv en gyldig udlÃ¸bsdato",UDP_EDIT_LABEL:"Rediger labeltilladelser",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"TILFÃ˜JEDE MODTAGERE",GET_MS_GRAPH_CONTACTS_MSG:"Log pÃ¥ med din Office-konto for at fÃ¥ adgang til Outlook-kontakter.",GET_MS_GRAPH_CONTACTS_MSG_1:"Log pÃ¥ med din ",GET_MS_GRAPH_CONTACTS_MSG_2:" konto for at fÃ¥ adgang til ",GET_MS_GRAPH_CONTACTS_MSG_3:" kontakter.",EMAIL_REMOVE:"Fjern mail"});
