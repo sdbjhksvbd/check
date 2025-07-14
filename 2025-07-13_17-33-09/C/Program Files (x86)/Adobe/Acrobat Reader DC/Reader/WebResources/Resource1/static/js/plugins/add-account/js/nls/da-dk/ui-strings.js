1d5888a8ffa7a0eae331094db148f2b7d4b060b0
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:07 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/da-dk/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"TilfÃ¸j",ADD_BUTTON_ARIA_LABEL:"<%= connectorName %> TilfÃ¸j",ADD_ACCOUNT_TITLE:"TilfÃ¸j konto",ADD_MEETING_SOURCE_TITLE:"Microsoft Teams-kontoen er allerede tilknyttet",ADD_ACCOUNT_MESSAGE:"Du har allerede en konto til denne lagerkonfiguration. Fjern kontoen, og prÃ¸v sÃ¥ igen.",ADD_MEETING_SOURCE_MESSAGE:"Du har allerede tilknyttet en Microsoft Teams-konto. Fjern den konto, og prÃ¸v igen.",ADD_BUTTON_ONE_CONNECTOR:"TilfÃ¸j konto",CONNECTOR_DESC:"Opret forbindelse til Microsoft SharePoint Server eller SharePoint Online for at fÃ¥ adgang til dine filer inden i programmet.",COMING_SOON_MSG:"Flere kontotyper kommer snart...",ADD_ACCOUNT_HEADER_TITLE:"TilfÃ¸j en lagerkonto",ADD_ACCOUNT_HEADER_DESCRIPTION:"Opret forbindelse til din konto for at fÃ¥ adgang til dine filer inde fra Acrobat.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Du omdirigeres muligvis til en webbrowser for at logge pÃ¥.",SIGN_IN_TO_TITLE:"Log pÃ¥ <%= connectorName %>",ENTER_EMAIL_ID:"E-mail",CHOOSE_ACCOUNT:"VÃ¦lg <%= connectorName %>-konto",PERSONAL_ACCOUNT_MSG:"Log pÃ¥ med din <%= MSBrandName %>-konto",BUSINESS_ACCOUNT_MSG:"Log pÃ¥ med din arbejds- eller skolekonto",ENTER_EMAIL_ID_TOOLTIP:"Skriv mailadressen pÃ¥ den <%= connectorName %>-konto, du vil tilfÃ¸je",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"TilfÃ¸j mÃ¸detransskriptioner",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Brug generative AI-funktioner til at generere mÃ¸deopsummeringer, <br/>diskussionsemner, handlingspunkter og mere fra enhver mÃ¸detransskription.",ADD_BUTTON_TEAMS_CONNECTOR:"Log pÃ¥ Teams",ADD_BUTTON_ZOOM_CONNECTOR:"FÃ¥ mere at vide",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"VÃ¦lg en mÃ¸dekilde her, eller Ã¥bn en VTT-fil direkte i Acrobat."});
