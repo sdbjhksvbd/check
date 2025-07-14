61a3ac6f9d0dc890145dcee49716e47566ec872a
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:08:03 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/sign-services-auth/js/nls/tr-tr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SIGNIN_REQUIRED:"Oturum aÃ§Ä±lmasÄ± gerekiyor",PRELOGIN_DIALOG_BODY_DEFAULT:"LÃ¼tfen yeniden oturum aÃ§Ä±n. Bu, Sign sÃ¶zleÅŸmelerinizin gÃ¼venli bir ÅŸekilde saklanmasÄ±na yardÄ±mcÄ± olur.",PRELOGIN_DIALOG_BODY_DOCTAB:"Ä°ptal'i tÄ±klatmak tÃ¼m aÃ§Ä±k Sign sÃ¶zleÅŸmelerini kapatacak.",CHECKBOX_TEXT:"Sonraki 14 gÃ¼n boyunca oturumumu aÃ§Ä±k tut",CANCEL:"Ä°ptal",NEXT:"Devam et",SIGOUT_DIALOG_TITLE:"Adobe Acrobat Reader'dan Ã§Ä±kÄ±ÅŸ yap",SIGOUT_DIALOG_DESCRIPTION_1:'Åžu anda <strong>"<%=emailId%>"</strong> Adobe ID\'niz ile giriÅŸ yapmÄ±ÅŸ durumdasÄ±nÄ±z. "Ã‡Ä±kÄ±ÅŸ yap" seÃ§eneÄŸini tÄ±klattÄ±ÄŸÄ±nÄ±zda bu aygÄ±ttaki uygulamadan Ã§Ä±kÄ±ÅŸ yaparsÄ±nÄ±z. Bu uygulama Ã§Ä±kÄ±ÅŸ yapmanÄ±zÄ± isteyebilir. Uygulama kaldÄ±rÄ±lmaz.',SIGOUT_DIALOG_DESCRIPTION_2:"Bu uygulama ve Adobe ID'nizle ilgili bulut varlÄ±klarÄ±, tekrar giriÅŸ yaptÄ±ÄŸÄ±nÄ±zda kullanÄ±labilir.",SIGOUT_DIALOG_CANCEL_BTN:"Ä°ptal",SIGOUT_DIALOG_SIGNOUT_BTN:"Ã‡Ä±kÄ±ÅŸ Yap"});
