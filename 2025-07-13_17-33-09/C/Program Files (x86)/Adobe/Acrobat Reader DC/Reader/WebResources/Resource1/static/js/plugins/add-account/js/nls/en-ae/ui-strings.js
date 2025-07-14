5693e5b0b52dead32953b65ffdedc691888bea4c
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:10 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/en-ae/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"Add",ADD_BUTTON_ARIA_LABEL:"<%= connectorName %> Add",ADD_ACCOUNT_TITLE:"Add Account",ADD_MEETING_SOURCE_TITLE:"Microsoft Teams account already linked",ADD_ACCOUNT_MESSAGE:"You already have an account for this storage set up. Please remove that account and then try again.",ADD_MEETING_SOURCE_MESSAGE:"You already linked a Microsoft Teams account. Please remove that account and try again.",ADD_BUTTON_ONE_CONNECTOR:"Add Account",CONNECTOR_DESC:"Connect to Microsoft SharePoint Server or SharePoint Online to access your files from within application.",COMING_SOON_MSG:"More account types coming soon...",ADD_ACCOUNT_HEADER_TITLE:"Add a Storage Account",ADD_ACCOUNT_HEADER_DESCRIPTION:"Connect to your account to access your files from within Acrobat.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"You may be redirected to a web browser to sign in.",SIGN_IN_TO_TITLE:"Sign in to <%= connectorName %>",ENTER_EMAIL_ID:"Email",CHOOSE_ACCOUNT:"Choose <%= connectorName %> Account",PERSONAL_ACCOUNT_MSG:"Sign in with your <%= MSBrandName %> account",BUSINESS_ACCOUNT_MSG:"Sign in with your work or school account",ENTER_EMAIL_ID_TOOLTIP:"Enter the email of the <%= connectorName %> account you want to add",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"Add meeting transcripts",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Use generative AI features to create meeting summaries, discussion<br/> topics, action items, and more from any meeting transcript.",ADD_BUTTON_TEAMS_CONNECTOR:"Sign in to Teams",ADD_BUTTON_ZOOM_CONNECTOR:"Learn more",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"Choose a meeting source here or open a VTT file directly in Acrobat."});
