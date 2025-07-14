993764c83751fb80eb00c7962422a35ca162da86
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:42 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/sv-se/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"Skanningar",SCAN_FILES_TITLE:"HÃ¤mta den kostnadsfria appen Adobe Scan",SCAN_FILES_READER_CONTENT:"GÃ¶r telefonen eller surfplattan till en bÃ¤rbar skanner om du vill skanna nÃ¥got till PDF. HÃ¤mta nu.",SCAN_FILES_SIGN_IN_CONTENT:"Ã„r du redan anvÃ¤ndare av Adobe Scan? Logga in fÃ¶r att fÃ¥ Ã¥tkomst till dina scanningar.",SCAN_FILES_CONTENT:"GÃ¶r telefonen eller surfplattan till en bÃ¤rbar skanner om du vill skanna nÃ¥got till PDF. HÃ¤mta nu.",NO_SCAN_TEXT:"Du har inga tillgÃ¤ngliga skanningar.",SCAN_FILES_QR_CODE_TITLE:"HÃ¤mta den kostnadsfria appen Adobe Scan",SCAN_FILES_QR_CODE_CONTENT:"Skanna QR-koden fÃ¶r att konvertera vad du vill till en PDF-fil pÃ¥ din mobila enhet, och fÃ¥ sedan Ã¥tkomst till dina skannade PDF:er hÃ¤r."});
