f54fb543f64d25adda822821ebc1e5c16d7a8289
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:07:11 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/variant.js
@@ -0,0 +1,15 @@
+{
+    "onboarding": [{
+        "range": [
+            1,
+            500
+        ],
+        "variant": "201510_On_Boarding_No_Show_Ctrl"
+    }, {
+        "range": [
+            501,
+            1000
+        ],
+        "variant": "201510_On_Boarding_Show_Test"
+    }]
+}
