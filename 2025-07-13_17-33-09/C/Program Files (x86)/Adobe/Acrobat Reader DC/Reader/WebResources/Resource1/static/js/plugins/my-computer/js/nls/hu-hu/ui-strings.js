745449ad5fe3ead79f29fff70e3beaf03d3e197d
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:51 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/hu-hu/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"SajÃ¡t szÃ¡mÃ­tÃ³gÃ©p",SELECT_FILES:"FÃ¡jlok kivÃ¡lasztÃ¡sa a szÃ¡mÃ­tÃ³gÃ©prÅ‘l",NO_RECENT_FOLDERS_SELECT:"Ha meg kÃ­vÃ¡n nyitni egy fÃ¡jlt a szÃ¡mÃ­tÃ³gÃ©pen, kattintson a TallÃ³zÃ¡s elemre.",HEADING_RECENT_LOCATIONS:"LegutÃ³bbi helyek",HEADING_SELECT_FROM_RECENT_FOLDER:"VÃ¡lasztÃ¡s LegutÃ³bbi mappÃ¡bÃ³l...",HEADING_SELECT_FOLDER:"MentÃ©s LegutÃ³bbi mappÃ¡ba...",BROWSE_BUTTON:"TallÃ³zÃ¡s",BROWSE_DIALOG_BUTTON:"TallÃ³zÃ¡s...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"VÃ¡lasszon egy mÃ¡sik mappÃ¡t...",BROWSE_FOLDER_TEXT:"VÃ¡lasszon mÃ¡sik mappÃ¡t",NO_RECENT_FOLDERS_SAVE_AS:"Nincsenek legutÃ³bbi mappÃ¡k",NO_FOLDERS_MYCOMPUTER_PROMPT:"MÃ©g nincsenek itt fÃ¡jlok.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"Ha a szÃ¡mÃ­tÃ³gÃ©pÃ©rÅ‘l nyit meg fÃ¡jlokat, itt jelennek meg azok mappÃ¡i."});
