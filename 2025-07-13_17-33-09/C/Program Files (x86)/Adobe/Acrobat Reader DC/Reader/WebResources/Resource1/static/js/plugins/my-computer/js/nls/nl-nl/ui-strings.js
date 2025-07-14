d7d554b613e4509cc6e00a03fa6938ae8c55350e
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:56 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/nl-nl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"Deze computer",SELECT_FILES:"Bestanden uit Deze computer kiezen",NO_RECENT_FOLDERS_SELECT:"Klik op Bladeren om een bestand te openen vanaf uw computer.",HEADING_RECENT_LOCATIONS:"Recente locaties",HEADING_SELECT_FROM_RECENT_FOLDER:"Selecteren in map Onlangs geopendâ€¦",HEADING_SELECT_FOLDER:"Opslaan in map Onlangs geopendâ€¦",BROWSE_BUTTON:"Bladeren",BROWSE_DIALOG_BUTTON:"Bladeren...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"Andere map kiezen...",BROWSE_FOLDER_TEXT:"Andere map selecteren",NO_RECENT_FOLDERS_SAVE_AS:"Geen recente mappen",NO_FOLDERS_MYCOMPUTER_PROMPT:"Er zijn hier nog geen bestanden.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"Wanneer u bestanden opent vanaf uw computer, worden de mappen hier weergegeven."});
