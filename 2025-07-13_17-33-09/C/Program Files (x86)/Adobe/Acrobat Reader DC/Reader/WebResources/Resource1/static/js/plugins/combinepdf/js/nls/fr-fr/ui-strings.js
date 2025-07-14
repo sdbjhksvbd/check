81d2358a9d5033c892e53dced2ac38f5a7aa3635
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:41:51 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/combinepdf/js/nls/fr-fr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({COMBINE_PDF:"Combinaison de fichiers PDF",COMBINE_PDF_TOOL_VIEW_TITLE_RHP:"Combinez deux fichiers ou plus dans un mÃªme document PDF.",COMBINE_PDF_ICON_TOOLTIP:"En savoir plus",COMBINE_PDF_START:"En savoir plus",COMBINE_PDF_HEADING:"Adobe AcrobatÂ Pro",COMBINE_PDF_RHP_GO_URL:"http://www.adobe.com/go/combinerhprdr_12_0_11_fr"});
