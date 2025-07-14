88a0ff78c9a36a816c90a8935dfa59b6866c985a
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:01 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/de-de/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Wartet...",OAUTHDIALOG_2:"Wir haben zum Anmelden einen neuen Browser-Tab fÃ¼r dich geÃ¶ffnet.",OAUTHDIALOG_3:"Nachdem du die Authentifizierung im Browser abgeschlossen hast, fÃ¤hrst du automatisch innerhalb der App fort.",OAUTHDIALOG_TROUBLE:"Probleme beim Anmelden?",OAUTHDIALOG_TIMEOUT_1:"Wir haben es nicht geschafft, dich anzumelden. Versuche es erneut.",CANCEL_BUTTON_TEXT:"Abbrechen",CANCEL_BUTTON_TEXT_2:"SchlieÃŸen",OAUTHDIALOG_HEADING_2:"Anmeldung fehlgeschlagen"});
