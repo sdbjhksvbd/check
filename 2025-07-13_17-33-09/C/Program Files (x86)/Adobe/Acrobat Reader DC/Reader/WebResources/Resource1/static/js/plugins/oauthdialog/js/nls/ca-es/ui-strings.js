84a474319cdfb78665c212096d3fbb20a592c524
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:52:58 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/ca-es/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"A l'espera...",OAUTHDIALOG_2:"Hem obert una pestanya nova del navegador perquÃ¨ inicieu la sessiÃ³.",OAUTHDIALOG_3:"Un cop que completeu lâ€™autenticaciÃ³ al navegador, continuareu automÃ ticament a lâ€™aplicaciÃ³.",OAUTHDIALOG_TROUBLE:"Teniu problemes per accedir?",OAUTHDIALOG_TIMEOUT_1:"No hem pogut iniciar-vos la sessiÃ³. Torneu-ho a provar.",CANCEL_BUTTON_TEXT:"CancelÂ·la",CANCEL_BUTTON_TEXT_2:"Tanca",OAUTHDIALOG_HEADING_2:"No sâ€™ha pogut iniciar la sessiÃ³"});
