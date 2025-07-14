e54f7e389ff51e219400b00749c8848b55d291d9
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:07:59 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/sign-services-auth/js/nls/nl-nl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SIGNIN_REQUIRED:"Aanmelden is verplicht",PRELOGIN_DIALOG_BODY_DEFAULT:"Meld u opnieuw aan. Zo blijven uw Sign-overeenkomsten beveiligd.",PRELOGIN_DIALOG_BODY_DOCTAB:"Als u op Annuleren klikt, worden alle geopende Sign-overeenkomsten gesloten.",CHECKBOX_TEXT:"Ik wil de komende 14 dagen aangemeld blijven",CANCEL:"Annuleren",NEXT:"Doorgaan",SIGOUT_DIALOG_TITLE:"Afmelden bij Adobe Acrobat Reader",SIGOUT_DIALOG_DESCRIPTION_1:'U bent momenteel aangemeld met uw Adobe ID <strong>"<%=emailId%>"</strong>. Als u klikt op â€˜afmeldenâ€™ meldt u zich af bij de app op dit apparaat. U wordt mogelijk gevraagd om de app af te sluiten. De app wordt niet van het apparaat verwijderd.',SIGOUT_DIALOG_DESCRIPTION_2:"Deze applicatie en cloud-assets die zijn gekoppeld aan uw Adobe ID zijn beschikbaar wanneer u zich opnieuw aanmeldt.",SIGOUT_DIALOG_CANCEL_BTN:"Annuleren",SIGOUT_DIALOG_SIGNOUT_BTN:"Afmelden"});
