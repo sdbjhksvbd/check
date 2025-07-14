2a6a17af3eab76d0fda4c473e79371df64be128d
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:03:26 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/scan-files/js/nls/hr-hr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"SCAN_FILES",ADOBE_SCAN:"Skeniranja",SCAN_FILES_TITLE:"Nabavite besplatnu aplikaciju Adobe Scan",SCAN_FILES_READER_CONTENT:"Telefon ili tablet pretvorite u prijenosni skener kojim bilo Å¡to moÅ¾ete skenirati i spremiti kao PDF. Preuzmite odmah.",SCAN_FILES_SIGN_IN_CONTENT:"VeÄ‡ upotrebljavate Adobe Scan? Prijavite se kako biste pristupili skeniranim dokumentima.",SCAN_FILES_CONTENT:"Telefon ili tablet pretvorite u prijenosni skener kojim bilo Å¡to moÅ¾ete skenirati i spremiti kao PDF. Preuzmite odmah.",NO_SCAN_TEXT:"Nemate dostupnih skeniranja.",SCAN_FILES_QR_CODE_TITLE:"Nabavite besplatnu aplikaciju Adobe Scan",SCAN_FILES_QR_CODE_CONTENT:"Skenirajte QR kod da biste bilo koji sadrÅ¾aj pretvorili u PDF na mobilnom ureÄ‘aju, a zatim ovdje pristupite skeniranim PDF dokumentima."});
