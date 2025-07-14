366b83ef23ae89eb9c8717cfe6df12aa23c01c50
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:50:06 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/tr-tr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"BilgisayarÄ±nÄ±z",SELECT_FILES:"BilgisayarÄ±mdaki Dosyalardan SeÃ§",NO_RECENT_FOLDERS_SELECT:'BilgisayarÄ±nÄ±zdaki bir dosyayÄ± aÃ§mak iÃ§in "GÃ¶zat" dÃ¼ÄŸmesini tÄ±klatÄ±n.',HEADING_RECENT_LOCATIONS:"Son Konumlar",HEADING_SELECT_FROM_RECENT_FOLDER:"Son KlasÃ¶rden SeÃ§...",HEADING_SELECT_FOLDER:"Son kullanÄ±lan klasÃ¶rlere kaydet...",BROWSE_BUTTON:"GÃ¶zat",BROWSE_DIALOG_BUTTON:"GÃ¶zat...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"FarklÄ± Bir KlasÃ¶r SeÃ§...",BROWSE_FOLDER_TEXT:"FarklÄ± bir klasÃ¶r seÃ§in",NO_RECENT_FOLDERS_SAVE_AS:"Son KlasÃ¶r Yok",NO_FOLDERS_MYCOMPUTER_PROMPT:"Burada henÃ¼z dosya yok.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"DosyalarÄ± bilgisayarÄ±nÄ±zdan aÃ§tÄ±ÄŸÄ±nÄ±zda, klasÃ¶rlerini burada gÃ¶receksiniz."});
