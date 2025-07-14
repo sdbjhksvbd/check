9fafd20916c91972283fb5a3fad3b757ae567ff5
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:56 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/tr-tr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"<%= connectorLabel %> DosyalarÄ±",GENERIC_ERROR_TITLE:"Hata",GENERIC_ERROR_MSG:"DosyalarÄ±nÄ±z alÄ±nÄ±rken bir sorun oluÅŸtu.",CONNECTOR_FOOTER_ANCHOR_TEXT:"<%= connectorDisplayName %> Web Sitesine Git",ACCOUNT_REMOVED_MESSAGE:"Acrobat, <%= connectorDisplayName %> hesabÄ±nÄ±za eriÅŸemiyor",ACCESS_REMOVED_ACTION:"DosyalarÄ±nÄ±za eriÅŸmek iÃ§in <%= connectorDisplayName %> hesabÄ±nÄ±zda oturum aÃ§Ä±n.",ACCESS_REMOVED_CTA:"Oturum AÃ§",CONNECTOR_NOTIFICATION_HEADING:"<%= connectorDisplayName %> DosyalarÄ±",FETCHING_TRANSCRIPTS:"Transkriptleriniz alÄ±nÄ±yor..."});
