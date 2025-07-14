ccd1a48e770de676f94ff415966e89cf8ace81ec
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:40 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/tr-tr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"Ekle",ADD_BUTTON_ARIA_LABEL:"<%= connectorName %> Ekle",ADD_ACCOUNT_TITLE:"Hesap Ekle",ADD_MEETING_SOURCE_TITLE:"Microsoft Teams hesabÄ± zaten baÄŸlÄ±",ADD_ACCOUNT_MESSAGE:"Bu depolama ayarÄ± iÃ§in zaten bir hesabÄ±nÄ±z var. LÃ¼tfen bu hesabÄ± kaldÄ±rÄ±n ve ardÄ±ndan tekrar deneyin.",ADD_MEETING_SOURCE_MESSAGE:"Zaten bir Microsoft Teams hesabÄ± baÄŸladÄ±nÄ±z. LÃ¼tfen bu hesabÄ± kaldÄ±rÄ±p tekrar deneyin.",ADD_BUTTON_ONE_CONNECTOR:"Hesap Ekle",CONNECTOR_DESC:"Uygulama iÃ§indeki dosyalarÄ±nÄ±za eriÅŸmek iÃ§in Microsoft SharePoint Sunucusuna ya da SharePoint Ã‡evrimiÃ§i hizmetine baÄŸlanÄ±n.",COMING_SOON_MSG:"Daha fazla hesap tÃ¼rÃ¼ Ã§ok yakÄ±nda sunulacak...",ADD_ACCOUNT_HEADER_TITLE:"Bir Depolama HesabÄ± Ekle",ADD_ACCOUNT_HEADER_DESCRIPTION:"DosyalarÄ±nÄ±za, Acrobat Ã¼zerinden eriÅŸmek iÃ§in hesabÄ±nÄ±za baÄŸlanÄ±n.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"GiriÅŸ yapmak iÃ§in bir web tarayÄ±cÄ±sÄ±na yÃ¶nlendirilebilirsiniz.",SIGN_IN_TO_TITLE:"<%= connectorName %> platformunda oturum aÃ§",ENTER_EMAIL_ID:"E-posta",CHOOSE_ACCOUNT:"<%= connectorName %> HesabÄ±nÄ± SeÃ§",PERSONAL_ACCOUNT_MSG:"<%= MSBrandName %> hesabÄ±nÄ±zla oturum aÃ§Ä±n",BUSINESS_ACCOUNT_MSG:"Ä°ÅŸ veya okul hesabÄ±nÄ±zla oturum aÃ§Ä±n",ENTER_EMAIL_ID_TOOLTIP:"Eklemek istediÄŸiniz <%= connectorName %> hesabÄ± e-posta adresini girin",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"ToplantÄ± transkriptleri ekle",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Herhangi bir toplantÄ± transkriptinden toplantÄ± Ã¶zetleri, tartÄ±ÅŸma<br/> konularÄ±, eylem Ã¶ÄŸeleri ve daha fazlasÄ±nÄ± oluÅŸturmak iÃ§in Ã¼retken yapay zeka Ã¶zelliklerini kullanÄ±n.",ADD_BUTTON_TEAMS_CONNECTOR:"Teams'e giriÅŸ yapÄ±n",ADD_BUTTON_ZOOM_CONNECTOR:"Daha fazla bilgi",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"Buradan bir toplantÄ± kaynaÄŸÄ± seÃ§in veya doÄŸrudan Acrobat'ta bir VTT dosyasÄ± aÃ§Ä±n."});
