baa4c7689ba9dc208eb065cb88c0013b33b4d3d9
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:31 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/sv-se/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"SÃ¶kresultat fÃ¶r <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"StÃ¤ng",SEARCH_CLOSE_LABEL:"StÃ¤ng  SÃ¶k",SEARCH_SIGNOUT_PROMPT:"<%= prefix %>Logga in<%= suffix %> om du vill sÃ¶ka i andra kategorier.",SEARCH_SIGNIN:"Logga in ",SEARCH_OFFLINE_PROMPT:"Det gick inte att slutfÃ¶ra din begÃ¤ran. Ett fel intrÃ¤ffade vid anslutning till tjÃ¤nsten. <%= prefix %>FÃ¶rsÃ¶k igen.<%= suffix %>",SEARCH_ONLINE:"FÃ¶rsÃ¶k igen",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Inga resultat hittades",SEARCH_NO_RESULTS_MESSAGE:"Vi kunde inte hitta nÃ¥gra resultat fÃ¶r <b><%= searchQuery %></b>.<br>Kontrollera stavningen eller fÃ¶rsÃ¶k att bredda sÃ¶kningen.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Inga resultat hittades i <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Vi kunde inte hitta nÃ¥gra resultat fÃ¶r <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"NÃ¥got gick fel",SEARCH_CATEGORY_ERROR:"Det gÃ¥r inte att sÃ¶ka i den hÃ¤r kategorin.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> SÃ¶kresultat fÃ¶r <%= query %>: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> SÃ¶kresultat: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Vi kan inte sÃ¶ka i den hÃ¤r kategorin. FÃ¶rsÃ¶k igen senare.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
