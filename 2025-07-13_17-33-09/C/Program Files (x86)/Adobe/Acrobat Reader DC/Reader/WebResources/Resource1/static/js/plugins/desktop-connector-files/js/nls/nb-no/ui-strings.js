acc5f79dd3896fcc623a50c562477a6cf6882515
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:44 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/nb-no/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"<%= connectorLabel %>-filer",GENERIC_ERROR_TITLE:"Feil",GENERIC_ERROR_MSG:"Det oppstod en feil under henting av filene.",CONNECTOR_FOOTER_ANCHOR_TEXT:"GÃ¥ til <%= connectorDisplayName %>-nettstedet",ACCOUNT_REMOVED_MESSAGE:"Acrobat fÃ¥r ikke tilgang til <%= connectorDisplayName %>-kontoen",ACCESS_REMOVED_ACTION:"Logg pÃ¥ <%= connectorDisplayName %>-kontoen for Ã¥ fÃ¥ tilgang til filene dine.",ACCESS_REMOVED_CTA:"Logg pÃ¥",CONNECTOR_NOTIFICATION_HEADING:"<%= connectorDisplayName %>-filer",FETCHING_TRANSCRIPTS:"Henter transkripsjonene dine â€¦"});
