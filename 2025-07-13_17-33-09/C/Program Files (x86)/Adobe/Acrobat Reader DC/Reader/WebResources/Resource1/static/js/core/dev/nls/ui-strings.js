a72217c2b16cb019507212c7a72db8fb9c5d524e
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:37:01 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/core/dev/nls/ui-strings.js
@@ -0,0 +1,20 @@
+// /*************************************************************************
+// *
+// * ADOBE CONFIDENTIAL
+// * ___________________
+// *
+// *  Copyright 2012 Adobe Systems Incorporated
+// *  All Rights Reserved.
+// *
+// * NOTICE:  All information contained herein is, and remains
+// * the property of Adobe Systems Incorporated and its suppliers,
+// * if any.  The intellectual and technical concepts contained
+// * herein are proprietary to Adobe Systems Incorporated and its
+// * suppliers and may be covered by U.S. and Foreign Patents,
+// * patents in process, and are protected by trade secret or copyright law.
+// * Dissemination of this information or reproduction of this material
+// * is strictly forbidden unless prior written permission is obtained
+// * from Adobe Systems Incorporated.
+// **************************************************************************/
+
+define({root:!0,"bg-bg":!0,"ar-ae":!0,"ca-es":!0,"cs-cz":!0,"da-dk":!0,"de-de":!0,"en-ae":!0,"en-gb":!0,"en-il":!0,"es-es":!0,"eu-es":!0,"fi-fi":!0,"fr-fr":!0,"fr-ma":!0,"he-il":!0,"hr-hr":!0,"hu-hu":!0,"it-it":!0,"ja-jp":!0,"ko-kr":!0,"nb-no":!0,"nl-nl":!0,"pl-pl":!0,"pt-br":!0,"ro-ro":!0,"ru-ru":!0,"sk-sk":!0,"sl-si":!0,"sl-sl":!0,"sv-se":!0,"tr-tr":!0,"uk-ua":!0,"zh-cn":!0,"zh-tw":!0});
