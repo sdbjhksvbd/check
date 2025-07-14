09fad08de52bb43a9aba1e3eee8d80f77bf3f862
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:30 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/ca-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"El teu ordinador",SELECT_FILES:"Seleccioneu els fitxers des d'El meu ordinador",NO_RECENT_FOLDERS_SELECT:"Feu clic a Navega per obrir un fitxer des de l'ordinador.",HEADING_RECENT_LOCATIONS:"Ubicacions recents",HEADING_SELECT_FROM_RECENT_FOLDER:"Seleccioneu-lo de la carpeta Recents...",HEADING_SELECT_FOLDER:"Desa a la carpeta Recents...",BROWSE_BUTTON:"Navega",BROWSE_DIALOG_BUTTON:"Navega...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"Trieu una altra carpeta...",BROWSE_FOLDER_TEXT:"Seleccioneu una altra carpeta",NO_RECENT_FOLDERS_SAVE_AS:"No hi ha cap carpeta recent.",NO_FOLDERS_MYCOMPUTER_PROMPT:"AquÃ­ encara no hi ha cap fitxer.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"En obrir els fitxers des de l'ordinador, aquÃ­ en veureu les carpetes."});
