634427ea7c411df64a778db0e568a8e2a74938d4
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:47:09 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/generic-rhp-app/js/nls/root/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({GENERIC_RHP_APP_REDACT:"Redact",GENERIC_RHP_APP_TOOL_VIEW_TITLE_RHP_REDACT:"Permanently delete sensitive text, graphics, or hidden data",GENERIC_RHP_APP_ICON_TOOLTIP_REDACT:"Learn more",GENERIC_RHP_APP_START_REDACT:"Learn more",GENERIC_RHP_APP_HEADING_REDACT:"Adobe Acrobat Pro DC",GENERIC_RHP_APP_RHP_GO_URL_REDACT:"http://www.google.com",GENERIC_RHP_APP_PROTECT:"Protect",GENERIC_RHP_APP_TOOL_VIEW_TITLE_RHP_PROTECT:"Prevent others from copying, printing &amp; editing the information in PDFs",GENERIC_RHP_APP_ICON_TOOLTIP_PROTECT:"Learn more",GENERIC_RHP_APP_START_PROTECT:"Learn more",GENERIC_RHP_APP_HEADING_PROTECT:"Adobe Acrobat Pro DC",GENERIC_RHP_APP_RHP_GO_URL_PROTECT:"http://www.google.com",GENERIC_RHP_APP_OPTIMIZE:"Optimize PDF",GENERIC_RHP_APP_TOOL_VIEW_TITLE_RHP_OPTIMIZE:"Reduce PDF file size, enable fast web view, control image and font options",GENERIC_RHP_APP_ICON_TOOLTIP_OPTIMIZE:"Learn more",GENERIC_RHP_APP_START_OPTIMIZE:"Learn more",GENERIC_RHP_APP_HEADING_OPTIMIZE:"Adobe Acrobat Pro DC",GENERIC_RHP_APP_RHP_GO_URL_OPTIMIZE:"http://www.google.com"});
