01a213340f10dfeee473548b53e1a9de226e8dfe
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:21 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/nl-nl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"Zoekresultaten voor <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Sluiten",SEARCH_CLOSE_LABEL:"Zoekopdracht sluiten",SEARCH_SIGNOUT_PROMPT:"<%= prefix %>Meld u aan<%= suffix %> om andere categorieÃ«n te doorzoeken.",SEARCH_SIGNIN:"Aanmelden",SEARCH_OFFLINE_PROMPT:"Kan uw verzoek niet uitvoeren. Fout bij maken van verbinding met service. <%= prefix %>Probeer het nogmaals<%= suffix %>.",SEARCH_ONLINE:"Opnieuw proberen",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Geen resultaten gevonden",SEARCH_NO_RESULTS_MESSAGE:"Geen zoekresultaten voor <b><%= searchQuery %></b>.<br> Controleer uw spelling of maak de zoekopdracht algemener.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Geen resultaten gevonden in <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Geen resultaten gevonden voor <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"Er is iets misgegaan",SEARCH_CATEGORY_ERROR:"We kunnen deze categorie niet doorzoeken.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> Zoekresultaten voor <%= query %>: <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> Zoekresultaten: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"We kunnen deze categorie niet doorzoeken. Probeer het later opnieuw. ",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
