2e8c5392f49aa120cfe5cdab7e28c55179355917
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:48:47 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/en-ae/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Select a sensitivity label",CANCEL_BUTTON_TEXT:"Cancel",UPDATE_BUTTON_TEXT:"Update",APPLY_BUTTON_TEXT:"Apply",BACK_BUTTON_TEXT:"Back",SUBMIT_BUTTON_TEXT:"Submit",SAVE_BUTTON_TEXT:"Save",ADD_BUTTON_TEXT:"Add",JUSTIFICATION_REQUIRED:"Justification required",JUSTIFICATION_HEAD_TEXT:"Your organization requires justification for changing sensitivity label.",JUSTIFICATION_OPTION_1:"The previous label no longer applies",JUSTIFICATION_OPTION_2:"Previous label was incorrect",JUSTIFICATION_OPTION_OTHER:"Other (explain) - Do not enter sensitive data",ENTER_JUSTIFICATION:"Enter justification",DELETE_LABEL:"Delete label",LABEL_DETAILS_TOOLTIP:"Sensitivity label details",SPECIFY_USER_OR_GROUP:"Specify user, group or domain",DEFINE_LABEL_PERMISSION:"Define label permission",MORE_OPTIONS:"More options",CAN_READ:"Can read",CAN_CHANGE:"Can change",CAN_FULL_CONTROL:"Can full control",READ:"Read",READ_COPY:"Read (can copy)",READ_PRINT:"Read (can print)",READ_COPY_PRINT:"Read (can copy and print)",CHANGE:"Change",CHANGE_PRINT:"Change (can print)",FULL_CONTROL:"Full control",DELETE:"Delete",CUSTOM:"Custom",OWNER:"Owner",OWNER_SIGNAL:"(you)",DOCUMENT_EXPIRY:"This document expires on ",READ_PERMISSION_TEXT:"People with read permission are also allowed to",COPY_CHECKBOX:"Copy this document",CHANGE_PERMISSION_TEXT:"People with read or change permission are also allowed to",PRINT_CHECKBOX:"Print this document",MMDDYYYY_PLACEHOLDER:"mm/dd/yyyy",EXPIRY_DATE_ERROR:"Enter a valid expiry date",UDP_EDIT_LABEL:"Edit label permissions",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"ADDED RECIPIENTS",GET_MS_GRAPH_CONTACTS_MSG:"Sign in with your Office account to access Outlook contacts.",GET_MS_GRAPH_CONTACTS_MSG_1:"Sign in with your ",GET_MS_GRAPH_CONTACTS_MSG_2:" account to access ",GET_MS_GRAPH_CONTACTS_MSG_3:" contacts.",EMAIL_REMOVE:"Remove email"});
