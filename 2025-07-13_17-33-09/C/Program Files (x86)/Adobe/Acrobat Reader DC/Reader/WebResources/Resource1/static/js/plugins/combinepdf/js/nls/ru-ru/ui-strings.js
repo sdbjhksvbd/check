81d8ca6f6055647bc24cb58b83e3ed5d0bb33b25
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:42:09 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/combinepdf/js/nls/ru-ru/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({COMBINE_PDF:"ÐžÐ±ÑŠÐµÐ´Ð¸Ð½ÐµÐ½Ð¸Ðµ Ñ„Ð°Ð¹Ð»Ð¾Ð² PDF",COMBINE_PDF_TOOL_VIEW_TITLE_RHP:"ÐžÐ±ÑŠÐµÐ´Ð¸Ð½ÐµÐ½Ð¸Ðµ Ð´Ð²ÑƒÑ… Ð¸Ð»Ð¸ Ð±Ð¾Ð»ÐµÐµ Ñ„Ð°Ð¹Ð»Ð¾Ð² Ð² Ð¾Ð´Ð¸Ð½ Ñ„Ð°Ð¹Ð» PDF",COMBINE_PDF_ICON_TOOLTIP:"ÐŸÐ¾Ð´Ñ€Ð¾Ð±Ð½ÐµÐµ",COMBINE_PDF_START:"ÐŸÐ¾Ð´Ñ€Ð¾Ð±Ð½ÐµÐµ",COMBINE_PDF_HEADING:"Adobe Acrobat Pro",COMBINE_PDF_RHP_GO_URL:"http://www.adobe.com/go/combinerhprdr_12_0_11_ru"});
