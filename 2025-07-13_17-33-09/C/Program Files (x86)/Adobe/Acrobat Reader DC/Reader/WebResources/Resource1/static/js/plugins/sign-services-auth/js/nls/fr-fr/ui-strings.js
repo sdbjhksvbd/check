69b69843dd878fb00ce1c91e463517b55e12d1a3
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:07:56 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/sign-services-auth/js/nls/fr-fr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SIGNIN_REQUIRED:"Connexion requise",PRELOGIN_DIALOG_BODY_DEFAULT:"Veuillez vous reconnecter. Ainsi, vos accords Ã  signer resteront en sÃ©curitÃ©.",PRELOGIN_DIALOG_BODY_DOCTAB:"Si vous cliquez sur Annuler, tous les accords ouverts pour signature seront fermÃ©s.",CHECKBOX_TEXT:"Maintenir ma connexion pour les 14Â prochains jours",CANCEL:"Annuler",NEXT:"Continuer",SIGOUT_DIALOG_TITLE:"Se dÃ©connecter d'Adobe Acrobat Reader",SIGOUT_DIALOG_DESCRIPTION_1:'Vous Ãªtes actuellement connectÃ© avec votre AdobeÂ ID <strong>"<%=emailId%>"</strong>. En cliquant sur "Se dÃ©connecter", vous vous dÃ©connectez de l\'application sur cet appareil. Cette application peut vous demander de quitter. L\'application ne sera pas dÃ©sinstallÃ©e.',SIGOUT_DIALOG_DESCRIPTION_2:"Cette application et les ressources cloud associÃ©es Ã  votre AdobeÂ ID seront disponibles lors de votre prochaine connexion.",SIGOUT_DIALOG_CANCEL_BTN:"Annuler",SIGOUT_DIALOG_SIGNOUT_BTN:"Se dÃ©connecter"});
