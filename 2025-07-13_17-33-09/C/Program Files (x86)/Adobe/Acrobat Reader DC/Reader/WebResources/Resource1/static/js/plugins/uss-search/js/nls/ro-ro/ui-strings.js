fd2718bf1267b6df7dd327616dc2cc78cce1be0f
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:26 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/ro-ro/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"Rezultatele cÄƒutÄƒrii pentru <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"ÃŽnchidere",SEARCH_CLOSE_LABEL:"ÃŽnchidere  CÄƒutare",SEARCH_SIGNOUT_PROMPT:"Pentru a cÄƒuta Ã®n alte categorii, vÄƒ rugÄƒm sÄƒ <%= prefix %>vÄƒ conectaÈ›i.<%= suffix %>",SEARCH_SIGNIN:"Conectare",SEARCH_OFFLINE_PROMPT:"Solicitarea dvs. nu a putut fi finalizatÄƒ. A survenit o eroare Ã®n timpul conectÄƒrii la serviciu. <%= prefix %>ÃŽncercaÈ›i din nou.<%= suffix %>",SEARCH_ONLINE:"ÃŽncercaÅ£i din nou",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Niciun rezultat gÄƒsit",SEARCH_NO_RESULTS_MESSAGE:"Nu am putut gÄƒsi niciun rezultat pentru <b><%= searchQuery %></b>.<br> VerificaÈ›i-vÄƒ ortografia sau Ã®ncercaÈ›i sÄƒ vÄƒ extindeÈ›i cÄƒutarea.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Niciun rezultat gÄƒsit Ã®n <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Nu am putut gÄƒsi niciun rezultat pentru <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"Ceva nu a funcÈ›ionat corect",SEARCH_CATEGORY_ERROR:"Nu am reuÈ™it sÄƒ cÄƒutÄƒm Ã®n aceastÄƒ categorie.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> Rezultatele cÄƒutÄƒrii pentru <%= query %> : <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> Rezultate cÄƒutare: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Nu am reuÈ™it sÄƒ cÄƒutÄƒm Ã®n aceastÄƒ categorie. VÄƒ rugÄƒm sÄƒ Ã®ncercaÈ›i din nou mai tÃ¢rziu.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
