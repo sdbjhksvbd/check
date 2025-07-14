e904a7023c323605b0f10b73ad8aaa42ce64d866
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:13:49 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/versions/framework-dev.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2015 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define(["aicx-unmapped/dev/all-modules","app-api-unmapped/dev/app-api","core-unmapped/dev/all-modules","files-unmapped/dev/all-modules","plugins-common-unmapped/dev/all-modules"],function(){return undefined});
