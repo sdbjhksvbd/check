cc0228f2cce42628756d58d227d644831581770d
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:48:46 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/de-de/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Vertraulichkeitsbezeichnung auswÃ¤hlen",CANCEL_BUTTON_TEXT:"Abbrechen",UPDATE_BUTTON_TEXT:"Aktualisieren",APPLY_BUTTON_TEXT:"Ãœbernehmen",BACK_BUTTON_TEXT:"ZurÃ¼ck",SUBMIT_BUTTON_TEXT:"Senden",SAVE_BUTTON_TEXT:"Speichern",ADD_BUTTON_TEXT:"HinzufÃ¼gen",JUSTIFICATION_REQUIRED:"BegrÃ¼ndung erforderlich",JUSTIFICATION_HEAD_TEXT:"Deine Organisation benÃ¶tigt eine BegrÃ¼ndung fÃ¼r die Ã„nderung der Vertraulichkeitsbezeichnung.",JUSTIFICATION_OPTION_1:"Die vorherige Bezeichnung ist nicht mehr gÃ¼ltig",JUSTIFICATION_OPTION_2:"Vorherige Bezeichnung war nicht korrekt",JUSTIFICATION_OPTION_OTHER:"Sonstiges (bitte erlÃ¤utern)Â â€“ Gib keine vertraulichen Daten ein",ENTER_JUSTIFICATION:"BegrÃ¼ndung eingeben",DELETE_LABEL:"Bezeichnung lÃ¶schen",LABEL_DETAILS_TOOLTIP:"Details zu Vertraulichkeitsbezeichnungen",SPECIFY_USER_OR_GROUP:"Benutzer, Gruppe oder Domain angeben",DEFINE_LABEL_PERMISSION:"Bezeichnungsberechtigung definieren",MORE_OPTIONS:"Weitere Optionen",CAN_READ:"Kann lesen",CAN_CHANGE:"Kann Ã¤ndern",CAN_FULL_CONTROL:"Kann vollstÃ¤ndige Kontrolle",READ:"Lesen",READ_COPY:"Lesen (kann kopieren)",READ_PRINT:"Lesen (kann drucken)",READ_COPY_PRINT:"Lesen (kann kopieren und drucken)",CHANGE:"Ã„ndern",CHANGE_PRINT:"Ã„ndern (kann drucken)",FULL_CONTROL:"Volle Kontrolle",DELETE:"LÃ¶schen",CUSTOM:"Benutzerdefiniert",OWNER:"EigentÃ¼mer",OWNER_SIGNAL:"(du)",DOCUMENT_EXPIRY:"Dieses Dokument lÃ¤uft ab am ",READ_PERMISSION_TEXT:"Personen mit Leseberechtigung kÃ¶nnen ebenfalls",COPY_CHECKBOX:"Dieses Dokument kopieren",CHANGE_PERMISSION_TEXT:"Personen mit Lese- oder Ã„nderungsberechtigung sind ebenfalls dazu berechtigt",PRINT_CHECKBOX:"Dieses Dokument drucken",MMDDYYYY_PLACEHOLDER:"mm/dd/yyyy",EXPIRY_DATE_ERROR:"GÃ¼ltiges Ablaufdatum eingeben",UDP_EDIT_LABEL:"Label-Berechtigungen bearbeiten",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"HINZUGEFÃœGTE EMPFÃ„NGER",GET_MS_GRAPH_CONTACTS_MSG:"Melde dich mit deinem Office-Konto an, um auf Outlook-Kontakte zuzugreifen.",GET_MS_GRAPH_CONTACTS_MSG_1:"Mit deinem Konto ",GET_MS_GRAPH_CONTACTS_MSG_2:" anmelden, um auf Kontakte ",GET_MS_GRAPH_CONTACTS_MSG_3:" zuzugreifen.",EMAIL_REMOVE:"E-Mail entfernen"});
