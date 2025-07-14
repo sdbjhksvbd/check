41a92c95449335dac3cff828c500f7d891c93370
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:52:23 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-recent-files/js/nls/hr-hr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"Nedavno koriÅ¡teno",MY_RECENT_FILES:"Moje nedavno koriÅ¡tene datoteke",NO_RECENT_FILES:"Nema nedavno koriÅ¡tenih datoteka",NO_SEARCH_RESULTS:"PretraÅ¾ivanje nije dalo rezultate.",TRY_DIFFERENT_SEARCH:"PokuÅ¡ajte pokrenuti drugo pretraÅ¾ivanje"});
