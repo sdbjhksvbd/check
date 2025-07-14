164b7921e4998d2ac3da493a09e12ed4e601d362
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:42:54 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/createpdfupsell-app/js/nls/pl-pl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2016 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({CREATEPDFUPSELL_APP:"UtwÃ³rz plik PDF",CREATEPDFUPSELL_APP_TOOL_VIEW_TITLE_RHP:"Konwertuj pliki do formatu PDF i Å‚atwo Å‚Ä…cz je z innymi rodzajami plikÃ³w dziÄ™ki pÅ‚atnej subskrypcji",CREATEPDFUPSELL_APP_ICON_TOOLTIP:"WiÄ™cej informacji",CREATEPDFUPSELL_APP_START:"WiÄ™cej informacji",CREATEPDFUPSELL_APP_HEADING:"Adobe Acrobat Pro",CREATEPDFUPSELL_APP_RHP_GO_URL:"http://www.adobe.com/go/cpdfrhprdr_12_0_11_pl"});
