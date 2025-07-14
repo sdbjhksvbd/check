feb4730d71033c97452444f4c84d1e47768849ad
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:38 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/sv-se/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"LÃ¤gg till",ADD_BUTTON_ARIA_LABEL:"<%= connectorName %> LÃ¤gg till",ADD_ACCOUNT_TITLE:"LÃ¤gg till konto",ADD_MEETING_SOURCE_TITLE:"Microsoft Teams-konto redan lÃ¤nkat",ADD_ACCOUNT_MESSAGE:"Du har redan ett konto fÃ¶r lagring. Ta bort det kontot och fÃ¶rsÃ¶k igen.",ADD_MEETING_SOURCE_MESSAGE:"Du har redan lÃ¤nkat ett Microsoft Teams-konto. Ta bort det kontot och fÃ¶rsÃ¶k igen.",ADD_BUTTON_ONE_CONNECTOR:"LÃ¤gg till konto",CONNECTOR_DESC:"Anslut till Microsoft SharePoint Server eller SharePoint Online nÃ¤r du vill komma Ã¥t dina filer frÃ¥n programmet.",COMING_SOON_MSG:"Fler kontotyper kommer snartâ€¦",ADD_ACCOUNT_HEADER_TITLE:"LÃ¤gg till ett lagringskonto",ADD_ACCOUNT_HEADER_DESCRIPTION:"Anslut ditt konto nÃ¤r du vill fÃ¥ Ã¥tkomst till dina filer via Acrobat.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Du kan omdirigeras till en webblÃ¤sare fÃ¶r att logga in.",SIGN_IN_TO_TITLE:"Logga in pÃ¥ <%= connectorName %>",ENTER_EMAIL_ID:"E-post",CHOOSE_ACCOUNT:"VÃ¤lj <%= connectorName %>-konto",PERSONAL_ACCOUNT_MSG:"Logga in med ditt <%= MSBrandName %>-konto",BUSINESS_ACCOUNT_MSG:"Logga in med ditt arbets- eller skolkonto",ENTER_EMAIL_ID_TOOLTIP:"Ange e-postadressen fÃ¶r <%= connectorName %>-kontot som du vill lÃ¤gga till",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"LÃ¤gg till mÃ¶tesutskrifter",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"AnvÃ¤nd funktioner fÃ¶r generativ AI fÃ¶r att generera mÃ¶tessammanfattningar,<br/> diskussionsÃ¤mnen, Ã¥tgÃ¤rder och annat frÃ¥n valfri mÃ¶tesutskrift.",ADD_BUTTON_TEAMS_CONNECTOR:"Logga in till Teams",ADD_BUTTON_ZOOM_CONNECTOR:"LÃ¤s mer",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"VÃ¤lj mÃ¶teskÃ¤lla hÃ¤r eller Ã¶ppna en VTT-fil direkt i Acrobat."});
