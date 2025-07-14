fde0e7c0f4766310b1940e7d6ae72ec479572bb2
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:22 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/pl-pl/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Oczekiwanie...",OAUTHDIALOG_2:"OtworzyliÅ›my nowÄ… kartÄ™ logowania w przeglÄ…darce.",OAUTHDIALOG_3:"Po zakoÅ„czeniu uwierzytelniania w przeglÄ…darce nastÄ…pi automatyczne przekierowanie z powrotem do aplikacji.",OAUTHDIALOG_TROUBLE:"Nie moÅ¼esz siÄ™ zalogowaÄ‡?",OAUTHDIALOG_TIMEOUT_1:"Logowanie nie powiodÅ‚o siÄ™. SprÃ³buj ponownie.",CANCEL_BUTTON_TEXT:"Anuluj",CANCEL_BUTTON_TEXT_2:"Zamknij",OAUTHDIALOG_HEADING_2:"Logowanie nie powiodÅ‚o siÄ™"});
