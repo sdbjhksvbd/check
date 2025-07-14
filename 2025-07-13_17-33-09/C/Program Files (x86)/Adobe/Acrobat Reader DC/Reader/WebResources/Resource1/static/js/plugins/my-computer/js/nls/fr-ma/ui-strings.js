6909b8819b64bbee1782b56e66742bc9d8178179
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:47 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/fr-ma/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"Votre ordinateur",SELECT_FILES:"Choisir les fichiers sur lâ€™ordinateur",NO_RECENT_FOLDERS_SELECT:"Pour ouvrir un fichier de votre ordinateur, cliquez sur Parcourir.",HEADING_RECENT_LOCATIONS:"Emplacements rÃ©cents",HEADING_SELECT_FROM_RECENT_FOLDER:"SÃ©lectionner dans le dossier des fichiers rÃ©centsâ€¦",HEADING_SELECT_FOLDER:"Enregistrer dans le dossier des fichiers rÃ©centsâ€¦",BROWSE_BUTTON:"Parcourir",BROWSE_DIALOG_BUTTON:"Parcourir...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"SÃ©lectionner un autre dossier...",BROWSE_FOLDER_TEXT:"SÃ©lectionner un autre dossier",NO_RECENT_FOLDERS_SAVE_AS:"Aucun dossier rÃ©cent",NO_FOLDERS_MYCOMPUTER_PROMPT:"Il nâ€™y a encore aucun fichier ici.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"Lorsque vous ouvrez des fichiers depuis lâ€™ordinateur, leurs dossiers sont rÃ©pertoriÃ©s ici."});
