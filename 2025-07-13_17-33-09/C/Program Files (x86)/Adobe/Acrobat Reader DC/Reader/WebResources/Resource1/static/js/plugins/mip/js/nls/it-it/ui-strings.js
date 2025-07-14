b3541f1a992c744c6b4da9e1beee62c8197ec23b
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:48:59 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/it-it/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Seleziona unâ€™etichetta di riservatezza",CANCEL_BUTTON_TEXT:"Annulla",UPDATE_BUTTON_TEXT:"Aggiorna",APPLY_BUTTON_TEXT:"Applica",BACK_BUTTON_TEXT:"Indietro",SUBMIT_BUTTON_TEXT:"Invia",SAVE_BUTTON_TEXT:"Salva",ADD_BUTTON_TEXT:"Aggiungi",JUSTIFICATION_REQUIRED:"Specifica una giustificazione",JUSTIFICATION_HEAD_TEXT:"La tua organizzazione richiede una giustificazione per modificare lâ€™etichetta di riservatezza.",JUSTIFICATION_OPTION_1:"Lâ€™etichetta precedente non Ã¨ piÃ¹ applicabile",JUSTIFICATION_OPTION_2:"Lâ€™etichetta precedente era sbagliata",JUSTIFICATION_OPTION_OTHER:"Altro (spiega) - Non inserire dati sensibili",ENTER_JUSTIFICATION:"Inserisci giustificazione",DELETE_LABEL:"Elimina etichetta",LABEL_DETAILS_TOOLTIP:"Dettagli dellâ€™etichetta di riservatezza",SPECIFY_USER_OR_GROUP:"Specifica utente, gruppo o dominio",DEFINE_LABEL_PERMISSION:"Definisci autorizzazioni per etichetta",MORE_OPTIONS:"Altre opzioni",CAN_READ:"PuÃ² leggere",CAN_CHANGE:"PuÃ² modificare",CAN_FULL_CONTROL:"PuÃ² avere pieno controllo",READ:"Leggere",READ_COPY:"Leggere (puÃ² copiare)",READ_PRINT:"Leggere (puÃ² stampare)",READ_COPY_PRINT:"Leggere (puÃ² copiare e stampare)",CHANGE:"Modificare",CHANGE_PRINT:"Modificare (puÃ² stampare)",FULL_CONTROL:"Pieno controllo",DELETE:"Eliminare",CUSTOM:"Personalizzato",OWNER:"Proprietario",OWNER_SIGNAL:"(tu)",DOCUMENT_EXPIRY:"Questo documento scade il ",READ_PERMISSION_TEXT:"Le persone autorizzate a leggere possono anche",COPY_CHECKBOX:"Copiare questo documento",CHANGE_PERMISSION_TEXT:"Le persone autorizzate a leggere o modificare possono anche",PRINT_CHECKBOX:"Stampare questo documento",MMDDYYYY_PLACEHOLDER:"gg/mm/aaaa",EXPIRY_DATE_ERROR:"Inserisci una data di scadenza valida",UDP_EDIT_LABEL:"Modifica autorizzazioni etichetta",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"DESTINATARI AGGIUNTI",GET_MS_GRAPH_CONTACTS_MSG:"Accedi con il tuo account Google per usare i contatti di Outlook.",GET_MS_GRAPH_CONTACTS_MSG_1:"Usa il tuo account ",GET_MS_GRAPH_CONTACTS_MSG_2:" per accedere ai contatti ",GET_MS_GRAPH_CONTACTS_MSG_3:" .",EMAIL_REMOVE:"Rimuovi e-mail"});
