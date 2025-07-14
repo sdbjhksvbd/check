abb9de6cbc96688e8394184d9f034d16e67aff75
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:49 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/pt-br/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"Arquivos <%= connectorLabel %>",GENERIC_ERROR_TITLE:"Erro",GENERIC_ERROR_MSG:"Ocorreu um erro ao buscar seus arquivos.",CONNECTOR_FOOTER_ANCHOR_TEXT:"Ir para o site <%= connectorDisplayName %>",ACCOUNT_REMOVED_MESSAGE:"O Acrobat nÃ£o conseguiu acessar sua conta do <%= connectorDisplayName %>",ACCESS_REMOVED_ACTION:"FaÃ§a logon na sua conta do <%= connectorDisplayName %> para acessar seus arquivos.",ACCESS_REMOVED_CTA:"Entrar",CONNECTOR_NOTIFICATION_HEADING:"<%= connectorDisplayName %> Arquivos",FETCHING_TRANSCRIPTS:"Obtendo suas transcriÃ§Ãµes..."});
