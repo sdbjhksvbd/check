47b10aed1797c446b73b8c59f2910fb7921d0b14
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:26 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/it-it/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"Aggiungi",ADD_BUTTON_ARIA_LABEL:"Aggiungi <%= connectorName %>",ADD_ACCOUNT_TITLE:"Aggiungi account",ADD_MEETING_SOURCE_TITLE:"Lâ€™account Microsoft Teams Ã¨ giÃ  collegato",ADD_ACCOUNT_MESSAGE:"Esiste giÃ  un account impostato per questa archiviazione. Rimuovere tale account e riprovare.",ADD_MEETING_SOURCE_MESSAGE:"Hai giÃ  collegato un account Microsoft Teams. Rimuovi tale account e riprova.",ADD_BUTTON_ONE_CONNECTOR:"Aggiungi account",CONNECTOR_DESC:"Connessione a Microsoft SharePoint Server o SharePoint Online per accedere ai file direttamente dall'applicazione.",COMING_SOON_MSG:"Altri tipi di account disponibili a breve...",ADD_ACCOUNT_HEADER_TITLE:"Aggiungere un account di archiviazione",ADD_ACCOUNT_HEADER_DESCRIPTION:"Connettersi al proprio account per accedere ai file direttamente da Acrobat.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Potresti essere reindirizzato a un browser Web per lâ€™accesso.",SIGN_IN_TO_TITLE:"Accedere a <%= connectorName %>",ENTER_EMAIL_ID:"E-mail",CHOOSE_ACCOUNT:"Scegliere l'account <%= connectorName %>",PERSONAL_ACCOUNT_MSG:"Accedere con il proprio account <%= MSBrandName %>",BUSINESS_ACCOUNT_MSG:"Accedere utilizzando l'account aziendale o scolastico",ENTER_EMAIL_ID_TOOLTIP:"Specificare l'indirizzo e-mail dell'account <%= connectorName %> da aggiungere",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"Aggiungi trascrizioni di riunioni",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Usa le funzioni di IA generativa per creare il riepilogo di una riunione,<br/> argomenti di discussione, action item e altro ancora.",ADD_BUTTON_TEAMS_CONNECTOR:"Accedi a Teams",ADD_BUTTON_ZOOM_CONNECTOR:"Scopri di piÃ¹",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"Scegli lâ€™origine di una riunione oppure apri un file VTT direttamente in Acrobat."});
