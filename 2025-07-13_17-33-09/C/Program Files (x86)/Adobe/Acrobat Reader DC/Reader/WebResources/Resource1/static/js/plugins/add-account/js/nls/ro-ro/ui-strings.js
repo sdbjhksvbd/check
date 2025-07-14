84947c30356a678aa0429daf9ec50f56f4e59d56
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:34 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/ro-ro/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"AdÄƒugare",ADD_BUTTON_ARIA_LABEL:"AdÄƒugare <%= connectorName %>",ADD_ACCOUNT_TITLE:"AdÄƒugare cont",ADD_MEETING_SOURCE_TITLE:"Contul Microsoft Teams account deja asociat",ADD_ACCOUNT_MESSAGE:"Deja aveÈ›i un cont pentru aceastÄƒ configurare de stocare. È˜tergeÈ›i acel cont È™i Ã®ncercaÈ›i din nou.",ADD_MEETING_SOURCE_MESSAGE:"AÈ›i asociat deja un cont Microsoft Teams. EliminaÈ›i acel cont È™i Ã®ncercaÈ›i din nou.",ADD_BUTTON_ONE_CONNECTOR:"AdÄƒugare cont",CONNECTOR_DESC:"VÄƒ conectaÅ£i la Microsoft SharePoint Server sau SharePoint Online pentru a vÄƒ accesa fiÅŸierele din interiorul aplicaÅ£iei.",COMING_SOON_MSG:"Mai multe tipuri de conturi vor fi disponibile Ã®n curÃ¢nd...",ADD_ACCOUNT_HEADER_TITLE:"AdÄƒugaÈ›i un cont de stocare",ADD_ACCOUNT_HEADER_DESCRIPTION:"ConectaÈ›i-vÄƒ la contul dvs. pentru a accesa fiÈ™ierele din Acrobat.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Este posibil sÄƒ fiÈ›i redirecÈ›ionat cÄƒtre un browser web pentru a vÄƒ conecta.",SIGN_IN_TO_TITLE:"ConectaÈ›i-vÄƒ la <%= connectorName %>",ENTER_EMAIL_ID:"E-mail",CHOOSE_ACCOUNT:"AlegeÈ›i Contul <%= connectorName %>",PERSONAL_ACCOUNT_MSG:"ConectaÈ›i-vÄƒ cu contul dvs. <%= MSBrandName %>",BUSINESS_ACCOUNT_MSG:"ConectaÈ›i-vÄƒ cu contul dvs. de serviciu sau de la È™coalÄƒ",ENTER_EMAIL_ID_TOOLTIP:"IntroduceÈ›i e-mail-ul contului <%= connectorName %> pe care doriÈ›i sÄƒ-l adÄƒugaÈ›i",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"AdÄƒugaÈ›i transcrieri ale È™edinÈ›elor",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"UtilizaÈ›i funcÈ›iile de inteligenÈ›Äƒ artificialÄƒ generativÄƒ pentru a crea rezumate de È™edinÈ›Äƒ, subiecte<br/> de discuÈ›ie, elemente de acÈ›iune È™i multe altele din orice transcriere de È™edinÈ›Äƒ.",ADD_BUTTON_TEAMS_CONNECTOR:"ConectaÈ›i-vÄƒ la Teams",ADD_BUTTON_ZOOM_CONNECTOR:"AflaÈ›i mai multe",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"AlegeÈ›i o sursÄƒ de È™edinÈ›Äƒ aici sau deschideÈ›i un fiÈ™ier VTT direct Ã®n Acrobat."});
