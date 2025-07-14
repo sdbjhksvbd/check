3351668f374bed49d5b259a158591d2d338b2511
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:12:14 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/uss-search/js/nls/hr-hr/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SEARCH_RESULTS_LABEL:"Rezultati pretraÅ¾ivanja za <b><%= searchQuery %></b>",SEARCH_RESULTS_COUNT:"(<%=count%>)",SEARCH_PENDING_STATUS:"(...)",SEARCH_SUCCESS_FAILED:"(!)",SEARCH_CLOSE:"Zatvori",SEARCH_CLOSE_LABEL:"Zatvori  TraÅ¾i",SEARCH_SIGNOUT_PROMPT:"Å½elite li pretraÅ¾iti ostale kategorije, <%= prefix %>prijavite se.<%= suffix %>",SEARCH_SIGNIN:"Prijava",SEARCH_OFFLINE_PROMPT:"Zahtjev se nije mogao dovrÅ¡iti. Pri pokuÅ¡aju uspostavljanja veze sa servisom doÅ¡lo je do pogreÅ¡ke. <%= prefix %>PokuÅ¡ajte ponovo.<%= suffix %>",SEARCH_ONLINE:"PokuÅ¡ajte ponovno",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",SEARCH_NO_RESULTS_TITLE:"Nema rezultata",SEARCH_NO_RESULTS_MESSAGE:"Nije pronaÄ‘en nijedan rezultat za <b><%= searchQuery %></b>.<br> Provjerite ima li pravopisnih pogreÅ¡aka ili pokuÅ¡ajte proÅ¡iriti pretraÅ¾ivanje.",SEARCH_NO_RESULTS_TITLE_CATEGORY:"Nema rezultata u <%= category %>",SEARCH_NO_RESULTS_MESSAGE_CATEGORY:"Nije pronaÄ‘en nijedan rezultat za <b><%= searchQuery %></b>.",SEARCH_NO_RESULTS_ERROR:"NeÅ¡to je poÅ¡lo po krivu",SEARCH_CATEGORY_ERROR:"Ta se kategorija ne moÅ¾e pretraÅ¾iti.",SEARCH_CATEGORY_RESULTS_ARIA_LABEL:"<%= category %> Rezultati pretraÅ¾ivanja za <%= query %> : <%= count %>",SEARCH_CATEGORY_SUMMARY_ARIA_LABEL:"<%= category %> Rezultati pretraÅ¾ivanja: <%= count %>",SEARCH_NO_RESULTS_MESSAGE_ERROR:"Ta se kategorija ne moÅ¾e pretraÅ¾iti. PokuÅ¡ajte ponovo kasnije.",AGREEMENT_ENTERPRISE_HEADER:"Adobe Acrobat Sign"});
