dc0a13fa8bb1d7d73b3cb0fabf96922add1eea7e
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:52:59 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/da-dk/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Venter...",OAUTHDIALOG_2:"Vi har Ã¥bnet en ny browserfane, hvor du kan logge pÃ¥.",OAUTHDIALOG_3:"Efter du fuldfÃ¸rer godkendelse i browseren, fortsÃ¦tter du automatisk i appen.",OAUTHDIALOG_TROUBLE:"Problemer med at komme pÃ¥?",OAUTHDIALOG_TIMEOUT_1:"Vi kunne ikke logge dig pÃ¥. PrÃ¸v igen.",CANCEL_BUTTON_TEXT:"Annuller",CANCEL_BUTTON_TEXT_2:"Luk",OAUTHDIALOG_HEADING_2:"Login mislykkedes"});
