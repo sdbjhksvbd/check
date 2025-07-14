9281643295438b360a68cac04cfe8ad0962d5082
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:09 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/de-de/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"HinzufÃ¼gen",ADD_BUTTON_ARIA_LABEL:"<%= connectorName %> hinzufÃ¼gen",ADD_ACCOUNT_TITLE:"Konto hinzufÃ¼gen",ADD_MEETING_SOURCE_TITLE:"Microsoft Teams-Konto bereits verknÃ¼pft",ADD_ACCOUNT_MESSAGE:"Du besitzt bereits ein Konto fÃ¼r dieses Speicher-Setup. Entferne das Konto und versuche es erneut.",ADD_MEETING_SOURCE_MESSAGE:"Du hast bereits ein Microsoft Teams-Konto verknÃ¼pft. Entferne dieses Konto, und versuche es erneut.",ADD_BUTTON_ONE_CONNECTOR:"Konto hinzufÃ¼gen",CONNECTOR_DESC:"Stelle eine Verbindung zum Microsoft SharePoint-Server oder zu SharePoint Online her, um Zugriff auf die Dateien Ã¼ber die Anwendung zu erhalten.",COMING_SOON_MSG:"Weitere Kontotypen folgen in KÃ¼rzeÂ â€¦",ADD_ACCOUNT_HEADER_TITLE:"FÃ¼ge ein Speicherkonto hinzu.",ADD_ACCOUNT_HEADER_DESCRIPTION:"Verbinde dich mit deinem Konto, um in Acrobat auf Dateien zuzugreifen.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Du wirst mÃ¶glicherweise zu einem Webbrowser weitergeleitet, um dich anzumelden.",SIGN_IN_TO_TITLE:"Bei <%= connectorName %> anmelden",ENTER_EMAIL_ID:"E-Mail",CHOOSE_ACCOUNT:"<%= connectorName %>-Konto auswÃ¤hlen",PERSONAL_ACCOUNT_MSG:"Mit <%= MSBrandName %>-Konto anmelden",BUSINESS_ACCOUNT_MSG:"Mit Konto eines Unternehmens oder einer Bildungseinrichtung anmelden",ENTER_EMAIL_ID_TOOLTIP:"Gib die E-Mail-Adresse fÃ¼r das <%= connectorName %>-Konto ein, das du hinzufÃ¼gen mÃ¶chtest.",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"Besprechungstranskripte hinzufÃ¼gen",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Nutze generative KI-Funktionen, um aus jedem Besprechungstranskript Zusammenfassungen von Besprechungen, Diskussionsthemen,<br/> Aktionselemente und mehr zu generieren.",ADD_BUTTON_TEAMS_CONNECTOR:"Bei Teams anmelden",ADD_BUTTON_ZOOM_CONNECTOR:"Weitere Infos",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"WÃ¤hle hier eine Besprechungsquelle oder Ã¶ffne eine VTT-Datei direkt in Acrobat."});
