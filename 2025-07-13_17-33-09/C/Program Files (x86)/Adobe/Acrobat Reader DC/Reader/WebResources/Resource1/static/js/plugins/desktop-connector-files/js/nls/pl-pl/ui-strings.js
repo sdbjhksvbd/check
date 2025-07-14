c2b75d16a34961390577ef793aae2664206a026b
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:47 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/pl-pl/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"<%= connectorLabel %> â€“ pliki",GENERIC_ERROR_TITLE:"BÅ‚Ä…d",GENERIC_ERROR_MSG:"Podczas przesyÅ‚ania plikÃ³w wystÄ…piÅ‚ bÅ‚Ä…d.",CONNECTOR_FOOTER_ANCHOR_TEXT:"PrzejdÅº na stronÄ™ <%= connectorDisplayName %>",ACCOUNT_REMOVED_MESSAGE:"Program Acrobat nie ma dostÄ™pu do konta <%= connectorDisplayName %>",ACCESS_REMOVED_ACTION:"Zaloguj siÄ™ do swojego konta <%= connectorDisplayName %>, aby uzyskaÄ‡ dostÄ™p do swoich plikÃ³w.",ACCESS_REMOVED_CTA:"Zaloguj siÄ™",CONNECTOR_NOTIFICATION_HEADING:"Pliki <%= connectorDisplayName %>",FETCHING_TRANSCRIPTS:"Pobieranie transkrypcjiâ€¦"});
