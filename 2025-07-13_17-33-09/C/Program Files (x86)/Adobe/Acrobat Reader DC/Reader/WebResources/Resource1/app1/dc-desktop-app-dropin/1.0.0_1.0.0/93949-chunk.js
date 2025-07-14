b99e72bb0df0d0552934f77cdb350f0f9d1f98b6
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:20:03 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/app1/dc-desktop-app-dropin/1.0.0_1.0.0/93949-chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackJsonp-desktop-app"]=self["webpackJsonp-desktop-app"]||[]).push([[93949],{gA51:(p,e,s)=>{s.r(e),s.d(e,{default:()=>a});const a=s.p+"9b1662bee64658ff8dd184737a056510.png"}}]);
