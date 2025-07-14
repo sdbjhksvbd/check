6af51338af807e5c3f2b631e2634b3fd56110e6e
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:20 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/ca-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"Fitxers del <%= connectorLabel %>",GENERIC_ERROR_TITLE:"Error",GENERIC_ERROR_MSG:"S'ha produÃ¯t un error en recuperar els fitxers.",CONNECTOR_FOOTER_ANCHOR_TEXT:"VÃ©s al lloc web de <%= connectorDisplayName %>",ACCOUNT_REMOVED_MESSAGE:"L'Acrobat no pot accedir al compte de <%= connectorDisplayName %>",ACCESS_REMOVED_ACTION:"Inicieu la sessiÃ³ al compte del <%= connectorDisplayName %> per accedir als fitxers.",ACCESS_REMOVED_CTA:"Inicia la sessiÃ³",CONNECTOR_NOTIFICATION_HEADING:"Fitxers del <%= connectorDisplayName %>",FETCHING_TRANSCRIPTS:"Sâ€™estan obtenint les transcripcions..."});
