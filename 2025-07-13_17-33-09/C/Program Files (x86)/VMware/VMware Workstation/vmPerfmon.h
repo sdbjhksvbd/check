9dc0d27048157f88f0aec16de1ca625c1f5632d3
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 05:01:02 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/VMware/VMware Workstation/vmPerfmon.h
@@ -0,0 +1,25 @@
+/* **********************************************************
+ * Copyright (c) 2003-2007 VMware, Inc.  All rights reserved.
+ * **********************************************************/
+
+#define OBJECT_1           0
+
+#define DEVICE_COUNTER_1   2
+#define DEVICE_COUNTER_2   4
+#define DEVICE_COUNTER_3   6
+#define DEVICE_COUNTER_4   8
+#define DEVICE_COUNTER_5   10
+#define DEVICE_COUNTER_6   12
+#define DEVICE_COUNTER_7   14
+#define DEVICE_COUNTER_8   16
+#define DEVICE_COUNTER_9   18
+#define DEVICE_COUNTER_10   20
+#define DEVICE_COUNTER_11   22
+#define DEVICE_COUNTER_12   24
+#define DEVICE_COUNTER_13   26
+#define DEVICE_COUNTER_14   28
+#define DEVICE_COUNTER_15   30
+#define DEVICE_COUNTER_16   32
+#define DEVICE_COUNTER_17   34
+#define DEVICE_COUNTER_18   36
+
