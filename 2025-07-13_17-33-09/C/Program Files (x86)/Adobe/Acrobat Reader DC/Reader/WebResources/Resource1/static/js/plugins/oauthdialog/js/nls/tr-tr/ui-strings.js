cbc278f5deda21e08b3be9093a4b5914cc638ff7
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:30 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/tr-tr/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Bekleniyor...",OAUTHDIALOG_2:"GiriÅŸ yapmanÄ±z iÃ§in yeni bir tarayÄ±cÄ± sekmesi aÃ§tÄ±k.",OAUTHDIALOG_3:"TarayÄ±cÄ±da kimlik doÄŸrulamasÄ±nÄ± bitirdikten sonra otomatik olarak uygulama iÃ§inden devam edeceksiniz.",OAUTHDIALOG_TROUBLE:"GiriÅŸ yapmakta sorun mu yaÅŸÄ±yorsunuz?",OAUTHDIALOG_TIMEOUT_1:"Oturumunuzu aÃ§amadÄ±k. LÃ¼tfen tekrar deneyin.",CANCEL_BUTTON_TEXT:"Ä°ptal",CANCEL_BUTTON_TEXT_2:"Kapat",OAUTHDIALOG_HEADING_2:"GiriÅŸ yapma baÅŸarÄ±sÄ±z"});
