4f369607c1898e01f6958ed1fe0acfcfb9187207
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:04 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/pl-pl/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Wybierz etykietÄ™ poufnoÅ›ci",CANCEL_BUTTON_TEXT:"Anuluj",UPDATE_BUTTON_TEXT:"Zaktualizuj",APPLY_BUTTON_TEXT:"Zastosuj",BACK_BUTTON_TEXT:"Wstecz",SUBMIT_BUTTON_TEXT:"PrzeÅ›lij",SAVE_BUTTON_TEXT:"Zapisz",ADD_BUTTON_TEXT:"Dodaj",JUSTIFICATION_REQUIRED:"Wymagane uzasadnienie",JUSTIFICATION_HEAD_TEXT:"Twoja organizacja wymaga uzasadnienia dla zmiany etykiety poufnoÅ›ci.",JUSTIFICATION_OPTION_1:"Poprzednia etykieta nie ma juÅ¼ zastosowania",JUSTIFICATION_OPTION_2:"Poprzednia etykieta byÅ‚a nieprawidÅ‚owa",JUSTIFICATION_OPTION_OTHER:"Inne (wyjaÅ›nij); nie wprowadzaj danych poufnych",ENTER_JUSTIFICATION:"WprowadÅº uzasadnienie",DELETE_LABEL:"UsuÅ„ etykietÄ™",LABEL_DETAILS_TOOLTIP:"SzczegÃ³Å‚y etykiety poufnoÅ›ci",SPECIFY_USER_OR_GROUP:"OkreÅ›l uÅ¼ytkownika, grupÄ™ lub domenÄ™",DEFINE_LABEL_PERMISSION:"OkreÅ›l uprawnienia etykiety",MORE_OPTIONS:"WiÄ™cej opcji",CAN_READ:"MoÅ¼e odczytaÄ‡",CAN_CHANGE:"MoÅ¼e edytowaÄ‡",CAN_FULL_CONTROL:"PeÅ‚ne uprawnienia",READ:"Odczyt",READ_COPY:"Odczyt (moÅ¼e kopiowaÄ‡)",READ_PRINT:"Odczyt (moÅ¼e drukowaÄ‡)",READ_COPY_PRINT:"Odczyt (moÅ¼e kopiowaÄ‡ i drukowaÄ‡)",CHANGE:"Edycja",CHANGE_PRINT:"Edycja (moÅ¼e drukowaÄ‡)",FULL_CONTROL:"PeÅ‚ne uprawnienia",DELETE:"UsuÅ„",CUSTOM:"Niestandardowe",OWNER:"WÅ‚aÅ›ciciel",OWNER_SIGNAL:"(Ty)",DOCUMENT_EXPIRY:"Niniejszy dokument wygasa ",READ_PERMISSION_TEXT:"Osoby z uprawnieniami do odczytu majÄ… teÅ¼ inne moÅ¼liwoÅ›ci:",COPY_CHECKBOX:"Skopiuj ten dokument",CHANGE_PERMISSION_TEXT:"Osoby zÂ uprawnieniami do odczytu lub edycji majÄ… teÅ¼ inne moÅ¼liwoÅ›ci:",PRINT_CHECKBOX:"Wydrukuj ten dokument",MMDDYYYY_PLACEHOLDER:"mm/dd/rrrr",EXPIRY_DATE_ERROR:"WprowadÅº prawidÅ‚owÄ… datÄ™ waÅ¼noÅ›ci",UDP_EDIT_LABEL:"Edytuj uprawnienia etykiety",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"DODANI ODBIORCY",GET_MS_GRAPH_CONTACTS_MSG:"Zaloguj siÄ™ za pomocÄ… konta Office, aby uzyskaÄ‡ dostÄ™p do kontaktÃ³w w programie Outlook.",GET_MS_GRAPH_CONTACTS_MSG_1:"Zaloguj siÄ™ za pomocÄ… konta,Â  ",GET_MS_GRAPH_CONTACTS_MSG_2:" aby uzyskaÄ‡ dostÄ™p do ",GET_MS_GRAPH_CONTACTS_MSG_3:" kontaktÃ³w.",EMAIL_REMOVE:"UsuÅ„ e-mail"});
