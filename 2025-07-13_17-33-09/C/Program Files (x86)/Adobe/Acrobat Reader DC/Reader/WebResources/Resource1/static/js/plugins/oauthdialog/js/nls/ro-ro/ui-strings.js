bddb4aa22958d24dba1e48744a04f6f278489e1e
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:25 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/ro-ro/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"ÃŽn aÈ™teptare...",OAUTHDIALOG_2:"Am deschis o nouÄƒ filÄƒ de browser pe care o puteÈ›i utiliza pentru a vÄƒ conecta.",OAUTHDIALOG_3:"DupÄƒ finalizarea autentificÄƒrii Ã®n browser, veÈ›i reveni automat Ã®n aplicaÈ›ie.",OAUTHDIALOG_TROUBLE:"Probleme la accesare?",OAUTHDIALOG_TIMEOUT_1:"Nu am reuÈ™it sÄƒ vÄƒ conectÄƒm. ÃŽncercaÈ›i din nou.",CANCEL_BUTTON_TEXT:"Anulare",CANCEL_BUTTON_TEXT_2:"ÃŽnchidere",OAUTHDIALOG_HEADING_2:"Conectarea a eÈ™uat"});
