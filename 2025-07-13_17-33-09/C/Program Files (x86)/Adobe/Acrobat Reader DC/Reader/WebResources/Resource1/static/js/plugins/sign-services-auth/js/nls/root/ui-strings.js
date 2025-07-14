748a0a0d5c57ac944544a319ffb965e75ff3fdf2
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 04:08:01 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/sign-services-auth/js/nls/root/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2017 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SIGNIN_REQUIRED:"Sign in required",PRELOGIN_DIALOG_BODY_DEFAULT:"Please sign in again. This helps keep your sign agreements secure.",PRELOGIN_DIALOG_BODY_DOCTAB:"Clicking Cancel will close all open sign agreements.",CHECKBOX_TEXT:"Keep me signed in for the next 14 days",CANCEL:"Cancel",NEXT:"Continue",SIGOUT_DIALOG_TITLE:"Sign out from Adobe Acrobat Reader",SIGOUT_DIALOG_DESCRIPTION_1:'Youâ€™re currently signed in with your Adobe ID <strong>"<%=emailId%>"</strong>. By clicking â€˜sign outâ€™ youâ€™ll sign out of application on this device. This app may ask to quit. App will not be uninstalled.',SIGOUT_DIALOG_DESCRIPTION_2:"This application and cloud assets related to your Adobe ID will be available when you sign in again.",SIGOUT_DIALOG_CANCEL_BTN:"Cancel",SIGOUT_DIALOG_SIGNOUT_BTN:"Sign out"});
