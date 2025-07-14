82172fe64f6832d751e32949f00a7ac0c764ea03
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:17 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/eu-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"Gehitu",ADD_BUTTON_ARIA_LABEL:"<%= connectorName %> gehitzea",ADD_ACCOUNT_TITLE:"Gehitu kontua",ADD_MEETING_SOURCE_TITLE:"Dagoeneko lotuta dago Microsoft Teams kontua",ADD_ACCOUNT_MESSAGE:"Dagoeneko duzu biltegi-konfigurazioaren kontu bat. Kendu kontua eta saiatu berriro.",ADD_MEETING_SOURCE_MESSAGE:"Dagoeneko lotuta zaude Microsoft Teams kontu bati. Kendu kontu hori eta saiatu berriro.",ADD_BUTTON_ONE_CONNECTOR:"Gehitu kontua",CONNECTOR_DESC:"Konektatu Microsoft SharePoint edo SharePoint Online zerbitzura fitxategiak aplikaziotik atzitzeko.",COMING_SOON_MSG:"Kontu mota berriak laster erabilgarri izango dituzu...",ADD_ACCOUNT_HEADER_TITLE:"Gehitu biltegiratze-kontua",ADD_ACCOUNT_HEADER_DESCRIPTION:"Konektatu zure kontura Acrobat-en dituzun fitxategiak atzitzeko.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"Baliteke web-arakatzaile batera birbideratzea saioa hasteko.",SIGN_IN_TO_TITLE:"Hasi saioa <%= connectorName %> zerbitzuan",ENTER_EMAIL_ID:"Helbide elektronikoa",CHOOSE_ACCOUNT:"Aukeratu <%= connectorName %> kontua",PERSONAL_ACCOUNT_MSG:"Hasi saioa <%= MSBrandName %> kontuarekin",BUSINESS_ACCOUNT_MSG:"Hasi saioa laneko edo eskolako kontuarekin",ENTER_EMAIL_ID_TOOLTIP:"Idatzi gehitu nahi duzun <%= connectorName %> kontuaren helbide elektronikoa",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"Gehitu bileren transkripzioak",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"Erabili AA sortzailearen eginbideak edozein bilera-transkripziotik bileren laburpenak, eztabaidagaiak<br/>, ekintza-elementuak eta gehiago sortzeko.",ADD_BUTTON_TEAMS_CONNECTOR:"Hasi saioa Teams-en",ADD_BUTTON_ZOOM_CONNECTOR:"Lortu informazio gehiago",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"Aukeratu bilera-iturburu bat hemen edo ireki VTT fitxategi bat Acrobat-en zuzenean."});
