0dc76bc55b36d51174235d939e02169350278f50
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:46:07 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/exportpdfupsell-app/js/nls/da-dk/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2016 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({EXPORTPDFUPSELL_APP:"EksportÃ©r PDF",EXPORTPDFUPSELL_APP_TOOL_VIEW_TITLE_RHP:"Konverter PDF-filer til Word eller Excel online",EXPORTPDFUPSELL_APP_ICON_TOOLTIP:"FÃ¥ mere at vide",EXPORTPDFUPSELL_APP_START:"FÃ¥ mere at vide",EXPORTPDFUPSELL_APP_HEADING:"Adobe Acrobat Pro DC",EXPORTPDFUPSELL_APP_RHP_GO_URL:"http://www.adobe.com/go/epdfrhprdr_12_0_11_dk"});
