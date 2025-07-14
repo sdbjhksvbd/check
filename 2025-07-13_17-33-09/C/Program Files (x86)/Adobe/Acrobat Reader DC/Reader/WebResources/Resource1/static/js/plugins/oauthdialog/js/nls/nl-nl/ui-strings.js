ab3ebc0f2a2a7208bd200852388eae12c77690d2
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:53:20 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/oauthdialog/js/nls/nl-nl/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({OAUTHDIALOG_1:"Wachten",OAUTHDIALOG_2:"Er is een nieuw tabblad geopend in de browser, zodat u zich kunt aanmelden.",OAUTHDIALOG_3:"Na verificatie in de browser gaat u automatisch verder in de app.",OAUTHDIALOG_TROUBLE:"Kunt u niet inloggen?",OAUTHDIALOG_TIMEOUT_1:"U kunt niet worden aangemeld. Probeer het opnieuw.",CANCEL_BUTTON_TEXT:"Annuleren",CANCEL_BUTTON_TEXT_2:"Sluiten",OAUTHDIALOG_HEADING_2:"Aanmelden mislukt"});
