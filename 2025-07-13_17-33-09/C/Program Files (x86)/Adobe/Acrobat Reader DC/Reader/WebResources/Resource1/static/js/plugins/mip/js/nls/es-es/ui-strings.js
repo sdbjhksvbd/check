4529d2cf3ebd549d9ef8b63e2d4daae3072b9b4e
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:48:51 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/mip/js/nls/es-es/ui-strings.js
@@ -0,0 +1,18 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2021 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+**************************************************************************/
+
+define({SENSITIVITY_LABEL:"Seleccionar una etiqueta de sensibilidad",CANCEL_BUTTON_TEXT:"Cancelar",UPDATE_BUTTON_TEXT:"Actualizar",APPLY_BUTTON_TEXT:"Aplicar",BACK_BUTTON_TEXT:"AtrÃ¡s",SUBMIT_BUTTON_TEXT:"Enviar",SAVE_BUTTON_TEXT:"Guardar",ADD_BUTTON_TEXT:"Agregar",JUSTIFICATION_REQUIRED:"JustificaciÃ³n requerida",JUSTIFICATION_HEAD_TEXT:"La organizaciÃ³n requiere una justificaciÃ³n para cambiar la etiqueta de confidencialidad.",JUSTIFICATION_OPTION_1:"La etiqueta anterior ya no se aplica",JUSTIFICATION_OPTION_2:"La etiqueta anterior era incorrecta",JUSTIFICATION_OPTION_OTHER:"Otra (explicar sin introducir datos confidenciales)",ENTER_JUSTIFICATION:"Introducir justificaciÃ³n",DELETE_LABEL:"Eliminar etiqueta",LABEL_DETAILS_TOOLTIP:"Detalles de la etiqueta de confidencialidad",SPECIFY_USER_OR_GROUP:"Especificar usuario, grupo o dominio",DEFINE_LABEL_PERMISSION:"Definir permiso de etiqueta",MORE_OPTIONS:"MÃ¡s opciones",CAN_READ:"Puede leer",CAN_CHANGE:"Puede cambiar",CAN_FULL_CONTROL:"Puede controlar totalmente",READ:"Leer",READ_COPY:"Leer (puede copiar)",READ_PRINT:"Leer (puede imprimir)",READ_COPY_PRINT:"Leer (puede copiar e imprimir)",CHANGE:"Cambiar",CHANGE_PRINT:"Cambiar (puede imprimir)",FULL_CONTROL:"Control total",DELETE:"Eliminar",CUSTOM:"Personalizado",OWNER:"Propietario",OWNER_SIGNAL:"(tÃº)",DOCUMENT_EXPIRY:"Este documento vence el ",READ_PERMISSION_TEXT:"Los usuarios con permiso de lectura tambiÃ©n pueden",COPY_CHECKBOX:"Copiar este documento",CHANGE_PERMISSION_TEXT:"Los usuarios con permiso de lectura o modificaciÃ³n tambiÃ©n pueden",PRINT_CHECKBOX:"Imprimir este documento",MMDDYYYY_PLACEHOLDER:"dd/mm/aaaa",EXPIRY_DATE_ERROR:"Introduzca una fecha de caducidad vÃ¡lida",UDP_EDIT_LABEL:"Editar permisos de etiquetas",GENERIC_ARIA_LABEL_STRING:"<%= prefix %>. <%= suffix %>",ADDED_RECIPIENT_NO_COUNT:"DESTINATARIOS AÃ‘ADIDOS",GET_MS_GRAPH_CONTACTS_MSG:"Inicia sesiÃ³n con tu cuenta de Office para acceder a tus contactos de Outlook.",GET_MS_GRAPH_CONTACTS_MSG_1:"Inicia sesiÃ³n con tu ",GET_MS_GRAPH_CONTACTS_MSG_2:" cuenta para acceder a ",GET_MS_GRAPH_CONTACTS_MSG_3:" los contactos.",EMAIL_REMOVE:"Quitar direcciÃ³n de correo electrÃ³nico"});
