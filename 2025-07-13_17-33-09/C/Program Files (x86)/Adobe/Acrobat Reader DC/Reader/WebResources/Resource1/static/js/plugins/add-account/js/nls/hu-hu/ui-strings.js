4c867fb8521a9edf9fa52fef420252f535d41e6a
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:39:24 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/add-account/js/nls/hu-hu/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({ADD_BUTTON:"HozzÃ¡adÃ¡s",ADD_BUTTON_ARIA_LABEL:"<%= connectorName %> hozzÃ¡adÃ¡sa",ADD_ACCOUNT_TITLE:"FiÃ³k hozzÃ¡adÃ¡sa",ADD_MEETING_SOURCE_TITLE:"A Microsoft Teams-fiÃ³k mÃ¡r hozzÃ¡ van rendelve",ADD_ACCOUNT_MESSAGE:"MÃ¡r rendelkezik fiÃ³kkal ehhez a tÃ¡rolÃ¡si konfigurÃ¡ciÃ³hoz. KÃ©rjÃ¼k, tÃ¡volÃ­tsa el ezt a fiÃ³kot, majd prÃ³bÃ¡lkozzon Ãºjra.",ADD_MEETING_SOURCE_MESSAGE:"MÃ¡r hozzÃ¡ van rendelve egy Microsoft Teams-fiÃ³khoz. TÃ¡volÃ­tsa el a fiÃ³kot, Ã©s prÃ³bÃ¡lkozzon Ãºjra.",ADD_BUTTON_ONE_CONNECTOR:"FiÃ³k hozzÃ¡adÃ¡sa",CONNECTOR_DESC:"Ha szeretnÃ© az alkalmazÃ¡sbÃ³l elÃ©rni fÃ¡jljait, akkor csatlakozzon a Microsoft SharePoint Serverhez vagy a SharePoint Online szolgÃ¡ltatÃ¡shoz.",COMING_SOON_MSG:"TÃ¶bb fiÃ³ktÃ­pus hamarosan...",ADD_ACCOUNT_HEADER_TITLE:"TÃ¡rolÃ¡si fiÃ³k hozzÃ¡adÃ¡sa",ADD_ACCOUNT_HEADER_DESCRIPTION:"Jelentkezzen be fiÃ³kjÃ¡ba, hogy az Acrobat programbÃ³l elÃ©rje fÃ¡jljait.",ADD_ACCOUNT_HEADER_DESCRIPTION_2:"ElÅ‘fordulhat, hogy a bejelentkezÃ©shez Ã¡tirÃ¡nyÃ­tjÃ¡k egy webbÃ¶ngÃ©szÅ‘be.",SIGN_IN_TO_TITLE:"BejelentkezÃ©s a <%= connectorName %>-ba",ENTER_EMAIL_ID:"E-mail",CHOOSE_ACCOUNT:"VÃ¡lasszon <%= connectorName %>-fiÃ³k tÃ­pust",PERSONAL_ACCOUNT_MSG:"Jelentkezzen be <%= MSBrandName %> fiÃ³kjÃ¡val",BUSINESS_ACCOUNT_MSG:"Jelentkezzen be munkahelyi vagy iskolai fiÃ³kjÃ¡val",ENTER_EMAIL_ID_TOOLTIP:"Adja meg a hozzÃ¡adandÃ³ <%= connectorName %> fiÃ³k e-mail cÃ­mÃ©t",ADD_ACCOUNT_TEAMS_HEADER_TITLE:"MegbeszÃ©lÃ©s jegyzeteinek hozzÃ¡adÃ¡sa",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION:"HasznÃ¡lja a generatÃ­v MI funkciÃ³kat a megbeszÃ©lÃ©sek Ã¶sszefoglalÃ³inak, vitatÃ©mÃ¡inak, cselekvÃ©si elemeinek Ã©s egyebeknek a lÃ©trehozÃ¡sÃ¡hoz bÃ¡rmilyen megbeszÃ©lÃ©si jegyzÅ‘kÃ¶nyv alapjÃ¡n.",ADD_BUTTON_TEAMS_CONNECTOR:"BejelentkezÃ©s a Teams felÃ¼letÃ©re",ADD_BUTTON_ZOOM_CONNECTOR:"TovÃ¡bbi informÃ¡ciÃ³",ADD_ACCOUNT_TEAMS_HEADER_DESCRIPTION_2:"VÃ¡lassza ki itt a megbeszÃ©lÃ©s forrÃ¡sÃ¡t, vagy nyissa meg a VTT-fÃ¡jlt kÃ¶zvetlenÃ¼l az Acrobatban."});
