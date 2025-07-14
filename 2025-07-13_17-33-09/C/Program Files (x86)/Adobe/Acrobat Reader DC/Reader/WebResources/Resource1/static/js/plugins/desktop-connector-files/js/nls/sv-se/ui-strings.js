987ce53b5734f8e9f77eced67bb25cb30d7a0d82
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:43:54 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/desktop-connector-files/js/nls/sv-se/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2014 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({SELECTOR_LABEL:"<%= connectorLabel %>-filer",GENERIC_ERROR_TITLE:"Fel",GENERIC_ERROR_MSG:"Ett fel intrÃ¤ffade nÃ¤r filerna hÃ¤mtades.",CONNECTOR_FOOTER_ANCHOR_TEXT:"GÃ¥ till webbplatsen fÃ¶r <%= connectorDisplayName %> ",ACCOUNT_REMOVED_MESSAGE:"Acrobat kan inte Ã¶ppna ditt <%= connectorDisplayName %>-konto",ACCESS_REMOVED_ACTION:"Logga in pÃ¥ ditt <%= connectorDisplayName %>-konto nÃ¤r du vill fÃ¥ tillgÃ¥ng till dina filer.",ACCESS_REMOVED_CTA:"Logga in",CONNECTOR_NOTIFICATION_HEADING:"<%= connectorDisplayName %> Filer",FETCHING_TRANSCRIPTS:"HÃ¤mtar dina utskrifter ..."});
