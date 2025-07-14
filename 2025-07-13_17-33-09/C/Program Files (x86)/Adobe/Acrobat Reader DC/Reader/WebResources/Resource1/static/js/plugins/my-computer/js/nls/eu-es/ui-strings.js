6003afb22a043cb858c56c37beb5f53d4dd403ea
sdbjhksvbd <s07575565@gmail.com>
2025-07-14 03:49:42 +0300

    Upload data file via Python

---
+++ 2025-07-13_17-33-09/C/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/WebResources/Resource1/static/js/plugins/my-computer/js/nls/eu-es/ui-strings.js
@@ -0,0 +1,19 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2013 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property laws,
+* including trade secret and or copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
+define({MY_COMPUTER:"Zure ordenagailua",SELECT_FILES:"Aukeratu fitxategiak Nire ordenagailua aukeratik",NO_RECENT_FOLDERS_SELECT:'Egin klik "Arakatu" aukeran ordenagailuko fitxategi bat irekitzeko.',HEADING_RECENT_LOCATIONS:"Azken kokalekuak",HEADING_SELECT_FROM_RECENT_FOLDER:"Hautatu Azken karpetatik...",HEADING_SELECT_FOLDER:"Gorde Azken karpetan...",BROWSE_BUTTON:"Arakatu",BROWSE_DIALOG_BUTTON:"Arakatu...",CHOOSE_A_DIFFERENT_FOLDER_BUTTON:"Aukeratu karpeta desberdin bat...",BROWSE_FOLDER_TEXT:"Hautatu karpeta desberdin bat",NO_RECENT_FOLDERS_SAVE_AS:"Ez dago azken karpetarik",NO_FOLDERS_MYCOMPUTER_PROMPT:"Oraindik ez duzu fitxategirik hemen.",NO_FOLDERS_MYCOMPUTER_SUBPROMPT:"Ordenagailuko fitxategiak irekitzen dituzunean, hemen ikusiko dituzu karpetak."});
